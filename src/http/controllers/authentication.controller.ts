948e9656a4c025ac27356c2b1f51949157232cc1
Lukas Hroch <lukas.hroch@mrc-epid.cam.ac.uk>
2020-05-14 12:00:57 +0100

    extract authentication logic to service

--- src/http/controllers/authentication.controller.ts
+++ src/http/controllers/authentication.controller.ts
@@ -1,32 +1,22 @@
-import bcrypt from 'bcryptjs';
-import { Request, Response, NextFunction } from 'express';
+import { Request, Response } from 'express';
 import ms from 'ms';
 import config from '@/config/security';
-import User from '@/db/models/system/user';
-import UserSurveyAliases from '@/db/models/system/user-survey-alias';
-import UserPassword from '@/db/models/system/user-password';
-import ApplicationError from '@/http/errors/application.error';
 import authSvc from '@/services/authentication.service';
-import jwtSvc, { TokenPayload } from '@/services/jwt.service';
+import { Tokens } from '@/services/jwt.service';
 
 /**
  * Sucessfull login response helper
- * - sign access & refresh token
  * - attach refresh token as secure cookie
  * - return access token in response body
  *
- * @param {User} user
+ * @param {Tokens} tokens
  * @param {Response} res
  * @returns {Promise<void>}
  */
-const sendTokenResponse = async (user: User, res: Response): Promise<void> => {
-  const payload: TokenPayload = {
-    userId: user.id,
-    roles: user.roleList(),
-  };
-
-  const { accessToken, refreshToken } = await jwtSvc.signTokens(payload);
-
+const sendTokenResponse = async (
+  { accessToken, refreshToken }: Tokens,
+  res: Response
+): Promise<void> => {
   const { name, httpOnly, maxAge, path, secure, sameSite } = config.jwt.cookie;
 
   res
@@ -40,75 +30,29 @@ const sendTokenResponse = async (user: User, res: Response): Promise<void> => {
     .json({ accessToken });
 };
 
-/**
- * Verify user's password
- * Includes legacy fallback (TODO: to implement)
- *
- * @param {string} password
- * @param {User} user
- * @returns {Promise<boolean>}
- */
-const verifyPassword = async (password: string, user: User): Promise<boolean> => {
-  if (user.password) return bcrypt.compare(password, user.password);
-
-  // TODO: verify user.legacyPassword
-  return false;
-};
-
 export default {
-  async emailLogin(req: Request, res: Response, next: NextFunction): Promise<void> {
+  async emailLogin(req: Request, res: Response): Promise<void> {
     const { email, password } = req.body;
 
-    const user = await User.scope(['legacyPassword', 'roles']).findOne({ where: { email } });
+    const tokens = await authSvc.emailLogin(email, password);
 
-    if (!user || !(await verifyPassword(password, user))) {
-      next(new ApplicationError(`Provided credentials doesn't match with our records.`));
-      return;
-    }
-
-    await sendTokenResponse(user, res);
+    await sendTokenResponse(tokens, res);
   },
 
-  async aliasLogin(req: Request, res: Response, next: NextFunction): Promise<void> {
+  async aliasLogin(req: Request, res: Response): Promise<void> {
     const { userName, password, surveyId } = req.body;
 
-    const user = await User.scope(['legacyPassword', 'roles']).findOne({
-      include: [
-        {
-          model: UserSurveyAliases,
-          where: { userName, surveyId },
-        },
-        { model: UserPassword },
-      ],
-    });
+    const tokens = await authSvc.aliasLogin(userName, password, surveyId);
 
-    if (!user || !(await verifyPassword(password, user))) {
-      next(new ApplicationError(`Provided credentials doesn't match with our records.`));
-      return;
-    }
-
-    await sendTokenResponse(user, res);
+    await sendTokenResponse(tokens, res);
   },
 
-  async tokenLogin(req: Request, res: Response, next: NextFunction): Promise<void> {
+  async tokenLogin(req: Request, res: Response): Promise<void> {
     const { token } = req.params;
 
-    const user = await User.scope('roles').findOne({
-      include: [
-        {
-          model: UserSurveyAliases,
-          where: { urlAuthToken: token },
-        },
-        { model: UserPassword },
-      ],
-    });
+    const tokens = await authSvc.tokenLogin(token);
 
-    if (!user) {
-      next(new ApplicationError(`Provided credentials doesn't match with our records.`));
-      return;
-    }
-
-    await sendTokenResponse(user, res);
+    await sendTokenResponse(tokens, res);
   },
 
   async refresh(req: Request, res: Response): Promise<void> {
--- src/services/authentication.service.ts
+++ src/services/authentication.service.ts
@@ -1,8 +1,75 @@
+import bcrypt from 'bcryptjs';
 import User from '@/db/models/system/user';
 import UnauthorizedError from '@/http/errors/unauthorized.error';
-import jwtSvc from './jwt.service';
+import ApplicationError from '@/http/errors/application.error';
+import UserSurveyAliases from '@/db/models/system/user-survey-alias';
+import UserPassword from '@/db/models/system/user-password';
+import jwtSvc, { TokenPayload, Tokens } from './jwt.service';
 
 export default {
+  async emailLogin(email: string, password: string): Promise<Tokens> {
+    const user = await User.scope(['legacyPassword', 'roles']).findOne({ where: { email } });
+
+    if (!user || !(await this.verifyPassword(password, user)))
+      throw new ApplicationError(`Provided credentials doesn't match with our records.`);
+
+    const payload: TokenPayload = { userId: user.id, roles: user.roleList() };
+
+    return jwtSvc.signTokens(payload);
+  },
+
+  async aliasLogin(userName: string, password: string, surveyId: string): Promise<Tokens> {
+    const user = await User.scope(['legacyPassword', 'roles']).findOne({
+      include: [
+        {
+          model: UserSurveyAliases,
+          where: { userName, surveyId },
+        },
+        { model: UserPassword },
+      ],
+    });
+
+    if (!user || !(await this.verifyPassword(password, user)))
+      throw new ApplicationError(`Provided credentials doesn't match with our records.`);
+
+    const payload: TokenPayload = { userId: user.id, roles: user.roleList() };
+
+    return jwtSvc.signTokens(payload);
+  },
+
+  async tokenLogin(token: string): Promise<Tokens> {
+    const user = await User.scope('roles').findOne({
+      include: [
+        {
+          model: UserSurveyAliases,
+          where: { urlAuthToken: token },
+        },
+        { model: UserPassword },
+      ],
+    });
+
+    if (!user) throw new ApplicationError(`Provided credentials doesn't match with our records.`);
+
+    const payload: TokenPayload = { userId: user.id, roles: user.roleList() };
+
+    return jwtSvc.signTokens(payload);
+  },
+
+  /**
+   * Verify user's password
+   * Includes legacy fallback (TODO: to implement)
+   *
+   * @param {string} password
+   * @param {User} user
+   * @returns {Promise<boolean>}
+   */
+  async verifyPassword(password: string, user: User): Promise<boolean> {
+    if (user.password) return bcrypt.compare(password, user.password);
+
+    // TODO: verify user.legacyPassword
+    return true;
+  },
+
   /**
    * Issue new access token using refresh token
    *
