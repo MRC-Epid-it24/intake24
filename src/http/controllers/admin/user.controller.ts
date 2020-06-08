be7f5b04d21310133858066b71e5f7b319680579
Lukas Hroch <lukas.hroch@mrc-epid.cam.ac.uk>
2020-06-08 10:48:56 +0100

    roles update for user record

--- src/http/controllers/admin/user.controller.ts
+++ src/http/controllers/admin/user.controller.ts
@@ -1,10 +1,12 @@
 import { Request, Response, NextFunction } from 'express';
 import { pick } from 'lodash';
+import { Op } from 'sequelize';
 import User from '@/db/models/system/user';
 import NotFoundError from '@/http/errors/not-found.error';
 import userResponse from '@/http/responses/admin/user-response';
 import { roleList } from '@/services/acl.service';
 import { hash } from '@/util/passwords';
+import UserRole from '@/db/models/system/user-role';
 
 const entry = async (req: Request, res: Response, next: NextFunction): Promise<void> => {
   const { id } = req.params;
@@ -29,7 +31,7 @@ export default {
   },
 
   async create(req: Request, res: Response): Promise<void> {
-    res.json({ data: { id: null }, refs: {} });
+    res.json({ data: { id: null }, refs: { roles: await roleList() } });
   },
 
   async store(req: Request, res: Response): Promise<void> {
@@ -43,11 +45,17 @@ export default {
         'simpleName',
         'emailNotifications',
         'smsNotifications',
+        'multiFactorAuthentication',
       ]),
       password: await hash(password),
     });
 
-    res.status(201).json({ data: userResponse(user) });
+    const { id } = user;
+    const { roles } = req.body;
+    const newRoles = roles.map((role: string) => ({ userId: id, role }));
+    await UserRole.bulkCreate(newRoles);
+
+    res.status(201).json({ data: userResponse((await User.scope('roles').findByPk(id)) as User) });
   },
 
   async show(req: Request, res: Response, next: NextFunction): Promise<void> {
@@ -75,10 +83,19 @@ export default {
         'simpleName',
         'emailNotifications',
         'smsNotifications',
+        'multiFactorAuthentication',
       ])
     );
 
-    res.json({ data: userResponse(user), refs: { roles: await roleList() } });
+    const { roles } = req.body;
+    await UserRole.destroy({ where: { userId: id, role: { [Op.notIn]: roles } } });
+    const newRoles = roles.map((role: string) => ({ userId: id, role }));
+    await UserRole.bulkCreate(newRoles);
+
+    res.json({
+      data: userResponse((await User.scope('roles').findByPk(id)) as User),
+      refs: { roles: await roleList() },
+    });
   },
 
   async delete(req: Request, res: Response, next: NextFunction): Promise<void> {
