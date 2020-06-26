3871432e416cea3f0e5f17184d57a43f005ace31
Lukas Hroch <lukas.hroch@mrc-epid.cam.ac.uk>
2020-06-26 16:07:05 +0100

    common aliases

--- survey-client/vue.config.js
+++ survey-client/vue.config.js
@@ -1,9 +1,15 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
+const path = require('path');
 const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer');
 
 module.exports = {
   configureWebpack: {
     plugins: process.env.WEBPACK_ANALYZE_BUNDLE === 'true' ? [new BundleAnalyzerPlugin()] : [],
+    resolve: {
+      alias: {
+        '@common': path.resolve(__dirname, '../common/src/'),
+      },
+    },
   },
   devServer: {
     port: 8200,
