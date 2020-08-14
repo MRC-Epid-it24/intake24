1c7d8326d2f74ba7eb07c3bbe23f0d52ec6dff32
Lukas Hroch <lukas.hroch@mrc-epid.cam.ac.uk>
2020-08-14 12:33:25 +0100

    remove legacy schemes since system won't support it

--- api-server/sequelize/system/migrations/20200601174002-schemes.js
+++ api-server/sequelize/system/migrations/20200601174002-schemes.js
@@ -28,19 +28,6 @@ module.exports = {
         { transaction }
       );
 
-      await queryInterface.bulkInsert(
-        'schemes',
-        [
-          { id: 'default', name: 'Default', type: 'legacy' },
-          { id: 'ndns_default', name: 'NDNS Default', type: 'legacy' },
-          { id: 'ndns419', name: 'NDNS April 2019', type: 'legacy' },
-          { id: 'ndns1019', name: 'NDNS October 2019', type: 'legacy' },
-          { id: 'sab', name: 'SAB', type: 'legacy' },
-          { id: 'bham1119', name: 'Birmingham November 2019', type: 'legacy' },
-        ],
-        { transaction }
-      );
-
       await queryInterface.addConstraint('surveys', ['scheme_id'], {
         name: 'surveys_scheme_id_schemes_fk',
         type: 'foreign key',
