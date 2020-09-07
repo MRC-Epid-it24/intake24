939539142106f6e63bc6b55611487468473f1bfa
Lukas Hroch <lukas.hroch@mrc-epid.cam.ac.uk>
2020-09-07 17:05:32 +0100

    deployment: nginx sites

---
+++ deployment/nginx-admin-site.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+if [ "$#" -ne 1 ]; then
+  echo "Instance name required"
+else
+  ansible-playbook -i ./instances/$1/hosts -e @./instances/$1/admin-site/nginx-site.json --extra-vars="instance=$1" ansible/nginx-site-admin.yml
+fi
