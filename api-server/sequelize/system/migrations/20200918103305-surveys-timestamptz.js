8e31aadc0b7bd8d6e0f5f4cb5e34258c804ee1dd
Lukas Hroch <lukas.hroch@mrc-epid.cam.ac.uk>
2020-09-18 15:00:46 +0100

    change survey datetimes to tz timestamps
    - prevents wrong offset assignments do to parsing through Date between client-server

---
+++ api-server/sequelize/system/migrations/20200918103305-surveys-timestamptz.js
@@ -0,0 +1,27 @@
+module.exports = {
+  up: (queryInterface, Sequelize) =>
+    queryInterface.sequelize.transaction(async (transaction) => {
+      await queryInterface.changeColumn(
+        'surveys',
+        'start_date',
+        {
+          type: Sequelize.DataTypes.DATE,
+          allowNull: false,
+          defaultValue: Sequelize.fn('now'),
+        },
+        { transaction }
+      );
+      await queryInterface.changeColumn(
+        'surveys',
+        'end_date',
+        {
+          type: Sequelize.DataTypes.DATE,
+          allowNull: false,
+          defaultValue: Sequelize.fn('now'),
+        },
+        { transaction }
+      );
+    }),
+
+  down: () => Promise.resolve(),
+};
