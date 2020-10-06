255465d8055b0acba7ce85919e91988e0837fef9
Lukas Hroch <lukas.hroch@mrc-epid.cam.ac.uk>
2020-10-06 14:32:30 +0100

    move api to separate namespace to free the root

---
+++ api-server/src/routes/api/index.ts
@@ -0,0 +1,24 @@
+import { Request, Response, Router } from 'express';
+import authentication from './authentication';
+import admin from './admin';
+import password from './password';
+import feedback from './feedback';
+import foods from './foods';
+import surveys from './surveys';
+
+const router = Router();
+
+router.use(authentication);
+router.use('/password', password);
+
+router.use('/admin', admin);
+
+router.use('/feedback', feedback);
+router.use('/foods', foods);
+router.use('/surveys', surveys);
+
+router.all('*', (req: Request, res: Response): void => {
+  res.status(404).json('Route not found.');
+});
+
+export default router;
--- api-server/src/routes/index.ts
+++ api-server/src/routes/index.ts
@@ -1,23 +1,6 @@
-import { Request, Response } from 'express';
 import { AppLoader } from '@/loaders/loader';
-import authentication from './authentication';
-import admin from './admin';
-import password from './password';
-import feedback from './feedback';
-import foods from './foods';
-import surveys from './surveys';
+import api from './api';
 
 export default ({ app }: AppLoader): void => {
-  app.use(authentication);
-  app.use('/password', password);
-
-  app.use('/admin', admin);
-
-  app.use('/feedback', feedback);
-  app.use('/foods', foods);
-  app.use('/surveys', surveys);
-
-  app.all('*', (req: Request, res: Response): void => {
-    res.status(404).json('Route not found.');
-  });
+  app.use('/api', api);
 };
