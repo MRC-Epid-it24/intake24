2ae0ee7a1c17c1782a89d804ef1d9c96c592d958
Lukas Hroch <lukas.hroch@mrc-epid.cam.ac.uk>
2020-10-26 12:30:01 +0000

    keep survey app in subfolder to avoid PWA non-root issues

--- api-server/src/routes/index.ts
+++ api-server/src/routes/index.ts
@@ -5,9 +5,21 @@ import docs from './docs';
 import survey from './survey';
 
 export default ({ app }: AppLoader): void => {
+  // API
   app.use('/api', api);
 
+  // Survey application
+  app.use('/survey', survey);
+
+  // Admin Tool application
   app.use('/admin', admin);
+
+  // Documentation
   app.use('/docs', docs);
-  app.use(survey);
+
+  /*
+   * TODO: keep root for static / portal content
+   * redirect to survey app for now
+   */
+  app.get('*', (req, res) => res.redirect('/survey'));
 };
