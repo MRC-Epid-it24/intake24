a092e25a48d51cde366886317af8d2284bd71d76
Lukas Hroch <lukas.hroch@mrc-epid.cam.ac.uk>
2020-10-27 16:03:28 +0000

    type for task's job

--- api-server/src/db/models/system/task.ts
+++ api-server/src/db/models/system/task.ts
@@ -1,5 +1,5 @@
 import { Table, Column, CreatedAt, DataType, UpdatedAt } from 'sequelize-typescript';
-// import { JobType } from '@/jobs/job';
+import { JobType } from '@/jobs/job';
 import { Task as TaskAttributes } from '@common/types/models/system';
 import BaseModel from '../model';
 
@@ -25,7 +25,7 @@ export default class Task extends BaseModel<Task> implements TaskAttributes {
   @Column({
     allowNull: false,
   })
-  public job!: string;
+  public job!: JobType;
 
   @Column({
     allowNull: false,
