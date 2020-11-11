c07f638a4995016c358515da6a9e3712a05205be
Lukas Hroch <lukas.hroch@mrc-epid.cam.ac.uk>
2020-11-11 17:38:56 +0000

    DB: add missing updated_at column for jobs

---
+++ api-server/sequelize/system/migrations/20201111172715-jobs-updatedat.js
@@ -0,0 +1,38 @@
+module.exports = {
+  up: (queryInterface, Sequelize) =>
+    queryInterface.sequelize.transaction(async (transaction) => {
+      await queryInterface.addColumn(
+        'tools_tasks',
+        'updated_at',
+        {
+          type: Sequelize.DATE,
+          allowNull: true,
+        },
+        { transaction }
+      );
+
+      await queryInterface.sequelize.query(`UPDATE tools_tasks SET updated_at = completed_at`, {
+        transaction,
+      });
+
+      await queryInterface.sequelize.query(
+        `UPDATE tools_tasks SET updated_at = created_at WHERE updated_at IS NULL`,
+        { transaction }
+      );
+
+      await queryInterface.changeColumn(
+        'tools_tasks',
+        'updated_at',
+        {
+          type: Sequelize.DATE,
+          allowNull: false,
+        },
+        { transaction }
+      );
+    }),
+
+  down: (queryInterface, Sequelize) =>
+    queryInterface.sequelize.transaction(async (transaction) => {
+      await queryInterface.removeColumn('tools_tasks', 'updated_at', { transaction });
+    }),
+};
