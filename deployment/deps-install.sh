086af4c3a46f28e2188dd29cbbbd67454b0bb3fc
Ivan Poliakov <ivpolyakov@gmail.com>
2020-11-15 14:28:14 +0000

    Add redis configuration options to allow non-localhost connections

--- deployment/deps-install.sh
+++ deployment/deps-install.sh
@@ -3,5 +3,5 @@
 if [ "$#" -ne 1 ]; then
   echo "Instance name required"
 else
-  ansible-playbook -i ./instances/$1/hosts --extra-vars="instance=$1" ansible/deps-install.yml
+  ansible-playbook -i ./instances/$1/hosts -e @./instances/$1/redis/redis-config.json --extra-vars="instance=$1" ansible/deps-install.yml
 fi
