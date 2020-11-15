2096d20f418dcc1d855978cd60ddea413bfa91da
Ivan Poliakov <ivpolyakov@gmail.com>
2020-11-15 17:29:49 +0000

    Workaround for sequelize bulk insert bug

--- api-server/sequelize/system/migrations/20200804154202-seed-acl.js
+++ api-server/sequelize/system/migrations/20200804154202-seed-acl.js
@@ -15,7 +15,8 @@ module.exports = {
 
     await queryInterface.bulkInsert('roles', defaultRoles);
 
-    const surveyIds = await queryInterface.sequelize.query(`SELECT id FROM surveys;`);
+    const surveyIds = await queryInterface.sequelize.query(`SELECT id
+                                                            FROM surveys;`);
 
     const surveyPerms = surveyIds[0].reduce((acc, survey) => {
       ['respondent', 'staff', 'support'].forEach((item) => {
@@ -29,78 +30,84 @@ module.exports = {
       return acc;
     }, []);
 
-    const fdbIds = await queryInterface.sequelize.query(`SELECT id FROM locales;`);
+    const fdbIds = await queryInterface.sequelize.query(`SELECT id
+                                                         FROM locales;`);
 
     const fdbPerms = fdbIds[0].reduce((acc, fdb) => {
-      acc.push({ name: `fdbm/${fdb.id}`, display_name: `fdbm/${fdb.id}`, created_at, updated_at });
+      acc.push({name: `fdbm/${fdb.id}`, display_name: `fdbm/${fdb.id}`, created_at, updated_at});
       return acc;
     }, []);
 
     const resourcePerms = [
-      { name: 'acl', display_name: 'Access Control List' },
-      { name: 'globalsupport', display_name: 'Global Support' },
-      { name: 'surveyadmin', display_name: 'Survey Admin' },
-      { name: 'foodsadmin', display_name: 'Food DB Admin' },
-      { name: 'users-list', display_name: 'List users' },
-      { name: 'users-detail', display_name: 'Browse users' },
-      { name: 'users-create', display_name: 'Create users' },
-      { name: 'users-edit', display_name: 'Edit users' },
-      { name: 'users-delete', display_name: 'Delete users' },
-      { name: 'roles-list', display_name: 'List roles' },
-      { name: 'roles-detail', display_name: 'Browse roles' },
-      { name: 'roles-create', display_name: 'Create roles' },
-      { name: 'roles-edit', display_name: 'Edit roles' },
-      { name: 'roles-delete', display_name: 'Delete roles' },
-      { name: 'permissions-list', display_name: 'List permissions' },
-      { name: 'permissions-detail', display_name: 'Browse permissions' },
-      { name: 'permissions-create', display_name: 'Create permissions' },
-      { name: 'permissions-edit', display_name: 'Edit permissions' },
-      { name: 'permissions-delete', display_name: 'Delete permissions' },
-      { name: 'languages-list', display_name: 'List languages' },
-      { name: 'languages-detail', display_name: 'Browse languages' },
-      { name: 'languages-create', display_name: 'Create languages' },
-      { name: 'languages-edit', display_name: 'Edit languages' },
-      { name: 'languages-delete', display_name: 'Delete languages' },
-      { name: 'locales-list', display_name: 'List locales' },
-      { name: 'locales-detail', display_name: 'Browse locales' },
-      { name: 'locales-create', display_name: 'Create locales' },
-      { name: 'locales-edit', display_name: 'Edit locales' },
-      { name: 'locales-delete', display_name: 'Delete locales' },
-      { name: 'schemes-list', display_name: 'List schemes' },
-      { name: 'schemes-detail', display_name: 'Browse schemes' },
-      { name: 'schemes-create', display_name: 'Create schemes' },
-      { name: 'schemes-edit', display_name: 'Edit schemes' },
-      { name: 'schemes-delete', display_name: 'Delete schemes' },
-      { name: 'schemes-questions', display_name: 'Scheme questions' },
-      { name: 'surveys-list', display_name: 'List surveys' },
-      { name: 'surveys-detail', display_name: 'Browse surveys' },
-      { name: 'surveys-create', display_name: 'Create surveys' },
-      { name: 'surveys-edit', display_name: 'Edit surveys' },
-      { name: 'surveys-delete', display_name: 'Delete surveys' },
-      { name: 'surveys-mgmt', display_name: 'Survey management' },
-      { name: 'surveys-respondents', display_name: 'Survey respondents' },
-      { name: 'surveys-submissions', display_name: 'Survey submissions' },
-      { name: 'tasks-list', display_name: 'List tasks' },
-      { name: 'tasks-detail', display_name: 'Browse tasks' },
-      { name: 'tasks-create', display_name: 'Create tasks' },
-      { name: 'tasks-edit', display_name: 'Edit tasks' },
-      { name: 'tasks-delete', display_name: 'Delete tasks' },
-    ].map((permission) => ({ ...permission, created_at, updated_at }));
+      {name: 'acl', display_name: 'Access Control List'},
+      {name: 'globalsupport', display_name: 'Global Support'},
+      {name: 'surveyadmin', display_name: 'Survey Admin'},
+      {name: 'foodsadmin', display_name: 'Food DB Admin'},
+      {name: 'users-list', display_name: 'List users'},
+      {name: 'users-detail', display_name: 'Browse users'},
+      {name: 'users-create', display_name: 'Create users'},
+      {name: 'users-edit', display_name: 'Edit users'},
+      {name: 'users-delete', display_name: 'Delete users'},
+      {name: 'roles-list', display_name: 'List roles'},
+      {name: 'roles-detail', display_name: 'Browse roles'},
+      {name: 'roles-create', display_name: 'Create roles'},
+      {name: 'roles-edit', display_name: 'Edit roles'},
+      {name: 'roles-delete', display_name: 'Delete roles'},
+      {name: 'permissions-list', display_name: 'List permissions'},
+      {name: 'permissions-detail', display_name: 'Browse permissions'},
+      {name: 'permissions-create', display_name: 'Create permissions'},
+      {name: 'permissions-edit', display_name: 'Edit permissions'},
+      {name: 'permissions-delete', display_name: 'Delete permissions'},
+      {name: 'languages-list', display_name: 'List languages'},
+      {name: 'languages-detail', display_name: 'Browse languages'},
+      {name: 'languages-create', display_name: 'Create languages'},
+      {name: 'languages-edit', display_name: 'Edit languages'},
+      {name: 'languages-delete', display_name: 'Delete languages'},
+      {name: 'locales-list', display_name: 'List locales'},
+      {name: 'locales-detail', display_name: 'Browse locales'},
+      {name: 'locales-create', display_name: 'Create locales'},
+      {name: 'locales-edit', display_name: 'Edit locales'},
+      {name: 'locales-delete', display_name: 'Delete locales'},
+      {name: 'schemes-list', display_name: 'List schemes'},
+      {name: 'schemes-detail', display_name: 'Browse schemes'},
+      {name: 'schemes-create', display_name: 'Create schemes'},
+      {name: 'schemes-edit', display_name: 'Edit schemes'},
+      {name: 'schemes-delete', display_name: 'Delete schemes'},
+      {name: 'schemes-questions', display_name: 'Scheme questions'},
+      {name: 'surveys-list', display_name: 'List surveys'},
+      {name: 'surveys-detail', display_name: 'Browse surveys'},
+      {name: 'surveys-create', display_name: 'Create surveys'},
+      {name: 'surveys-edit', display_name: 'Edit surveys'},
+      {name: 'surveys-delete', display_name: 'Delete surveys'},
+      {name: 'surveys-mgmt', display_name: 'Survey management'},
+      {name: 'surveys-respondents', display_name: 'Survey respondents'},
+      {name: 'surveys-submissions', display_name: 'Survey submissions'},
+      {name: 'tasks-list', display_name: 'List tasks'},
+      {name: 'tasks-detail', display_name: 'Browse tasks'},
+      {name: 'tasks-create', display_name: 'Create tasks'},
+      {name: 'tasks-edit', display_name: 'Edit tasks'},
+      {name: 'tasks-delete', display_name: 'Delete tasks'},
+    ].map((permission) => ({...permission, created_at, updated_at}));
 
     await queryInterface.bulkInsert('permissions', resourcePerms.concat(surveyPerms, fdbPerms));
 
-    const roles = await queryInterface.sequelize.query(`SELECT id, name FROM roles;`);
-    const permissions = await queryInterface.sequelize.query(`SELECT id, name FROM permissions;`);
+    const roles = await queryInterface.sequelize.query(`SELECT id, name
+                                                        FROM roles;`);
+    const permissions = await queryInterface.sequelize.query(`SELECT id, name
+                                                              FROM permissions;`);
 
     const records = [];
     roles[0].forEach((role) => {
       permissions[0].reduce((acc, perm) => {
-        acc.push({ permission_id: perm.id, role_id: role.id, created_at, updated_at });
+        acc.push({permission_id: perm.id, role_id: role.id, created_at, updated_at});
         return acc;
       }, records);
     });
 
-    await queryInterface.bulkInsert('permission_role', records);
+    // Length check is a workaround for https://github.com/sequelize/sequelize/issues/11071
+    if (records.length > 0) {
+      await queryInterface.bulkInsert('permission_role', records);
+    }
 
     // Assign permissions based on legacy ACL system
     const roleMap = roles[0].reduce((acc, role) => {
@@ -114,7 +121,8 @@ module.exports = {
     }, {});
 
     const legacyRoles = await queryInterface.sequelize.query(
-      `SELECT user_id, role FROM user_roles;`
+      `SELECT user_id, role
+         FROM user_roles;`
     );
 
     const newPermissions = [];
@@ -143,8 +151,15 @@ module.exports = {
       });
     }
 
-    await queryInterface.bulkInsert('permission_user', newPermissions);
-    await queryInterface.bulkInsert('role_user', newRoles);
+    // Length check is a workaround for https://github.com/sequelize/sequelize/issues/11071
+
+    if (newPermissions.length > 0) {
+      await queryInterface.bulkInsert('permission_user', newPermissions);
+    }
+
+    if (newRoles.length > 0) {
+      await queryInterface.bulkInsert('role_user', newRoles);
+    }
   },
 
   down: async (queryInterface, Sequelize) => {
