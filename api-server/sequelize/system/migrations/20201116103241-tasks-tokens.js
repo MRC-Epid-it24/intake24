475b35cacdc38351d62eaaab795c8f4482996027
Lukas Hroch <lukas.hroch@mrc-epid.cam.ac.uk>
2020-11-16 11:22:03 +0000

    Purge refresh tokens task

---
+++ api-server/sequelize/system/migrations/20201116103241-tasks-tokens.js
@@ -0,0 +1,18 @@
+module.exports = {
+  up: (queryInterface, Sequelize) =>
+    queryInterface.sequelize.transaction(async (transaction) => {
+      await queryInterface.sequelize.query(
+        `INSERT INTO tasks("name", "job", "cron", "active", "description", "created_at", "updated_at")
+        VALUES ('Purge refresh tokens', 'PurgeRefreshTokens', '0 * * * *', true, 'Regularly purge expired refresh tokens', NOW(), NOW())`,
+        { transaction }
+      );
+    }),
+
+  down: (queryInterface, Sequelize) =>
+    queryInterface.sequelize.transaction(async (transaction) => {
+      await queryInterface.sequelize.query(
+        `DELETE FROM tasks WHERE "name" = 'Purge refresh tokens'`,
+        { transaction }
+      );
+    }),
+};
