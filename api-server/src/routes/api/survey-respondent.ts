671aab6100ef626c7055745d21f2933883653d52
Lukas Hroch <lukas.hroch@mrc-epid.cam.ac.uk>
2020-11-26 15:58:48 +0000

    fix not merged router params

--- api-server/src/routes/api/survey-respondent.ts
+++ api-server/src/routes/api/survey-respondent.ts
@@ -4,14 +4,14 @@ import surveyController from '@/http/controllers/survey.controller';
 import { isSurveyRespondent } from '@/http/middleware/acl';
 import { wrapAsync } from '@/util';
 
-const router = Router();
+const router = Router({ mergeParams: true });
 
 router.use(passport.authenticate('user', { session: false }));
 router.use(isSurveyRespondent());
 
-router.get('parameters', wrapAsync(surveyController.parameters));
-router.get('user-info', wrapAsync(surveyController.userInfo));
-router.get('request-help', wrapAsync(surveyController.requestHelp));
-router.get('submission', wrapAsync(surveyController.submission));
+router.get('/parameters', wrapAsync(surveyController.parameters));
+router.get('/user-info', wrapAsync(surveyController.userInfo));
+router.get('/request-help', wrapAsync(surveyController.requestHelp));
+router.get('/submission', wrapAsync(surveyController.submission));
 
 export default router;
