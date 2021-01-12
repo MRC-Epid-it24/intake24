9e4a6d16fd15a717a1ea6836e2bacf6e9c4153d8
Lukas Hroch <lukas.hroch@mrc-epid.cam.ac.uk>
2021-01-12 12:48:37 +0000

    allow not-specifying user for job run

---
+++ api-server/sequelize/system/migrations/20210112124510-jobs-userid.js
@@ -0,0 +1,27 @@
+module.exports = {
+  up: (queryInterface, Sequelize) =>
+    queryInterface.sequelize.transaction(async (transaction) => {
+      await queryInterface.changeColumn(
+        'tools_tasks',
+        'user_id',
+        {
+          type: Sequelize.INTEGER,
+          allowNull: true,
+        },
+        { transaction }
+      );
+    }),
+
+  down: (queryInterface, Sequelize) =>
+    queryInterface.sequelize.transaction(async (transaction) => {
+      await queryInterface.changeColumn(
+        'tools_tasks',
+        'user_id',
+        {
+          type: Sequelize.INTEGER,
+          allowNull: false,
+        },
+        { transaction }
+      );
+    }),
+};
