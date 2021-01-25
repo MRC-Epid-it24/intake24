16dbfd89e2bdce4403d82092e91c01f7ac3c4cf2
Lukas Hroch <lukas.hroch@mrc-epid.cam.ac.uk>
2021-01-25 11:36:02 +0000

    update express helpers types

--- api-server/src/http/controllers/controller.ts
+++ api-server/src/http/controllers/controller.ts
@@ -1,6 +1,12 @@
-import { RequestHandler } from 'express';
+import { Request, Response, NextFunction } from 'express';
 
-export type Controller<T extends string | number | symbol> = Record<T, RequestHandler>;
+export type AsyncRequestHandler = (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => Promise<void>;
+
+export type Controller<T extends string | number | symbol> = Record<T, AsyncRequestHandler>;
 
 export type CrudActions = 'list' | 'create' | 'store' | 'detail' | 'edit' | 'update' | 'destroy';
 
--- api-server/src/util/wrap-async.ts
+++
@@ -1,7 +0,0 @@
-import { Request, Response, NextFunction } from 'express';
-
-export default (fn: (req: Request, res: Response, next: NextFunction) => Promise<void>) => {
-  return (req: Request, res: Response, next: NextFunction): void => {
-    fn(req, res, next).catch(next);
-  };
-};
