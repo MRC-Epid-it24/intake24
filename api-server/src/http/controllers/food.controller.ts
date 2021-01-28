415672335baa4b197f5ced8e032bf2c09b70b50e
Dinislam Abdulgalimov <dinislam.abdulgalimov@monash.edu>
2021-01-28 22:07:28 +1100

    added portionsize, portionsizeparams and meal options to getFoodData

--- api-server/src/http/controllers/food.controller.ts
+++ api-server/src/http/controllers/food.controller.ts
@@ -1,5 +1,5 @@
 import { Request, Response } from 'express';
-import { FoodLocal, FoodCategory, Category } from '@/db/models/foods';
+import { FoodLocal, FoodCategory, FoodAttribute, PortionSizeMethod, PortionSizeMethodParameter } from '@/db/models/foods';
 import { NotFoundError } from '../errors';
 import { Controller } from './controller';
 import { getAllParentCategories } from '@api-server/db/raw/food-controller-sql';
@@ -18,14 +18,22 @@ export default (): FoodController => {
   const entry = async (req: Request, res: Response): Promise<void> => {
 
   /* Constructing response object */
-    const result: FoodData = {
-      categories: []
-    };
+    const result: FoodData = {};
     const { code, localeId } = req.params;
 
-    //1.Food data (Food Local)
+    //1.Food data (Food Local), portionSizeMethod and PortiobSizeMethod Parameters
     const food = await FoodLocal.findOne({
-      where: { localeId: localeId, foodCode: code }
+      where: { localeId: localeId, foodCode: code },
+      include: [{
+        model: PortionSizeMethod,
+        as: 'portionSizeMethods',
+        attributes: ['method', 'description', 'imageUrl', 'useForRecipes', 'conversionFactor'],
+        include: [{
+          model: PortionSizeMethodParameter,
+          as: 'parameters',
+          attributes: ['name', 'value']
+        }]
+      }]
     });
     if (!food || food == null) {
       res.status(404).json("Not Found!");
@@ -33,6 +41,9 @@ export default (): FoodController => {
     else {
       result.code = food.foodCode;
       result.localDescription = food.name;
+      result.portionSizeMethods = food.portionSizeMethods;
+      result.readyMealOption = null;
+      result.sameAsBeforeOption = null;
 
       //2. Food categories
       const foodCategories = await FoodCategory.findAll({
@@ -40,7 +51,6 @@ export default (): FoodController => {
         attributes: ['categoryCode']
       })
       result.categories = foodCategories.reduce<string[]>((acc, currentFoodCategory) => [...acc, currentFoodCategory.categoryCode], []);
-
 
       //3. Retrieving all parent categories of categories
       if (result.categories != null && result.categories.length !== 0) {
@@ -54,12 +64,20 @@ export default (): FoodController => {
         if (parentCat !== undefined && parentCat.length > 0) {
           const additionalCats = parentCat.reduce<string[]>((acc, currentCategory) => {
             // FIXME: TypeScript Error due to returned values.
+            // @ts-ignore
             return currentCategory ? [...acc, currentCategory.category_code] : acc;
           }, [])
           result.categories = [...result.categories, ...additionalCats];
         } else { console.log('No parent categories were found! ') }
       }
-      //4. Retrieving readyMealOption, sameAsBeforeOption, portionSizeMethods
+
+      //4. Retrieving readyMealOption, sameAsBeforeOption
+      const foodMealAndAsBeforeOptions = await FoodAttribute.findOne({
+        where: { foodCode: code },
+        attributes: ['sameAsBeforeOption', 'readyMealOption']
+      });
+      result.readyMealOption = foodMealAndAsBeforeOptions?.readyMealOption;
+      result.sameAsBeforeOption = foodMealAndAsBeforeOptions?.sameAsBeforeOption;
 
       //5. Retrieving associatedFoods and brands
 
