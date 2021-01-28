e8b31aebbdfd60a1f82fa5ac7b6b309e33d8c9e2
Dinislam Abdulgalimov <dinislam.abdulgalimov@monash.edu>
2021-01-28 11:13:38 +1100

    adding parent categories

--- api-server/src/http/controllers/food.controller.ts
+++ api-server/src/http/controllers/food.controller.ts
@@ -1,11 +1,16 @@
 import { Request, Response } from 'express';
-import { FoodLocal, FoodLocalList, FoodCategory } from '@/db/models/foods';
+import { FoodLocal, FoodCategory, Category } from '@/db/models/foods';
 import { NotFoundError } from '../errors';
 import { Controller } from './controller';
+import { getAllParentCategories } from '@api-server/db/raw/food-controller-sql';
+const { QueryTypes } = require('sequelize');
+
 
 type FoodData = {
   [key: string]: any // FIXME: use more specific types
 }
+
+type ParentCategories = undefined | [null | { category_code: string }]
 
 export type FoodController = Controller<
   'entry' | 'entryWithSource' | 'brands' | 'associatedFoods' | 'composition'
@@ -13,11 +18,12 @@ export type FoodController = Controller<
 
 export default (): FoodController => {
   const entry = async (req: Request, res: Response): Promise<void> => {
-    //constructing response object
+
+  /* Constructing response object */
     const result: FoodData = {};
     const { code, localeId } = req.params;
 
-    //1.Food data
+    //1.Food data (Food Local)
     const food = await FoodLocal.findOne({
       where: { localeId: localeId, foodCode: code }
     });
@@ -25,24 +31,38 @@ export default (): FoodController => {
       res.status(404).json("Not Found!");
     }
     else {
-      //2. Food categories and parent categories
+      result.code = food.foodCode;
+      result.localDescription = food.name;
+
+      //2. Food categories
       const foodCategories = await FoodCategory.findAll({
         where: { foodCode: code },
         attributes: ['categoryCode']
-      });
+      })
+      result.categories = foodCategories.reduce<string[]>((acc, currentFoodCategory) => [...acc, currentFoodCategory.categoryCode], []);
 
       //3. Retrieving all parent categories of categories
-
+      if (result.categories != null && result.categories.length !== 0) {
+        const parentCat = await FoodCategory.sequelize?.query(
+          getAllParentCategories,
+          {
+            replacements: { subcategory_code: result.categories },
+            raw: false,
+            type: QueryTypes.SELECT
+          }
+        );
+        if (parentCat !== undefined && parentCat[0]! == undefined) {
+          console.log("Found some parent categories! ");
+          console.log(parentCat[0]);
+          //const caties = parentCat.reduce<string[]>((acc, currentFoodCategory) => [...acc, currentFoodCategory.categoryCode], []);
+          //result.categories = [...result.categories, ...parentCat.category_code];
+        } else { console.log("parent undefined"); }
+      }
       //4. Retrieving readyMealOption, sameAsBeforeOption, portionSizeMethods
 
       //5. Retrieving associatedFoods and brands
 
       //6. Calculating caloriesPer100g
-
-      //TODO: More elegant way of bulk assignment of properties
-      result.code = food.foodCode;
-      result.localDescription = food.name;
-      result.categories = foodCategories.reduce<string[]>((acc, currentFoodCategory) => [...acc, currentFoodCategory.categoryCode], []);
 
       res.status(200).json(result);
     }
