f6b4ebedf97b54b406e2659b7374cb81a8ec6c92
Lukas Hroch <lukas.hroch@mrc-epid.cam.ac.uk>
2021-02-23 14:51:22 +0000

    fix(data-export UI): scale to full width on smaller screen

--- admin-client/src/views/surveys/DataExport.vue
+++ admin-client/src/views/surveys/DataExport.vue
@@ -5,7 +5,7 @@
         <v-card-title>{{ $t('surveys.data-export.title') }}</v-card-title>
         <v-card-text>
           <v-row>
-            <v-col cols="6">
+            <v-col cols="12" md="6">
               <v-row>
                 <v-col cols="12">
                   <v-dialog
