cd9fd6b28722a02f8cfd7de5e80cb26cb4370a09
Lukas Hroch <lukas.hroch@mrc-epid.cam.ac.uk>
2021-03-10 16:10:53 +0000

    feat(build): build improvements
    - remove cross-env dep and use in-built env webpack functionality
    - add progress bar

--- api-server/webpack.config.js
+++ api-server/webpack.config.js
@@ -2,59 +2,62 @@ const path = require('path');
 const nodeExternals = require('webpack-node-externals');
 const NodemonPlugin = require('nodemon-webpack-plugin');
 const TsconfigPathsPlugin = require('tsconfig-paths-webpack-plugin');
+const WebpackBar = require('webpackbar');
 
-const { NODE_ENV = 'development' } = process.env;
-const isDev = NODE_ENV === 'development';
+module.exports = (env) => {
+  const { NODE_ENV = 'development' } = env;
+  const isDev = NODE_ENV === 'development';
 
-const plugins = [];
+  const plugins = [new WebpackBar({ name: 'Server' })];
 
-if (isDev)
-  plugins.push(
-    new NodemonPlugin({
-      script: './dist/server.js',
-      watch: path.resolve('./dist'),
-      nodeArgs: ['--trace-warnings', '--inspect=5959'],
-    })
-  );
+  if (isDev)
+    plugins.push(
+      new NodemonPlugin({
+        script: './dist/server.js',
+        watch: path.resolve('./dist'),
+        nodeArgs: ['--trace-warnings', '--inspect=5959'],
+      })
+    );
 
-module.exports = {
-  context: __dirname,
-  entry: {
-    server: path.resolve('./src/index.ts'),
-    foodIndexBuilder: path.resolve('./src/food-index/workers/index-builder.ts'),
-  },
-  output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: '[name].js',
-  },
-  mode: NODE_ENV,
-  target: 'node',
-  watch: isDev,
-  watchOptions: { ignored: ['node_modules/**', 'public/**'] },
-  devtool: isDev ? 'source-map' : undefined,
-  optimization: {
-    minimize: false,
-  },
-  externals: [nodeExternals()],
-  resolve: {
-    extensions: ['.tsx', '.ts', '.js', '.json'],
-    plugins: [
-      new TsconfigPathsPlugin({
-        configFile: './tsconfig.json',
-        logLevel: 'info',
-        logInfoToStdOut: true,
-        extensions: ['.ts'],
-      }),
-    ],
-  },
-  module: {
-    rules: [
-      {
-        test: /\.tsx?$/,
-        use: 'ts-loader',
-        exclude: /node_modules/,
-      },
-    ],
-  },
-  plugins,
+  return {
+    context: __dirname,
+    entry: {
+      server: path.resolve('./src/index.ts'),
+      foodIndexBuilder: path.resolve('./src/food-index/workers/index-builder.ts'),
+    },
+    output: {
+      path: path.resolve(__dirname, 'dist'),
+      filename: '[name].js',
+    },
+    mode: NODE_ENV,
+    target: 'node',
+    watch: isDev,
+    watchOptions: { ignored: ['node_modules/**', 'public/**'] },
+    devtool: isDev ? 'source-map' : undefined,
+    optimization: {
+      minimize: false,
+    },
+    externals: [nodeExternals()],
+    resolve: {
+      extensions: ['.tsx', '.ts', '.js', '.json'],
+      plugins: [
+        new TsconfigPathsPlugin({
+          configFile: './tsconfig.json',
+          logLevel: 'info',
+          logInfoToStdOut: true,
+          extensions: ['.ts'],
+        }),
+      ],
+    },
+    module: {
+      rules: [
+        {
+          test: /\.tsx?$/,
+          use: 'ts-loader',
+          exclude: /node_modules/,
+        },
+      ],
+    },
+    plugins,
+  };
 };
