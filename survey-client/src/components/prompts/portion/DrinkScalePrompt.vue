d506fa8c01ee8235edda7aa8311732bd4e193e29
Tom Feltwell <tfeltwell@gmail.com>
2021-03-18 16:26:36 +0000

    feat(drinkScale): Fleshed out interface inc error checking
    - Fleshed out interface with buttons, slider, error checking.
    - Still needs image map similar to guideImage

--- survey-client/src/components/prompts/portion/DrinkScalePrompt.vue
+++ survey-client/src/components/prompts/portion/DrinkScalePrompt.vue
@@ -4,16 +4,38 @@
       <template v-slot:headerText>
         {{ localeDescription }}
       </template>
-      <v-expansion-panels>
+      <v-expansion-panels v-model="panelOpen">
         <v-expansion-panel>
-          <v-expansion-panel-header>
+          <v-expansion-panel-header disable-icon-rotate>
             {{ $t('portion.drinkScale.label', { food: localeDescription }) }}
+            <template v-slot:actions>
+              <v-icon color="success" v-if="selectedGuide">fas fa-fw fa-check</v-icon>
+              <v-icon color="error" v-if="!selectedGuide">fas fa-fw fa-exclamation</v-icon>
+            </template>
           </v-expansion-panel-header>
-          <v-expansion-panel-content>Image map here</v-expansion-panel-content>
+          <v-expansion-panel-content>
+            <v-row>
+              <v-col>
+                <v-img class="align-end" :src="selectionImageUrl" :aspect-ratio="16 / 9"></v-img>
+              </v-col>
+            </v-row>
+            <v-row>
+              <v-col>
+                <v-btn color="success" @click="onSelectGuide()">
+                  {{ $t('common.continue') }}
+                </v-btn>
+              </v-col>
+            </v-row>
+            
+          </v-expansion-panel-content>
         </v-expansion-panel>
         <v-expansion-panel>
-          <v-expansion-panel-header>
+          <v-expansion-panel-header disable-icon-rotate>
             {{ $t('portion.drinkScale.sliderLabel', { food: localeDescription }) }}
+            <template v-slot:actions>
+              <v-icon color="success" v-if="selectedQuantity">fas fa-fw fa-check</v-icon>
+              <v-icon color="error" v-if="!selectedQuantity">fas fa-fw fa-exclamation</v-icon>
+            </template>
           </v-expansion-panel-header>
           <v-expansion-panel-content>
             <v-row>
@@ -25,14 +47,13 @@
                   <v-row class="drink-slider">
                     <v-spacer></v-spacer>
                     <v-col xs="2" sm="1" class="d-flex justify-end mr-auto">
+                      <!-- TODO: Height of this -->
                       <v-slider
                         v-model="sliderValue"
                         :hint="$t('portion.drinkScale.lessFullButton')"
                         max="100"
                         min="0"
                         vertical
-                        thumb-label
-                        ticks
                         class="ma-0"
                       ></v-slider>
                     </v-col>
@@ -47,15 +68,22 @@
                 </v-img>
               </v-col>
             </v-row>
+            <v-row v-if="hasErrors">
+              <v-col>
+                <v-alert color="error" class="ma-0">
+                  <span v-for="(e, index) in errors" :key="index">{{ e }}</span>
+                </v-alert>
+              </v-col>
+            </v-row>
             <v-row>
               <v-col>
-                <v-btn>{{ $t('portion.drinkScale.lessFullButton') }}</v-btn>
+                <v-btn @click="modifySliderValue(-10)">{{ $t('portion.drinkScale.lessFullButton') }}</v-btn>
               </v-col>
               <v-col>
-                <v-btn>{{ $t('portion.drinkScale.moreFullButton') }}</v-btn>
+                <v-btn @click="modifySliderValue(10)">{{ $t('portion.drinkScale.moreFullButton') }}</v-btn>
               </v-col>
               <v-col>
-                <v-btn color="success">{{ $t('portion.drinkScale.confirmFullButton') }}</v-btn>
+                <v-btn color="success" @click="onSubmit()">{{ $t('portion.drinkScale.confirmFullButton') }}</v-btn>
               </v-col>
             </v-row>
           </v-expansion-panel-content>
@@ -96,7 +124,7 @@ export default (Vue as VueConstructor<Vue & Portion>).extend({
       errors: [] as string[],
       selectedGuide: false, // TODO: Model this correctly
       selectedQuantity: false,
-      panelOpen: 0,
+      panelOpen: 0, // ID which panel is open
       sliderValue: 75,
     };
   },
@@ -114,11 +142,40 @@ export default (Vue as VueConstructor<Vue & Portion>).extend({
   },
 
   methods: {
-    selectImage() {
-      this.selectedGuide = true;
+    modifySliderValue(value: number) {
+      // Handle upper and lower bounds, otherwise assign.
+      if (this.sliderValue + value > 100) {
+        this.sliderValue = 100;
+      } else if (this.sliderValue + value < 0) {
+        this.sliderValue = 0;
+      } else {
+        this.sliderValue += value;
+      }
+    },
+    onSelectGuide() {
+      this.selectedGuide = !this.selectedGuide;
+      this.panelOpen = 1;
+    },
+    clearErrors() {
+      this.errors = [];
+    },
+    isValid() {
+      if (this.selectedGuide) {
+        return true;
+      }
+      return false;
     },
     onSubmit() {
+      if (!this.isValid()) {
+        this.errors = [
+          this.getLocaleContent(this.validation.message) ??
+            (this.$t('portion.guideImage.validation.required') as string),
+        ];
+        return;
+      }
       this.$emit('DrinkScale completed');
+      this.selectedQuantity = true;
+      this.panelOpen = -1;
     },
   },
 });
