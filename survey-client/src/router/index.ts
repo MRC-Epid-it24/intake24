22b96b4885cb94c2334809e20a43c03e13f5d9ad
Dinislam Abdulgalimov <dinislam.abdulgalimov@monash.edu>
2021-04-01 15:09:16 +1100

    feat(MealList.vue): basic layout for meal list

--- survey-client/src/router/index.ts
+++ survey-client/src/router/index.ts
@@ -9,131 +9,131 @@ import { globalGuard, recallGuard } from './guards';
 Vue.use(VueRouter);
 
 export default (store: Store<RootState>): VueRouter => {
-  const beforeRecall = recallGuard(store);
+	const beforeRecall = recallGuard(store);
 
-  const routes: RouteConfig[] = [
-    {
-      path: '/',
-      name: 'home',
-      component: views.home,
-      meta: { module: 'public', title: 'common._' },
-    },
-    {
-      path: '/:surveyId',
-      name: 'login',
-      component: views.login,
-      meta: { module: 'login', title: 'login._' },
-    },
+	const routes: RouteConfig[] = [
+		{
+			path: '/',
+			name: 'home',
+			component: views.home,
+			meta: { module: 'public', title: 'common._' },
+		},
+		{
+			path: '/:surveyId',
+			name: 'login',
+			component: views.login,
+			meta: { module: 'login', title: 'login._' },
+		},
 
-    // TESTING Temporary route for testing portion size code before foods & meals are loaded
-    {
-      path: '/:surveyId/portion-test',
-      name: 'portion-test',
-      component: PortionTest,
-      meta: { module: 'public', title: 'portionTest._' },
-    },
+		// TESTING Temporary route for testing portion size code before foods & meals are loaded
+		{
+			path: '/:surveyId/portion-test',
+			name: 'portion-test',
+			component: PortionTest,
+			meta: { module: 'public', title: 'portionTest._' },
+		},
 
-    // Testing Meals List
-    {
-      path: '/:surveyId/recall/meals_test',
-      name: 'recall-meals_test',
-      component: views.recall.meals,
-      meta: { module: 'recall', title: 'recall._' },
-      props: true,
-      beforeEnter: beforeRecall,
-    },
-    {
-      path: '/:surveyId/recall/meals_test2',
-      name: 'recall-meals_test2',
-      component: views.recall.meals2,
-      meta: { module: 'recall', title: 'recall._' },
-      props: true,
-      beforeEnter: beforeRecall,
-    },
-    // END of TEST
+		// Testing Meals List
+		{
+			path: '/:surveyId/recall/meals_test',
+			name: 'recall-meals_test',
+			component: views.recall.meals,
+			meta: { module: 'recall', title: 'recall._' },
+			props: true,
+			beforeEnter: beforeRecall,
+		},
+		{
+			path: '/:surveyId/recall/meals_test2',
+			name: 'recall-meals_test2',
+			component: views.recall.meals2,
+			meta: { module: 'recall', title: 'recall._' },
+			props: true,
+			beforeEnter: beforeRecall,
+		},
+		// END of TEST
 
-    {
-      path: '/:surveyId/profile',
-      name: 'profile',
-      component: views.profile,
-      meta: { module: 'profile', title: 'profile._' },
-    },
-    {
-      path: '/:surveyId/generate-user',
-      name: 'generate-user',
-      component: views.generateUser,
-      meta: { module: 'public', title: 'login._' },
-    },
-    {
-      path: '/:surveyId/recall',
-      name: 'recall-entry',
-      component: views.recall.entry,
-      meta: { module: 'recall', title: 'recall._' },
-      props: true,
-      beforeEnter: beforeRecall,
-    },
-    {
-      path: '/:surveyId/recall/meals/:mealId/:questionId?',
-      name: 'recall-meals',
-      component: views.recall.flow,
-      meta: { module: 'recall', title: 'recall._', section: 'meals' },
-      props: true,
-      beforeEnter: beforeRecall,
-    },
-    {
-      path: '/:surveyId/recall/pre-meals/:questionId?',
-      name: 'recall-preMeals',
-      component: views.recall.flow,
-      meta: { module: 'recall', title: 'recall._', section: 'preMeals' },
-      props: true,
-      beforeEnter: beforeRecall,
-    },
-    {
-      path: '/:surveyId/recall/post-meals/:questionId?',
-      name: 'recall-postMeals',
-      component: views.recall.flow,
-      meta: { module: 'recall', title: 'recall._', section: 'postMeals' },
-      props: true,
-      beforeEnter: beforeRecall,
-    },
-    {
-      path: '/:surveyId/recall/submission/:questionId?',
-      name: 'recall-submission',
-      component: views.recall.flow,
+		{
+			path: '/:surveyId/profile',
+			name: 'profile',
+			component: views.profile,
+			meta: { module: 'profile', title: 'profile._' },
+		},
+		{
+			path: '/:surveyId/generate-user',
+			name: 'generate-user',
+			component: views.generateUser,
+			meta: { module: 'public', title: 'login._' },
+		},
+		{
+			path: '/:surveyId/recall',
+			name: 'recall-entry',
+			component: views.recall.entry,
+			meta: { module: 'recall', title: 'recall._' },
+			props: true,
+			beforeEnter: beforeRecall,
+		},
+		{
+			path: '/:surveyId/recall/meals/:mealId/:questionId?',
+			name: 'recall-meals',
+			component: views.recall.flow,
+			meta: { module: 'recall', title: 'recall._', section: 'meals' },
+			props: true,
+			beforeEnter: beforeRecall,
+		},
+		{
+			path: '/:surveyId/recall/pre-meals/:questionId?',
+			name: 'recall-preMeals',
+			component: views.recall.flow,
+			meta: { module: 'recall', title: 'recall._', section: 'preMeals' },
+			props: true,
+			beforeEnter: beforeRecall,
+		},
+		{
+			path: '/:surveyId/recall/post-meals/:questionId?',
+			name: 'recall-postMeals',
+			component: views.recall.flow,
+			meta: { module: 'recall', title: 'recall._', section: 'postMeals' },
+			props: true,
+			beforeEnter: beforeRecall,
+		},
+		{
+			path: '/:surveyId/recall/submission/:questionId?',
+			name: 'recall-submission',
+			component: views.recall.flow,
 
-      meta: { module: 'recall', title: 'recall._', section: 'submission' },
-      props: true,
-      beforeEnter: beforeRecall,
-    },
-    {
-      path: '/:surveyId/feedback',
-      name: 'feedback',
-      component: views.feedback,
-      meta: { module: 'feedback', title: 'feedback._' },
-    },
-    {
-      path: '/:surveyId/:token',
-      name: 'token',
-      component: views.login,
-      meta: { module: 'login', title: 'login._' },
-    },
+			meta: { module: 'recall', title: 'recall._', section: 'submission' },
+			props: true,
+			beforeEnter: beforeRecall,
+		},
+		{
+			path: '/:surveyId/feedback',
+			name: 'feedback',
+			component: views.feedback,
+			meta: { module: 'feedback', title: 'feedback._' },
+		},
+		{
+			path: '/:surveyId/:token',
+			name: 'token',
+			component: views.login,
+			meta: { module: 'login', title: 'login._' },
+		},
 
-    // Catch any unmatched routes
-    {
-      path: '*',
-      name: '404',
-      component: views.home,
-      meta: { module: 'public', title: 'common._' },
-    },
-  ];
+		// Catch any unmatched routes
+		{
+			path: '*',
+			name: '404',
+			component: views.home,
+			meta: { module: 'public', title: 'common._' },
+		},
+	];
 
-  const router = new VueRouter({
-    mode: 'history',
-    base: process.env.VUE_APP_BASE_URL ?? '/',
-    routes,
-  });
+	const router = new VueRouter({
+		mode: 'history',
+		base: process.env.VUE_APP_BASE_URL ?? '/',
+		routes,
+	});
 
-  router.beforeEach(globalGuard(store));
+	router.beforeEach(globalGuard(store));
 
-  return router;
+	return router;
 };
--- survey-client/src/views/recall/index.ts
+++ survey-client/src/views/recall/index.ts
@@ -4,8 +4,8 @@ import meals from './Meals_test.vue';
 import meals2 from './Meals_test_double.vue';
 
 export default {
-  entry,
-  flow,
-  meals,
-  meals2,
+	entry,
+	flow,
+	meals,
+	meals2,
 };
