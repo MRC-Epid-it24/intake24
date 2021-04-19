df9b7becdc28835b7b648825ca1a192433118c9d
Lukas Hroch <lukas.hroch@mrc-epid.cam.ac.uk>
2021-04-19 10:22:43 +0100

    refactor(admin): remove original path drawer in favor of vue implementation

--- admin-client/src/helpers/path-drawer/index.ts
+++
@@ -1,6 +0,0 @@
-export { default as PathDrawer } from './path-drawer';
-export { default as PathNode } from './path-node';
-export { default as PathSvg } from './path-svg';
-export { default as Path } from './path';
-
-export * from './path-constants';
