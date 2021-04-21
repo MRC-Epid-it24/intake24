4db4fbe4630c5b4bbc4e0d3f39390caf1add3298
Lukas Hroch <lukas.hroch@mrc-epid.cam.ac.uk>
2021-04-21 14:31:55 +0100

    refactor(db): mark v3 deprecated tabled

---
+++ api-server/sequelize/system/migrations/20210421142654-v3-deprecated-tables.js
@@ -0,0 +1,13 @@
+module.exports = {
+  up: (queryInterface) =>
+    queryInterface.sequelize.transaction(async (transaction) => {
+      await queryInterface.renameTable('user_permissions', 'v3_user_permissions', { transaction });
+      await queryInterface.renameTable('user_roles', 'v3_user_roles', { transaction });
+    }),
+
+  down: (queryInterface) =>
+    queryInterface.sequelize.transaction(async (transaction) => {
+      await queryInterface.renameTable('v3_user_permissions', 'user_permissions', { transaction });
+      await queryInterface.renameTable('v3_user_roles', 'user_roles', { transaction });
+    }),
+};
