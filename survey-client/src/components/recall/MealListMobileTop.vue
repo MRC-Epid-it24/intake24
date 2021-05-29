abe2114a1838332d593eaaa2b8cd27db3be39219
Dinislam Abdulgalimov <dinislam.abdulgalimov@monash.edu>
2021-05-29 15:41:25 +1000

    feat(survey): showing meal time on mobile meal tab

--- survey-client/src/components/recall/MealListMobileTop.vue
+++ survey-client/src/components/recall/MealListMobileTop.vue
@@ -9,34 +9,11 @@
         <v-tab v-for="meal in meals" :key="meal.name" @click="emitFoodsList(meal.foods)">
           {{ meal.name }}
           <v-icon x-small v-if="meal.time.length === 0">far fa-question-circle </v-icon>
+					<p v-else>{{meal.time}}</p>
         </v-tab>
       </v-tabs>
     </v-toolbar>
   </v-card>
-  <!-- <v-bottom-navigation absolute color="orange" horizontal grow>
-    <v-menu
-      :close-on-content-click="false"
-      top
-      v-for="(meal, index) in meals"
-      :key="meal.name"
-      :offset-y="offset"
-    >
-      <template v-slot:activator="{ on, attrs }">
-        <v-btn v-if="isMobile" v-bind="attrs" v-on="on" x-small text>
-          <span v-if="meal.time.length > 0">{{ index + 1 }}</span>
-          <v-icon v-else >far fa-question-circle </v-icon>
-        </v-btn>
-        <v-btn v-else-if="!isMobile" v-bind="attrs" v-on="on">
-          <span>{{ meal.name }}</span>
-          <v-icon v-if="meal.time.length === 0">far fa-question-circle </v-icon>
-        </v-btn>
-      </template>
-      <meal-item-mobile :name="meal.name" :time="meal.time" :foods="meal.foods"></meal-item-mobile>
-    </v-menu>
-    <v-btn color="secondary">
-      <span>Add </span>
-    </v-btn>
-  </v-bottom-navigation> -->
 </template>
 
 <script lang="ts">
