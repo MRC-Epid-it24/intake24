542a900b7da11513aab7e296c5f6e7b5b1c78f54
Lukas Hroch <lukas.hroch@mrc-epid.cam.ac.uk>
2021-07-20 11:20:03 +0100

    fix(lint): no exports in task-queue-handler

--- api-server/src/services/queues/index.ts
+++ api-server/src/services/queues/index.ts
@@ -1,7 +1,6 @@
 /* eslint-disable import/no-cycle */
 export * from './jobs-queue-handler';
 export * from './queue-handler';
-export * from './tasks-queue-handler';
 
 export { default as JobsQueueHandler } from './jobs-queue-handler';
 export { default as TasksQueueHandler } from './tasks-queue-handler';
