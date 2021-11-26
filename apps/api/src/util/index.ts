176ce6d2da3f365d15ec1cd44a5764d134108b8e
Lukas Hroch <lukas.hroch@mrc-epid.cam.ac.uk>
2021-11-26 15:39:12 +0000

    feat(util): promisify jwt verify util

--- apps/api/src/util/index.ts
+++ apps/api/src/util/index.ts
@@ -1,4 +1,5 @@
 export * from './express';
 export * from './date-time';
+export * as jwt from './jwt';
 export * from './memory';
 export * from './strings';
---
+++ apps/api/src/util/jwt.ts
@@ -0,0 +1,13 @@
+/* eslint-disable import/prefer-default-export */
+import jwt, { JwtPayload, Secret, VerifyOptions } from 'jsonwebtoken';
+
+export const verify = async <T = JwtPayload>(
+  token: string,
+  secret: Secret,
+  options?: VerifyOptions
+): Promise<T | undefined> =>
+  new Promise((resolve, reject) => {
+    jwt.verify(token, secret, options, (err, decoded) =>
+      err ? reject(err) : resolve(decoded as T | undefined)
+    );
+  });
