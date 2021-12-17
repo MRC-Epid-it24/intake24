bf2c91e50f36d992389ee9f65a6ab2fc30402660
Lukas Hroch <lukas.hroch@mrc-epid.cam.ac.uk>
2021-12-17 12:03:31 +0000

    fix(db): initialize food_portion_size_methods sequence

---
+++ apps/api/sequelize/foods/migrations/20211217104032-food_psm_sequence.js
@@ -0,0 +1,15 @@
+const { updateSequence } = require('../../utils.js');
+
+module.exports = {
+  up: (queryInterface) =>
+    queryInterface.sequelize.transaction(async (transaction) => {
+      await updateSequence('food_portion_size_methods', 'id', {
+        queryInterface,
+        transaction,
+      });
+    }),
+
+  down: () => {
+    throw new Error('This migration cannot be undone');
+  },
+};
