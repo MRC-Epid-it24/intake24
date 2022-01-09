0c1ab62f619003f0369bc6068a6651ddf39e0a0a
Lukas Hroch <lukas.hroch@mrc-epid.cam.ac.uk>
2022-01-09 20:31:04 +0000

    feat(i18n): improve locale switch logic

--- packages/ui/src/mixins/sets-language.ts
+++ packages/ui/src/mixins/sets-language.ts
@@ -2,15 +2,38 @@ import Vue from 'vue';
 
 export default Vue.extend({
   methods: {
-    async setLanguage(app: 'admin' | 'survey', languageId: string) {
-      const {
-        data: { messages, textDirection },
-      } = await this.$http.get(`i18n/${languageId}`, { params: { app } });
+    isRrlLocale(languageId: string): boolean {
+      return ['ar'].includes(languageId);
+    },
 
-      this.$root.$i18n.setLocaleMessage(languageId, messages);
-      this.$root.$i18n.locale = languageId;
-      this.$vuetify.rtl = textDirection === 'rtl';
-      this.$ls.set('language', languageId);
+    async setLanguage(app: 'admin' | 'survey', languageId: string) {
+      let languageSet = false;
+
+      // Fetch locale messages
+      try {
+        const {
+          data: { messages, textDirection },
+        } = await this.$http.get(`i18n/${languageId}`, { params: { app } });
+
+        if (Object.keys(messages).length) this.$root.$i18n.setLocaleMessage(languageId, messages);
+
+        this.$root.$i18n.locale = languageId;
+        this.$ls.set('language', languageId);
+        this.$vuetify.rtl = textDirection === 'rtl';
+
+        languageSet = true;
+      } catch {
+        //
+      }
+
+      if (languageSet) return;
+
+      // If language not updated, try local data
+      if (Object.keys(this.$root.$i18n.messages).includes(languageId)) {
+        this.$root.$i18n.locale = languageId;
+        this.$ls.set('language', languageId);
+        this.$vuetify.rtl = this.isRrlLocale(languageId);
+      }
     },
   },
 });
