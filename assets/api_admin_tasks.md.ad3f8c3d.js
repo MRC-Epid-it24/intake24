f7bb7b0357252540663fc2018569d5613a571adc
lukashroch <223967+lukashroch@users.noreply.github.com>
2022-01-17 15:53:00 +0000

    Deploying to ghp-docs from @ MRC-Epid-it24/intake24@62997f987822c036b09eb7e572bd979b0df3d8ec 🚀

---
+++ assets/api_admin_tasks.md.ad3f8c3d.js
@@ -0,0 +1,76 @@
+import{_ as a,c as e,o as s,a as n}from"./app.9160966e.js";const v='{"title":"Tasks","description":"","frontmatter":{},"headers":[{"level":2,"title":"Browse tasks","slug":"browse-tasks"},{"level":3,"title":"Request","slug":"request"},{"level":3,"title":"Response","slug":"response"},{"level":2,"title":"Create task","slug":"create-task"},{"level":3,"title":"Request","slug":"request-1"},{"level":3,"title":"Response","slug":"response-1"},{"level":2,"title":"Get task","slug":"get-task"},{"level":3,"title":"Request","slug":"request-2"},{"level":3,"title":"Response","slug":"response-2"},{"level":2,"title":"Update task","slug":"update-task"},{"level":3,"title":"Request","slug":"request-3"},{"level":3,"title":"Response","slug":"response-3"},{"level":2,"title":"Delete task","slug":"delete-task"},{"level":3,"title":"Request","slug":"request-4"},{"level":3,"title":"Response","slug":"response-4"},{"level":2,"title":"Run task","slug":"run-task"},{"level":3,"title":"Request","slug":"request-5"},{"level":3,"title":"Response","slug":"response-5"},{"level":2,"title":"Task references","slug":"task-references"},{"level":3,"title":"Request","slug":"request-6"},{"level":3,"title":"Response","slug":"response-6"}],"relativePath":"api/admin/tasks.md","lastUpdated":1642434740643}',t={},p=n(`<h1 id="tasks" tabindex="-1">Tasks <a class="header-anchor" href="#tasks" aria-hidden="true">#</a></h1><h2 id="browse-tasks" tabindex="-1">Browse tasks <a class="header-anchor" href="#browse-tasks" aria-hidden="true">#</a></h2><p>Browse paginated task list</p><h3 id="request" tabindex="-1">Request <a class="header-anchor" href="#request" aria-hidden="true">#</a></h3><div class="language-http"><pre><code>GET /api/admin/tasks
+    ?search={searchText}
+    &amp;page={page}
+    &amp;limit={limit}
+
+<span class="token header"><span class="token header-name keyword">Authorization</span><span class="token punctuation">:</span> <span class="token header-value">Bearer {accessToken}</span></span>
+<span class="token header"><span class="token header-name keyword">Content-Type</span><span class="token punctuation">:</span> <span class="token header-value">application/json</span></span>
+</code></pre></div><h3 id="response" tabindex="-1">Response <a class="header-anchor" href="#response" aria-hidden="true">#</a></h3><div class="language-json"><pre><code><span class="token number">200</span> OK
+
+<span class="token punctuation">{</span>
+    <span class="token property">&quot;data&quot;</span><span class="token operator">:</span> <span class="token punctuation">[</span><span class="token punctuation">{</span>...<span class="token punctuation">}</span><span class="token punctuation">]</span><span class="token punctuation">,</span>
+    <span class="token property">&quot;meta&quot;</span><span class="token operator">:</span> <span class="token punctuation">{</span>...<span class="token punctuation">}</span>
+<span class="token punctuation">}</span>
+</code></pre></div><h2 id="create-task" tabindex="-1">Create task <a class="header-anchor" href="#create-task" aria-hidden="true">#</a></h2><p>Create new task entry</p><h3 id="request-1" tabindex="-1">Request <a class="header-anchor" href="#request-1" aria-hidden="true">#</a></h3><div class="language-http"><pre><code>POST /api/admin/tasks
+
+<span class="token header"><span class="token header-name keyword">Authorization</span><span class="token punctuation">:</span> <span class="token header-value">Bearer {accessToken}</span></span>
+<span class="token header"><span class="token header-name keyword">Content-Type</span><span class="token punctuation">:</span> <span class="token header-value">application/json</span></span>
+<span class="token application-json">
+<span class="token punctuation">{</span>
+    <span class="token property">&quot;name&quot;</span><span class="token operator">:</span> string<span class="token punctuation">,</span>
+    <span class="token property">&quot;job&quot;</span><span class="token operator">:</span> string<span class="token punctuation">,</span>
+    <span class="token property">&quot;cron&quot;</span><span class="token operator">:</span> &#39;<span class="token number">0</span> * * * *&#39;<span class="token punctuation">,</span>
+    <span class="token property">&quot;active&quot;</span><span class="token operator">:</span> boolean<span class="token punctuation">,</span>
+    <span class="token property">&quot;description&quot;</span><span class="token operator">:</span> string | <span class="token null keyword">null</span>
+<span class="token punctuation">}</span>
+</span></code></pre></div><h3 id="response-1" tabindex="-1">Response <a class="header-anchor" href="#response-1" aria-hidden="true">#</a></h3><div class="language-json"><pre><code><span class="token number">201</span> Created
+
+<span class="token punctuation">{</span>
+    ...
+<span class="token punctuation">}</span>
+</code></pre></div><h2 id="get-task" tabindex="-1">Get task <a class="header-anchor" href="#get-task" aria-hidden="true">#</a></h2><p>Get task entry</p><h3 id="request-2" tabindex="-1">Request <a class="header-anchor" href="#request-2" aria-hidden="true">#</a></h3><div class="language-http"><pre><code>GET /api/admin/tasks/:taskId
+
+<span class="token header"><span class="token header-name keyword">Authorization</span><span class="token punctuation">:</span> <span class="token header-value">Bearer {accessToken}</span></span>
+<span class="token header"><span class="token header-name keyword">Content-Type</span><span class="token punctuation">:</span> <span class="token header-value">application/json</span></span>
+</code></pre></div><h3 id="response-2" tabindex="-1">Response <a class="header-anchor" href="#response-2" aria-hidden="true">#</a></h3><div class="language-json"><pre><code><span class="token number">200</span> OK
+
+<span class="token punctuation">{</span>
+    ...
+<span class="token punctuation">}</span>
+</code></pre></div><h2 id="update-task" tabindex="-1">Update task <a class="header-anchor" href="#update-task" aria-hidden="true">#</a></h2><p>Update task entry</p><h3 id="request-3" tabindex="-1">Request <a class="header-anchor" href="#request-3" aria-hidden="true">#</a></h3><div class="language-http"><pre><code>PUT /api/admin/tasks/:taskId
+
+<span class="token header"><span class="token header-name keyword">Authorization</span><span class="token punctuation">:</span> <span class="token header-value">Bearer {accessToken}</span></span>
+<span class="token header"><span class="token header-name keyword">Content-Type</span><span class="token punctuation">:</span> <span class="token header-value">application/json</span></span>
+<span class="token application-json">
+<span class="token punctuation">{</span>
+    <span class="token property">&quot;name&quot;</span><span class="token operator">:</span> string<span class="token punctuation">,</span>
+    <span class="token property">&quot;job&quot;</span><span class="token operator">:</span> string<span class="token punctuation">,</span>
+    <span class="token property">&quot;cron&quot;</span><span class="token operator">:</span> &#39;<span class="token number">0</span> * * * *&#39;<span class="token punctuation">,</span>
+    <span class="token property">&quot;active&quot;</span><span class="token operator">:</span> boolean<span class="token punctuation">,</span>
+    <span class="token property">&quot;description&quot;</span><span class="token operator">:</span> string | <span class="token null keyword">null</span>
+<span class="token punctuation">}</span>
+</span></code></pre></div><h3 id="response-3" tabindex="-1">Response <a class="header-anchor" href="#response-3" aria-hidden="true">#</a></h3><div class="language-json"><pre><code><span class="token number">200</span> OK
+
+<span class="token punctuation">{</span>
+    ...
+<span class="token punctuation">}</span>
+</code></pre></div><h2 id="delete-task" tabindex="-1">Delete task <a class="header-anchor" href="#delete-task" aria-hidden="true">#</a></h2><p>Delete task entry</p><h3 id="request-4" tabindex="-1">Request <a class="header-anchor" href="#request-4" aria-hidden="true">#</a></h3><div class="language-http"><pre><code>DELETE /api/admin/tasks/:taskId
+
+<span class="token header"><span class="token header-name keyword">Authorization</span><span class="token punctuation">:</span> <span class="token header-value">Bearer {accessToken}</span></span>
+<span class="token header"><span class="token header-name keyword">Content-Type</span><span class="token punctuation">:</span> <span class="token header-value">application/json</span></span>
+</code></pre></div><h3 id="response-4" tabindex="-1">Response <a class="header-anchor" href="#response-4" aria-hidden="true">#</a></h3><div class="language-json"><pre><code><span class="token number">204</span> No Content
+</code></pre></div><h2 id="run-task" tabindex="-1">Run task <a class="header-anchor" href="#run-task" aria-hidden="true">#</a></h2><p>Trigger task pushing into the job queue</p><h3 id="request-5" tabindex="-1">Request <a class="header-anchor" href="#request-5" aria-hidden="true">#</a></h3><div class="language-http"><pre><code>POST /api/admin/tasks/:taskId/run
+
+<span class="token header"><span class="token header-name keyword">Authorization</span><span class="token punctuation">:</span> <span class="token header-value">Bearer {accessToken}</span></span>
+<span class="token header"><span class="token header-name keyword">Content-Type</span><span class="token punctuation">:</span> <span class="token header-value">application/json</span></span>
+</code></pre></div><h3 id="response-5" tabindex="-1">Response <a class="header-anchor" href="#response-5" aria-hidden="true">#</a></h3><div class="language-json"><pre><code><span class="token number">200</span> OK
+</code></pre></div><h2 id="task-references" tabindex="-1">Task references <a class="header-anchor" href="#task-references" aria-hidden="true">#</a></h2><p>Get task references</p><h3 id="request-6" tabindex="-1">Request <a class="header-anchor" href="#request-6" aria-hidden="true">#</a></h3><div class="language-http"><pre><code>GET /api/admin/tasks/refs
+
+<span class="token header"><span class="token header-name keyword">Authorization</span><span class="token punctuation">:</span> <span class="token header-value">Bearer {accessToken}</span></span>
+<span class="token header"><span class="token header-name keyword">Content-Type</span><span class="token punctuation">:</span> <span class="token header-value">application/json</span></span>
+</code></pre></div><h3 id="response-6" tabindex="-1">Response <a class="header-anchor" href="#response-6" aria-hidden="true">#</a></h3><div class="language-json"><pre><code><span class="token number">200</span> OK
+
+<span class="token punctuation">{</span>
+    <span class="token property">&quot;jobs&quot;</span><span class="token operator">:</span> string<span class="token punctuation">[</span><span class="token punctuation">]</span>
+<span class="token punctuation">}</span>
+</code></pre></div>`,43),o=[p];function r(c,l,i,d,u,k){return s(),e("div",null,o)}var g=a(t,[["render",r]]);export{v as __pageData,g as default};
