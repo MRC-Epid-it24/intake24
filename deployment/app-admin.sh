0433905ece7ca728719ec16f7690273b27087165
Lukas Hroch <lukas.hroch@mrc-epid.cam.ac.uk>
2022-02-18 13:00:36 +0000

    feat(deployment): app deployment improvements
    - add basic deploy app info to shared config
    - reuse some of the same build logic
    - allow to deploy all apps in one go

--- deployment/app-admin.sh
+++ deployment/app-admin.sh
@@ -3,5 +3,5 @@
 if [ "$#" -ne 1 ]; then
   echo "Instance name required"
 else
-  ansible-playbook -i ./instances/$1/hosts -e @./instances/$1/app-admin/app.json --extra-vars="instance=$1" ansible/app-admin.yml
+  ansible-playbook -i ./instances/$1/hosts --extra-vars="instance=$1" ansible/app-admin.yml
 fi
--- deployment/app-api.sh
+++ deployment/app-api.sh
@@ -3,5 +3,5 @@
 if [ "$#" -ne 1 ]; then
   echo "Instance name required"
 else
-  ansible-playbook -i ./instances/$1/hosts -e @./instances/$1/app-api/app.json --extra-vars="instance=$1" ansible/app-api.yml
+  ansible-playbook -i ./instances/$1/hosts --extra-vars="instance=$1" ansible/app-api.yml
 fi
