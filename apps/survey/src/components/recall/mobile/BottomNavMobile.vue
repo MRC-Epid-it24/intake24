e3fe24aca3259218051e2ee77d024c7e008375cd
Dinislam <11454021+cheburan@users.noreply.github.com>
2022-02-22 10:03:50 +1100

    feat(survey): Base for reactive Continue Button based on Prompt answer and tempPormptAnswer state

--- apps/survey/src/components/recall/mobile/BottomNavMobile.vue
+++ apps/survey/src/components/recall/mobile/BottomNavMobile.vue
@@ -18,7 +18,7 @@
       <v-icon>$survey</v-icon>
     </v-btn>
 
-    <v-btn>
+    <v-btn :disabled="!this.continue">
       <span>Next question</span>
       <v-icon>$next</v-icon>
     </v-btn>
@@ -26,9 +26,10 @@
 </template>
 
 <script lang="ts">
+import { mapState } from 'pinia';
 import { useSurvey } from '@intake24/survey/stores';
 import { defineComponent } from '@vue/composition-api';
-import { mapState } from 'pinia';
+import { PromptAnswer } from '@intake24/common/types';
 
 export default defineComponent({
   name: 'BottomNavigationMobile',
@@ -37,21 +38,40 @@ export default defineComponent({
     bottomNavigation: Number,
   },
 
-  data() {
+  data(): {
+    activeItem: string;
+    color: string;
+    promptAnswer: PromptAnswer | null;
+    continue: boolean;
+  } {
     return {
       activeItem: 'meal',
       color: '#ffffff',
+      promptAnswer: null,
+      // TODO: change default to false once  all prompts will be changed to work with the tempPromptAnswer State.
+      continue: true,
     };
   },
 
   computed: {
-    ...mapState(useSurvey, ['selectedMeal', 'selectedFood']),
+    ...mapState(useSurvey, ['selectedMeal', 'selectedFood', 'currentTempPromptAnswer']),
   },
 
   methods: {
     onItemClick(tab: number) {
       if (!tab) this.$emit('navigation-item-click', this.$props.bottomNavigation);
       this.$emit('navigation-item-click', tab);
+    },
+  },
+  watch: {
+    currentTempPromptAnswer: {
+      immediate: true,
+      deep: true,
+      handler(value: PromptAnswer) {
+        this.promptAnswer = value;
+        // TODO: Switch it ON once all prompts will be changed to work with the tempPromptAnswer State.
+        // if (this.promptAnswer.response) this.continue = true;
+      },
     },
   },
 });
