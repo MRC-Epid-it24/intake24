b394bb3ff8114a691041b708d4eb559755364023
Ivan Poliakov <ivpolyakov@gmail.com>
2022-03-08 19:12:07 +0000

    (survey) Recall debug log

--- apps/survey/src/dynamic-recall/prompt-manager.ts
+++ apps/survey/src/dynamic-recall/prompt-manager.ts
@@ -9,6 +9,7 @@ import {
   standardPortionComplete,
 } from './portion-size-checks';
 import type { SurveyState } from '../stores';
+import { recallLog } from '../stores';
 
 const checkRecallNumber = (state: SurveyState, condition: Condition) => {
   if (state.user === null) {
@@ -104,41 +105,155 @@ const checkFoodStandardConditions = (
     throw new Error('This function must only be called when a food is selected');
 
   switch (prompt.component) {
-    case 'info-prompt':
-      return !selectedFood.flags.includes(`${prompt.id}-acknowledged`);
-
-    case 'food-search-prompt':
-      return selectedFood.type === 'free-text';
-
-    case 'portion-size-option-prompt':
-      return selectedFood.type === 'encoded-food' && selectedFood.portionSizeMethodIndex === null;
-
-    case 'as-served-prompt':
-      return (
-        portionSizeMethodSelected(selectedFood, 'as-served') &&
-        !asServedServingComplete(selectedFood)
-      );
-
-    case 'as-served-leftovers-prompt':
-      return (
-        portionSizeMethodSelected(selectedFood, 'as-served') &&
-        !asServedLeftoversComplete(selectedFood)
-      );
-
-    case 'guide-image-prompt':
-      return (
-        portionSizeMethodSelected(selectedFood, 'guide-image') && !guideImageComplete(selectedFood)
-      );
-
-    case 'standard-portion-prompt': {
-      return (
-        portionSizeMethodSelected(selectedFood, 'standard-portion') &&
-        !standardPortionComplete(selectedFood)
-      );
+    case 'info-prompt': {
+      if (selectedFood.flags.includes(`${prompt.id}-acknowledged`)) {
+        recallLog().promptCheck('info-prompt', false, `${prompt.id}-acknowledged flag set`);
+        return false;
+      }
+      recallLog().promptCheck('info-prompt', true, `${prompt.id}-acknowledged flag not set`);
+      return true;
     }
 
-    default:
-      return selectedFood.customPromptAnswers[prompt.id] === undefined;
+    case 'food-search-prompt': {
+      if (selectedFood.type === 'free-text') {
+        recallLog().promptCheck(
+          'food-search-prompt',
+          true,
+          'Selected food entry type is free-text'
+        );
+        return true;
+      }
+      recallLog().promptCheck(
+        'food-search-prompt',
+        false,
+        `Selected food entry type is ${selectedFood.type}`
+      );
+      return false;
+    }
+
+    case 'portion-size-option-prompt': {
+      if (selectedFood.type === 'encoded-food' && selectedFood.portionSizeMethodIndex === null) {
+        recallLog().promptCheck(
+          'portion-size-option-prompt',
+          true,
+          'Entry type is encoded-food and no portion size method is selected'
+        );
+        return true;
+      }
+      recallLog().promptCheck(
+        'portion-size-option-prompt',
+        false,
+        selectedFood.type === 'encoded-food'
+          ? 'Portion size method already selected'
+          : 'Entry type is not encoded-food'
+      );
+      return false;
+    }
+
+    case 'as-served-prompt': {
+      if (
+        portionSizeMethodSelected(selectedFood, 'as-served') &&
+        !asServedServingComplete(selectedFood)
+      ) {
+        recallLog().promptCheck(
+          'as-served-prompt',
+          true,
+          'As served portion size method selected but yet not complete'
+        );
+        return true;
+      }
+      recallLog().promptCheck(
+        'as-served-prompt',
+        false,
+        portionSizeMethodSelected(selectedFood, 'as-served')
+          ? 'As served portion size estimation not selected'
+          : 'As served portion size estimation already complete'
+      );
+      return false;
+    }
+
+    case 'as-served-leftovers-prompt': {
+      if (
+        portionSizeMethodSelected(selectedFood, 'as-served') &&
+        !asServedLeftoversComplete(selectedFood)
+      ) {
+        recallLog().promptCheck(
+          'as-served-leftovers-prompt',
+          true,
+          'As served portion size method selected but leftovers not yet estimated'
+        );
+        return true;
+      }
+      recallLog().promptCheck(
+        'as-served-leftovers-prompt',
+        false,
+        portionSizeMethodSelected(selectedFood, 'as-served')
+          ? 'As served leftovers estimation already complete'
+          : 'As served portion size estimation not selected'
+      );
+      return false;
+    }
+
+    case 'guide-image-prompt': {
+      if (
+        portionSizeMethodSelected(selectedFood, 'guide-image') &&
+        !guideImageComplete(selectedFood)
+      ) {
+        recallLog().promptCheck(
+          'guide-image-prompt',
+          true,
+          'Guide image selected but estimation not yet complete'
+        );
+        return true;
+      }
+      recallLog().promptCheck(
+        'guide-image-prompt',
+        false,
+        portionSizeMethodSelected(selectedFood, 'guide-image')
+          ? 'Guide image estimation already complete'
+          : 'Guide image estimation not selected'
+      );
+      return false;
+    }
+
+    case 'standard-portion-prompt': {
+      if (
+        portionSizeMethodSelected(selectedFood, 'standard-portion') &&
+        !standardPortionComplete(selectedFood)
+      ) {
+        recallLog().promptCheck(
+          'standard-portion-prompt',
+          true,
+          'Standard portion estimation selected but not yet complete'
+        );
+        return true;
+      }
+      recallLog().promptCheck(
+        'standard-portion-prompt',
+        false,
+        portionSizeMethodSelected(selectedFood, 'standard-portion')
+          ? 'Standard portion estimation already complete'
+          : 'Standard portion estimation not selected'
+      );
+      return false;
+    }
+
+    default: {
+      if (selectedFood.customPromptAnswers[prompt.id] === undefined) {
+        recallLog().promptCheck(
+          prompt.component,
+          true,
+          `Custom prompt answer ${prompt.id} is undefined`
+        );
+        return true;
+      }
+      recallLog().promptCheck(
+        prompt.component,
+        false,
+        `Custom prompt answer ${prompt.id} is defined`
+      );
+      return false;
+    }
   }
 };
 
