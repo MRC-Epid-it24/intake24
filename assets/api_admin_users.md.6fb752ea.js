6ea4596a834270f073c29fbcf2d829a5193130ca
lukashroch <223967+lukashroch@users.noreply.github.com>
2022-03-23 15:02:02 +0000

    Deploying to ghp-docs from @ MRC-Epid-it24/intake24@82e3f68a07dffc0e13eca851b187981bb8f5761a 🚀

---
+++ assets/api_admin_users.md.6fb752ea.js
@@ -0,0 +1,107 @@
+import{_ as s,c as n,o as a,a as e}from"./app.954531fb.js";const q='{"title":"Users","description":"","frontmatter":{},"headers":[{"level":2,"title":"Browse users","slug":"browse-users"},{"level":3,"title":"Request","slug":"request"},{"level":3,"title":"Response","slug":"response"},{"level":2,"title":"Create user","slug":"create-user"},{"level":3,"title":"Request","slug":"request-1"},{"level":3,"title":"Response","slug":"response-1"},{"level":2,"title":"Get user","slug":"get-user"},{"level":3,"title":"Request","slug":"request-2"},{"level":3,"title":"Response","slug":"response-2"},{"level":2,"title":"Update user","slug":"update-user"},{"level":3,"title":"Request","slug":"request-3"},{"level":3,"title":"Response","slug":"response-3"},{"level":2,"title":"Delete user","slug":"delete-user"},{"level":3,"title":"Request","slug":"request-4"},{"level":3,"title":"Response","slug":"response-4"},{"level":2,"title":"User references","slug":"user-references"},{"level":3,"title":"Request","slug":"request-5"},{"level":3,"title":"Response","slug":"response-5"}],"relativePath":"api/admin/users.md"}',t={},p=e(`<h1 id="users" tabindex="-1">Users <a class="header-anchor" href="#users" aria-hidden="true">#</a></h1><h2 id="browse-users" tabindex="-1">Browse users <a class="header-anchor" href="#browse-users" aria-hidden="true">#</a></h2><p>Browse paginated user list</p><h3 id="request" tabindex="-1">Request <a class="header-anchor" href="#request" aria-hidden="true">#</a></h3><div class="language-http"><pre><code>GET /api/admin/users
+    ?search={searchText}
+    &amp;page={page}
+    &amp;limit={limit}
+
+<span class="token header"><span class="token header-name keyword">Authorization</span><span class="token punctuation">:</span> <span class="token header-value">Bearer {accessToken}</span></span>
+<span class="token header"><span class="token header-name keyword">Content-Type</span><span class="token punctuation">:</span> <span class="token header-value">application/json</span></span>
+</code></pre></div><h3 id="response" tabindex="-1">Response <a class="header-anchor" href="#response" aria-hidden="true">#</a></h3><div class="language-json"><pre><code><span class="token number">200</span> OK
+
+<span class="token punctuation">{</span>
+    <span class="token property">&quot;data&quot;</span><span class="token operator">:</span> <span class="token punctuation">[</span><span class="token punctuation">{</span>...<span class="token punctuation">}</span><span class="token punctuation">]</span><span class="token punctuation">,</span>
+    <span class="token property">&quot;meta&quot;</span><span class="token operator">:</span> <span class="token punctuation">{</span>...<span class="token punctuation">}</span>
+<span class="token punctuation">}</span>
+</code></pre></div><h2 id="create-user" tabindex="-1">Create user <a class="header-anchor" href="#create-user" aria-hidden="true">#</a></h2><p>Create new user entry</p><h3 id="request-1" tabindex="-1">Request <a class="header-anchor" href="#request-1" aria-hidden="true">#</a></h3><div class="language-http"><pre><code>POST /api/admin/users
+
+<span class="token header"><span class="token header-name keyword">Authorization</span><span class="token punctuation">:</span> <span class="token header-value">Bearer {accessToken}</span></span>
+<span class="token header"><span class="token header-name keyword">Content-Type</span><span class="token punctuation">:</span> <span class="token header-value">application/json</span></span>
+<span class="token application-json">
+<span class="token punctuation">{</span>
+    <span class="token property">&quot;name&quot;</span><span class="token operator">:</span> string<span class="token punctuation">,</span>
+    <span class="token property">&quot;email&quot;</span><span class="token operator">:</span> string<span class="token punctuation">,</span>
+    <span class="token property">&quot;phone&quot;</span><span class="token operator">:</span> string<span class="token punctuation">,</span>
+    <span class="token property">&quot;emailNotifications&quot;</span><span class="token operator">:</span> boolean<span class="token punctuation">,</span>
+    <span class="token property">&quot;smsNotifications&quot;</span><span class="token operator">:</span> boolean<span class="token punctuation">,</span>
+    <span class="token property">&quot;multiFactorAuthentication&quot;</span><span class="token operator">:</span> boolean<span class="token punctuation">,</span>
+    <span class="token property">&quot;password&quot;</span><span class="token operator">:</span> string<span class="token punctuation">,</span>
+    <span class="token property">&quot;passwordConfirm&quot;</span><span class="token operator">:</span> string<span class="token punctuation">,</span>
+    <span class="token property">&quot;customFields&quot;</span><span class="token operator">:</span> <span class="token punctuation">[</span>
+        <span class="token punctuation">{</span>
+            <span class="token property">&quot;name&quot;</span><span class="token operator">:</span> string<span class="token punctuation">,</span>
+            <span class="token property">&quot;value&quot;</span><span class="token operator">:</span> string
+        <span class="token punctuation">}</span><span class="token punctuation">,</span>
+        ...
+    <span class="token punctuation">]</span><span class="token punctuation">,</span>
+    <span class="token property">&quot;permissions&quot;</span><span class="token operator">:</span> number<span class="token punctuation">[</span><span class="token punctuation">]</span><span class="token punctuation">,</span>
+    <span class="token property">&quot;roles&quot;</span><span class="token operator">:</span> number<span class="token punctuation">[</span><span class="token punctuation">]</span>
+<span class="token punctuation">}</span>
+</span></code></pre></div><h3 id="response-1" tabindex="-1">Response <a class="header-anchor" href="#response-1" aria-hidden="true">#</a></h3><div class="language-json"><pre><code><span class="token number">201</span> Created
+
+<span class="token punctuation">{</span>
+    ...
+<span class="token punctuation">}</span>
+</code></pre></div><h2 id="get-user" tabindex="-1">Get user <a class="header-anchor" href="#get-user" aria-hidden="true">#</a></h2><p>Get user entry</p><h3 id="request-2" tabindex="-1">Request <a class="header-anchor" href="#request-2" aria-hidden="true">#</a></h3><div class="language-http"><pre><code>GET /api/admin/users/:userId
+
+<span class="token header"><span class="token header-name keyword">Authorization</span><span class="token punctuation">:</span> <span class="token header-value">Bearer {accessToken}</span></span>
+<span class="token header"><span class="token header-name keyword">Content-Type</span><span class="token punctuation">:</span> <span class="token header-value">application/json</span></span>
+</code></pre></div><h3 id="response-2" tabindex="-1">Response <a class="header-anchor" href="#response-2" aria-hidden="true">#</a></h3><div class="language-json"><pre><code><span class="token number">200</span> OK
+
+<span class="token punctuation">{</span>
+    ...
+<span class="token punctuation">}</span>
+</code></pre></div><h2 id="update-user" tabindex="-1">Update user <a class="header-anchor" href="#update-user" aria-hidden="true">#</a></h2><p>Update user entry</p><h3 id="request-3" tabindex="-1">Request <a class="header-anchor" href="#request-3" aria-hidden="true">#</a></h3><div class="language-http"><pre><code>PUT /api/admin/users/:userId
+
+<span class="token header"><span class="token header-name keyword">Authorization</span><span class="token punctuation">:</span> <span class="token header-value">Bearer {accessToken}</span></span>
+<span class="token header"><span class="token header-name keyword">Content-Type</span><span class="token punctuation">:</span> <span class="token header-value">application/json</span></span>
+<span class="token application-json">
+<span class="token punctuation">{</span>
+    <span class="token property">&quot;name&quot;</span><span class="token operator">:</span> string<span class="token punctuation">,</span>
+    <span class="token property">&quot;email&quot;</span><span class="token operator">:</span> string<span class="token punctuation">,</span>
+    <span class="token property">&quot;phone&quot;</span><span class="token operator">:</span> string<span class="token punctuation">,</span>
+    <span class="token property">&quot;emailNotifications&quot;</span><span class="token operator">:</span> boolean<span class="token punctuation">,</span>
+    <span class="token property">&quot;smsNotifications&quot;</span><span class="token operator">:</span> boolean<span class="token punctuation">,</span>
+    <span class="token property">&quot;multiFactorAuthentication&quot;</span><span class="token operator">:</span> boolean<span class="token punctuation">,</span>
+    <span class="token property">&quot;customFields&quot;</span><span class="token operator">:</span> <span class="token punctuation">[</span>
+        <span class="token punctuation">{</span>
+            <span class="token property">&quot;name&quot;</span><span class="token operator">:</span> string<span class="token punctuation">,</span>
+            <span class="token property">&quot;value&quot;</span><span class="token operator">:</span> string
+        <span class="token punctuation">}</span>
+    <span class="token punctuation">]</span><span class="token punctuation">,</span>
+    <span class="token property">&quot;permissions&quot;</span><span class="token operator">:</span> number<span class="token punctuation">[</span><span class="token punctuation">]</span><span class="token punctuation">,</span>
+    <span class="token property">&quot;roles&quot;</span><span class="token operator">:</span> number<span class="token punctuation">[</span><span class="token punctuation">]</span>
+<span class="token punctuation">}</span>
+</span></code></pre></div><h3 id="response-3" tabindex="-1">Response <a class="header-anchor" href="#response-3" aria-hidden="true">#</a></h3><div class="language-json"><pre><code><span class="token number">200</span> OK
+
+<span class="token punctuation">{</span>
+    ...
+<span class="token punctuation">}</span>
+</code></pre></div><h2 id="delete-user" tabindex="-1">Delete user <a class="header-anchor" href="#delete-user" aria-hidden="true">#</a></h2><p>Delete user entry</p><h3 id="request-4" tabindex="-1">Request <a class="header-anchor" href="#request-4" aria-hidden="true">#</a></h3><div class="language-http"><pre><code>DELETE /api/admin/users/:userId
+
+<span class="token header"><span class="token header-name keyword">Authorization</span><span class="token punctuation">:</span> <span class="token header-value">Bearer {accessToken}</span></span>
+<span class="token header"><span class="token header-name keyword">Content-Type</span><span class="token punctuation">:</span> <span class="token header-value">application/json</span></span>
+</code></pre></div><h3 id="response-4" tabindex="-1">Response <a class="header-anchor" href="#response-4" aria-hidden="true">#</a></h3><div class="language-json"><pre><code><span class="token number">204</span> No Content
+</code></pre></div><h2 id="user-references" tabindex="-1">User references <a class="header-anchor" href="#user-references" aria-hidden="true">#</a></h2><p>Get user references</p><h3 id="request-5" tabindex="-1">Request <a class="header-anchor" href="#request-5" aria-hidden="true">#</a></h3><div class="language-http"><pre><code>GET /api/admin/users/refs
+
+<span class="token header"><span class="token header-name keyword">Authorization</span><span class="token punctuation">:</span> <span class="token header-value">Bearer {accessToken}</span></span>
+<span class="token header"><span class="token header-name keyword">Content-Type</span><span class="token punctuation">:</span> <span class="token header-value">application/json</span></span>
+</code></pre></div><h3 id="response-5" tabindex="-1">Response <a class="header-anchor" href="#response-5" aria-hidden="true">#</a></h3><div class="language-json"><pre><code><span class="token number">200</span> OK
+
+<span class="token punctuation">{</span>
+    <span class="token property">&quot;permissions&quot;</span><span class="token operator">:</span> <span class="token punctuation">[</span>
+        <span class="token punctuation">{</span>
+            <span class="token property">&quot;id&quot;</span><span class="token operator">:</span> string<span class="token punctuation">,</span>
+            <span class="token property">&quot;name&quot;</span><span class="token operator">:</span> string<span class="token punctuation">,</span>
+            <span class="token property">&quot;displayName&quot;</span><span class="token operator">:</span> string
+        <span class="token punctuation">}</span><span class="token punctuation">,</span>
+        ...
+    <span class="token punctuation">]</span><span class="token punctuation">,</span>
+    <span class="token property">&quot;roles&quot;</span><span class="token operator">:</span> <span class="token punctuation">[</span>
+        <span class="token punctuation">{</span>
+            <span class="token property">&quot;id&quot;</span><span class="token operator">:</span> string<span class="token punctuation">,</span>
+            <span class="token property">&quot;name&quot;</span><span class="token operator">:</span> string<span class="token punctuation">,</span>
+            <span class="token property">&quot;displayName&quot;</span><span class="token operator">:</span> string
+        <span class="token punctuation">}</span><span class="token punctuation">,</span>
+        ...
+    <span class="token punctuation">]</span>
+<span class="token punctuation">}</span>
+</code></pre></div>`,37),o=[p];function r(c,u,l,i,d,k){return a(),n("div",null,o)}var g=s(t,[["render",r]]);export{q as __pageData,g as default};
