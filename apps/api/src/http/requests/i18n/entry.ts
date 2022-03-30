a21b1f6df9bb5ae2dc9bad69a03c8e234076a442
Lukas Hroch <lukas.hroch@mrc-epid.cam.ac.uk>
2022-03-30 12:47:55 +0100

    fix(api): validate i18n languageId param

--- apps/api/src/http/requests/i18n/entry.ts
+++ apps/api/src/http/requests/i18n/entry.ts
@@ -3,6 +3,11 @@ import validate from '@intake24/api/http/requests/validate';
 
 export default validate(
   checkSchema({
+    languageId: {
+      in: ['params'],
+      errorMessage: `Invalid locale parameter.`,
+      isLocale: true,
+    },
     app: {
       in: ['query'],
       isIn: { options: [['admin', 'survey']] },
