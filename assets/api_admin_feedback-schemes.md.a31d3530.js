46f6b3c48f39b9792b06ec6fbe236fbcf68c23c2
lukashroch <223967+lukashroch@users.noreply.github.com>
2022-04-07 12:53:28 +0000

    Deploying to ghp-docs from @ MRC-Epid-it24/intake24@a00764424c77d3f95b9784028f9e33373d0e29ee 🚀

--- assets/api_admin_feedback-schemes.md.a31d3530.js
+++ assets/api_admin_feedback-schemes.md.a31d3530.js
@@ -1,4 +1,4 @@
-import{_ as e,c as a,o as s,a as n}from"./app.954531fb.js";const m='{"title":"Feedback schemes","description":"","frontmatter":{},"headers":[{"level":2,"title":"Browse feedback schemes","slug":"browse-feedback-schemes"},{"level":3,"title":"Request","slug":"request"},{"level":3,"title":"Response","slug":"response"},{"level":2,"title":"Create feedback scheme","slug":"create-feedback-scheme"},{"level":3,"title":"Request","slug":"request-1"},{"level":3,"title":"Response","slug":"response-1"},{"level":2,"title":"Get feedback scheme","slug":"get-feedback-scheme"},{"level":3,"title":"Request","slug":"request-2"},{"level":3,"title":"Response","slug":"response-2"},{"level":2,"title":"Update feedback scheme","slug":"update-feedback-scheme"},{"level":3,"title":"Request","slug":"request-3"},{"level":3,"title":"Response","slug":"response-3"},{"level":2,"title":"Delete feedback scheme","slug":"delete-feedback-scheme"},{"level":3,"title":"Request","slug":"request-4"},{"level":3,"title":"Response","slug":"response-4"},{"level":2,"title":"Feedback scheme references","slug":"feedback-scheme-references"},{"level":3,"title":"Request","slug":"request-5"},{"level":3,"title":"Response","slug":"response-5"},{"level":2,"title":"Copy feedback scheme","slug":"copy-feedback-scheme"},{"level":3,"title":"Request","slug":"request-6"},{"level":3,"title":"Response","slug":"response-6"}],"relativePath":"api/admin/feedback-schemes.md"}',t={},p=n(`<h1 id="feedback-schemes" tabindex="-1">Feedback schemes <a class="header-anchor" href="#feedback-schemes" aria-hidden="true">#</a></h1><h2 id="browse-feedback-schemes" tabindex="-1">Browse feedback schemes <a class="header-anchor" href="#browse-feedback-schemes" aria-hidden="true">#</a></h2><p>Browse paginated feedback scheme list</p><h3 id="request" tabindex="-1">Request <a class="header-anchor" href="#request" aria-hidden="true">#</a></h3><div class="language-http"><pre><code>GET /api/admin/feedback-schemes
+import{_ as e,c as a,o as s,a as n}from"./app.954531fb.js";const m='{"title":"Feedback schemes","description":"","frontmatter":{},"headers":[{"level":2,"title":"Browse feedback schemes","slug":"browse-feedback-schemes"},{"level":3,"title":"Request","slug":"request"},{"level":3,"title":"Response","slug":"response"},{"level":2,"title":"Create feedback scheme","slug":"create-feedback-scheme"},{"level":3,"title":"Request","slug":"request-1"},{"level":3,"title":"Response","slug":"response-1"},{"level":2,"title":"Get feedback scheme","slug":"get-feedback-scheme"},{"level":3,"title":"Request","slug":"request-2"},{"level":3,"title":"Response","slug":"response-2"},{"level":2,"title":"Update feedback scheme","slug":"update-feedback-scheme"},{"level":3,"title":"Request","slug":"request-3"},{"level":3,"title":"Response","slug":"response-3"},{"level":2,"title":"Delete feedback scheme","slug":"delete-feedback-scheme"},{"level":3,"title":"Request","slug":"request-4"},{"level":3,"title":"Response","slug":"response-4"},{"level":2,"title":"Copy feedback scheme","slug":"copy-feedback-scheme"},{"level":3,"title":"Request","slug":"request-5"},{"level":3,"title":"Response","slug":"response-5"},{"level":2,"title":"Feedback scheme references","slug":"feedback-scheme-references"},{"level":3,"title":"Request","slug":"request-6"},{"level":3,"title":"Response","slug":"response-6"}],"relativePath":"api/admin/feedback-schemes.md"}',t={},p=n(`<h1 id="feedback-schemes" tabindex="-1">Feedback schemes <a class="header-anchor" href="#feedback-schemes" aria-hidden="true">#</a></h1><h2 id="browse-feedback-schemes" tabindex="-1">Browse feedback schemes <a class="header-anchor" href="#browse-feedback-schemes" aria-hidden="true">#</a></h2><p>Browse paginated feedback scheme list</p><h3 id="request" tabindex="-1">Request <a class="header-anchor" href="#request" aria-hidden="true">#</a></h3><div class="language-http"><pre><code>GET /api/admin/feedback-schemes
     ?search={searchText}
     &amp;page={page}
     &amp;limit={limit}
@@ -61,11 +61,24 @@ import{_ as e,c as a,o as s,a as n}from"./app.954531fb.js";const m='{"title":"Fe
 <span class="token header"><span class="token header-name keyword">Authorization</span><span class="token punctuation">:</span> <span class="token header-value">Bearer {accessToken}</span></span>
 <span class="token header"><span class="token header-name keyword">Content-Type</span><span class="token punctuation">:</span> <span class="token header-value">application/json</span></span>
 </code></pre></div><h3 id="response-4" tabindex="-1">Response <a class="header-anchor" href="#response-4" aria-hidden="true">#</a></h3><div class="language-json"><pre><code><span class="token number">204</span> No Content
-</code></pre></div><h2 id="feedback-scheme-references" tabindex="-1">Feedback scheme references <a class="header-anchor" href="#feedback-scheme-references" aria-hidden="true">#</a></h2><p>Get feedback scheme references</p><h3 id="request-5" tabindex="-1">Request <a class="header-anchor" href="#request-5" aria-hidden="true">#</a></h3><div class="language-http"><pre><code>GET /api/admin/feedback-schemes/refs
+</code></pre></div><h2 id="copy-feedback-scheme" tabindex="-1">Copy feedback scheme <a class="header-anchor" href="#copy-feedback-scheme" aria-hidden="true">#</a></h2><p>Create a new copy of specified source feedback scheme</p><h3 id="request-5" tabindex="-1">Request <a class="header-anchor" href="#request-5" aria-hidden="true">#</a></h3><div class="language-http"><pre><code>POST /api/admin/feedback-schemes/:feedbackSchemeId/copy
 
 <span class="token header"><span class="token header-name keyword">Authorization</span><span class="token punctuation">:</span> <span class="token header-value">Bearer {accessToken}</span></span>
 <span class="token header"><span class="token header-name keyword">Content-Type</span><span class="token punctuation">:</span> <span class="token header-value">application/json</span></span>
-</code></pre></div><h3 id="response-5" tabindex="-1">Response <a class="header-anchor" href="#response-5" aria-hidden="true">#</a></h3><div class="language-json"><pre><code><span class="token number">200</span> OK
+<span class="token application-json">
+<span class="token punctuation">{</span>
+    <span class="token property">&quot;name&quot;</span><span class="token operator">:</span> string
+<span class="token punctuation">}</span>
+</span></code></pre></div><h3 id="response-5" tabindex="-1">Response <a class="header-anchor" href="#response-5" aria-hidden="true">#</a></h3><div class="language-json"><pre><code><span class="token number">200</span> OK
+
+<span class="token punctuation">{</span>
+    ...
+<span class="token punctuation">}</span>
+</code></pre></div><h2 id="feedback-scheme-references" tabindex="-1">Feedback scheme references <a class="header-anchor" href="#feedback-scheme-references" aria-hidden="true">#</a></h2><p>Get feedback scheme references</p><h3 id="request-6" tabindex="-1">Request <a class="header-anchor" href="#request-6" aria-hidden="true">#</a></h3><div class="language-http"><pre><code>GET /api/admin/feedback-schemes/refs
+
+<span class="token header"><span class="token header-name keyword">Authorization</span><span class="token punctuation">:</span> <span class="token header-value">Bearer {accessToken}</span></span>
+<span class="token header"><span class="token header-name keyword">Content-Type</span><span class="token punctuation">:</span> <span class="token header-value">application/json</span></span>
+</code></pre></div><h3 id="response-6" tabindex="-1">Response <a class="header-anchor" href="#response-6" aria-hidden="true">#</a></h3><div class="language-json"><pre><code><span class="token number">200</span> OK
 
 <span class="token punctuation">{</span>
     <span class="token property">&quot;languages&quot;</span><span class="token operator">:</span> <span class="token punctuation">[</span>
@@ -85,19 +98,5 @@ import{_ as e,c as a,o as s,a as n}from"./app.954531fb.js";const m='{"title":"Fe
         <span class="token punctuation">}</span><span class="token punctuation">,</span>
         ...
     <span class="token punctuation">]</span>
-<span class="token punctuation">}</span>
-</code></pre></div><h2 id="copy-feedback-scheme" tabindex="-1">Copy feedback scheme <a class="header-anchor" href="#copy-feedback-scheme" aria-hidden="true">#</a></h2><p>Create a new copy of specified source feedback scheme</p><h3 id="request-6" tabindex="-1">Request <a class="header-anchor" href="#request-6" aria-hidden="true">#</a></h3><div class="language-http"><pre><code>POST /api/admin/feedback-schemes/copy
-
-<span class="token header"><span class="token header-name keyword">Authorization</span><span class="token punctuation">:</span> <span class="token header-value">Bearer {accessToken}</span></span>
-<span class="token header"><span class="token header-name keyword">Content-Type</span><span class="token punctuation">:</span> <span class="token header-value">application/json</span></span>
-<span class="token application-json">
-<span class="token punctuation">{</span>
-    <span class="token property">&quot;sourceId&quot;</span><span class="token operator">:</span> string<span class="token punctuation">,</span>
-    <span class="token property">&quot;name&quot;</span><span class="token operator">:</span> string
-<span class="token punctuation">}</span>
-</span></code></pre></div><h3 id="response-6" tabindex="-1">Response <a class="header-anchor" href="#response-6" aria-hidden="true">#</a></h3><div class="language-json"><pre><code><span class="token number">200</span> OK
-
-<span class="token punctuation">{</span>
-    ...
 <span class="token punctuation">}</span>
 </code></pre></div>`,43),o=[p];function c(r,l,u,d,i,k){return s(),a("div",null,o)}var f=e(t,[["render",c]]);export{m as __pageData,f as default};
--- assets/api_admin_survey-schemes.md.0b5c44b9.js
+++ assets/api_admin_survey-schemes.md.0b5c44b9.js
@@ -1,4 +1,4 @@
-import{_ as s,c as a,o as n,a as e}from"./app.954531fb.js";const v='{"title":"Survey schemes","description":"","frontmatter":{},"headers":[{"level":2,"title":"Browse survey schemes","slug":"browse-survey-schemes"},{"level":3,"title":"Request","slug":"request"},{"level":3,"title":"Response","slug":"response"},{"level":2,"title":"Create survey scheme","slug":"create-survey-scheme"},{"level":3,"title":"Request","slug":"request-1"},{"level":3,"title":"Response","slug":"response-1"},{"level":2,"title":"Get survey scheme","slug":"get-survey-scheme"},{"level":3,"title":"Request","slug":"request-2"},{"level":3,"title":"Response","slug":"response-2"},{"level":2,"title":"Update survey scheme","slug":"update-survey-scheme"},{"level":3,"title":"Request","slug":"request-3"},{"level":3,"title":"Response","slug":"response-3"},{"level":2,"title":"Delete survey scheme","slug":"delete-survey-scheme"},{"level":3,"title":"Request","slug":"request-4"},{"level":3,"title":"Response","slug":"response-4"},{"level":2,"title":"Survey scheme references","slug":"survey-scheme-references"},{"level":3,"title":"Request","slug":"request-5"},{"level":3,"title":"Response","slug":"response-5"},{"level":2,"title":"Copy survey scheme","slug":"copy-survey-scheme"},{"level":3,"title":"Request","slug":"request-6"},{"level":3,"title":"Response","slug":"response-6"},{"level":2,"title":"Data export references","slug":"data-export-references"},{"level":3,"title":"Request","slug":"request-7"},{"level":3,"title":"Response","slug":"response-7"},{"level":2,"title":"Question templates","slug":"question-templates"},{"level":3,"title":"Request","slug":"request-8"},{"level":3,"title":"Response","slug":"response-8"}],"relativePath":"api/admin/survey-schemes.md"}',t={},p=e(`<h1 id="survey-schemes" tabindex="-1">Survey schemes <a class="header-anchor" href="#survey-schemes" aria-hidden="true">#</a></h1><h2 id="browse-survey-schemes" tabindex="-1">Browse survey schemes <a class="header-anchor" href="#browse-survey-schemes" aria-hidden="true">#</a></h2><p>Browse paginated survey scheme list</p><h3 id="request" tabindex="-1">Request <a class="header-anchor" href="#request" aria-hidden="true">#</a></h3><div class="language-http"><pre><code>GET /api/admin/survey-schemes
+import{_ as s,c as e,o as a,a as n}from"./app.954531fb.js";const v='{"title":"Survey schemes","description":"","frontmatter":{},"headers":[{"level":2,"title":"Browse survey schemes","slug":"browse-survey-schemes"},{"level":3,"title":"Request","slug":"request"},{"level":3,"title":"Response","slug":"response"},{"level":2,"title":"Create survey scheme","slug":"create-survey-scheme"},{"level":3,"title":"Request","slug":"request-1"},{"level":3,"title":"Response","slug":"response-1"},{"level":2,"title":"Get survey scheme","slug":"get-survey-scheme"},{"level":3,"title":"Request","slug":"request-2"},{"level":3,"title":"Response","slug":"response-2"},{"level":2,"title":"Update survey scheme","slug":"update-survey-scheme"},{"level":3,"title":"Request","slug":"request-3"},{"level":3,"title":"Response","slug":"response-3"},{"level":2,"title":"Delete survey scheme","slug":"delete-survey-scheme"},{"level":3,"title":"Request","slug":"request-4"},{"level":3,"title":"Response","slug":"response-4"},{"level":2,"title":"Copy survey scheme","slug":"copy-survey-scheme"},{"level":3,"title":"Request","slug":"request-5"},{"level":3,"title":"Response","slug":"response-5"},{"level":2,"title":"Survey scheme references","slug":"survey-scheme-references"},{"level":3,"title":"Request","slug":"request-6"},{"level":3,"title":"Response","slug":"response-6"},{"level":2,"title":"Data export references","slug":"data-export-references"},{"level":3,"title":"Request","slug":"request-7"},{"level":3,"title":"Response","slug":"response-7"},{"level":2,"title":"Question templates","slug":"question-templates"},{"level":3,"title":"Request","slug":"request-8"},{"level":3,"title":"Response","slug":"response-8"}],"relativePath":"api/admin/survey-schemes.md"}',t={},p=n(`<h1 id="survey-schemes" tabindex="-1">Survey schemes <a class="header-anchor" href="#survey-schemes" aria-hidden="true">#</a></h1><h2 id="browse-survey-schemes" tabindex="-1">Browse survey schemes <a class="header-anchor" href="#browse-survey-schemes" aria-hidden="true">#</a></h2><p>Browse paginated survey scheme list</p><h3 id="request" tabindex="-1">Request <a class="header-anchor" href="#request" aria-hidden="true">#</a></h3><div class="language-http"><pre><code>GET /api/admin/survey-schemes
     ?search={searchText}
     &amp;page={page}
     &amp;limit={limit}
@@ -59,11 +59,24 @@ import{_ as s,c as a,o as n,a as e}from"./app.954531fb.js";const v='{"title":"Su
 <span class="token header"><span class="token header-name keyword">Authorization</span><span class="token punctuation">:</span> <span class="token header-value">Bearer {accessToken}</span></span>
 <span class="token header"><span class="token header-name keyword">Content-Type</span><span class="token punctuation">:</span> <span class="token header-value">application/json</span></span>
 </code></pre></div><h3 id="response-4" tabindex="-1">Response <a class="header-anchor" href="#response-4" aria-hidden="true">#</a></h3><div class="language-json"><pre><code><span class="token number">204</span> No Content
-</code></pre></div><h2 id="survey-scheme-references" tabindex="-1">Survey scheme references <a class="header-anchor" href="#survey-scheme-references" aria-hidden="true">#</a></h2><p>Get survey scheme references</p><h3 id="request-5" tabindex="-1">Request <a class="header-anchor" href="#request-5" aria-hidden="true">#</a></h3><div class="language-http"><pre><code>GET /api/admin/survey-schemes/refs
+</code></pre></div><h2 id="copy-survey-scheme" tabindex="-1">Copy survey scheme <a class="header-anchor" href="#copy-survey-scheme" aria-hidden="true">#</a></h2><p>Create a new copy of specified source survey scheme</p><h3 id="request-5" tabindex="-1">Request <a class="header-anchor" href="#request-5" aria-hidden="true">#</a></h3><div class="language-http"><pre><code>POST /api/admin/survey-schemes/:surveySchemeId/copy
 
 <span class="token header"><span class="token header-name keyword">Authorization</span><span class="token punctuation">:</span> <span class="token header-value">Bearer {accessToken}</span></span>
 <span class="token header"><span class="token header-name keyword">Content-Type</span><span class="token punctuation">:</span> <span class="token header-value">application/json</span></span>
-</code></pre></div><h3 id="response-5" tabindex="-1">Response <a class="header-anchor" href="#response-5" aria-hidden="true">#</a></h3><div class="language-json"><pre><code><span class="token number">200</span> OK
+<span class="token application-json">
+<span class="token punctuation">{</span>
+    <span class="token property">&quot;name&quot;</span><span class="token operator">:</span> string
+<span class="token punctuation">}</span>
+</span></code></pre></div><h3 id="response-5" tabindex="-1">Response <a class="header-anchor" href="#response-5" aria-hidden="true">#</a></h3><div class="language-json"><pre><code><span class="token number">200</span> OK
+
+<span class="token punctuation">{</span>
+    ...
+<span class="token punctuation">}</span>
+</code></pre></div><h2 id="survey-scheme-references" tabindex="-1">Survey scheme references <a class="header-anchor" href="#survey-scheme-references" aria-hidden="true">#</a></h2><p>Get survey scheme references</p><h3 id="request-6" tabindex="-1">Request <a class="header-anchor" href="#request-6" aria-hidden="true">#</a></h3><div class="language-http"><pre><code>GET /api/admin/survey-schemes/refs
+
+<span class="token header"><span class="token header-name keyword">Authorization</span><span class="token punctuation">:</span> <span class="token header-value">Bearer {accessToken}</span></span>
+<span class="token header"><span class="token header-name keyword">Content-Type</span><span class="token punctuation">:</span> <span class="token header-value">application/json</span></span>
+</code></pre></div><h3 id="response-6" tabindex="-1">Response <a class="header-anchor" href="#response-6" aria-hidden="true">#</a></h3><div class="language-json"><pre><code><span class="token number">200</span> OK
 
 <span class="token punctuation">{</span>
     <span class="token property">&quot;languages&quot;</span><span class="token operator">:</span> <span class="token punctuation">[</span>
@@ -76,20 +89,6 @@ import{_ as s,c as a,o as n,a as e}from"./app.954531fb.js";const v='{"title":"Su
         ...
     <span class="token punctuation">]</span><span class="token punctuation">,</span>
     <span class="token property">&quot;templates&quot;</span><span class="token operator">:</span> PromptQuestion<span class="token punctuation">[</span><span class="token punctuation">]</span>
-<span class="token punctuation">}</span>
-</code></pre></div><h2 id="copy-survey-scheme" tabindex="-1">Copy survey scheme <a class="header-anchor" href="#copy-survey-scheme" aria-hidden="true">#</a></h2><p>Create a new copy of specified source survey scheme</p><h3 id="request-6" tabindex="-1">Request <a class="header-anchor" href="#request-6" aria-hidden="true">#</a></h3><div class="language-http"><pre><code>POST /api/admin/survey-schemes/copy
-
-<span class="token header"><span class="token header-name keyword">Authorization</span><span class="token punctuation">:</span> <span class="token header-value">Bearer {accessToken}</span></span>
-<span class="token header"><span class="token header-name keyword">Content-Type</span><span class="token punctuation">:</span> <span class="token header-value">application/json</span></span>
-<span class="token application-json">
-<span class="token punctuation">{</span>
-    <span class="token property">&quot;sourceId&quot;</span><span class="token operator">:</span> string<span class="token punctuation">,</span>
-    <span class="token property">&quot;name&quot;</span><span class="token operator">:</span> string
-<span class="token punctuation">}</span>
-</span></code></pre></div><h3 id="response-6" tabindex="-1">Response <a class="header-anchor" href="#response-6" aria-hidden="true">#</a></h3><div class="language-json"><pre><code><span class="token number">200</span> OK
-
-<span class="token punctuation">{</span>
-    ...
 <span class="token punctuation">}</span>
 </code></pre></div><h2 id="data-export-references" tabindex="-1">Data export references <a class="header-anchor" href="#data-export-references" aria-hidden="true">#</a></h2><p>Available fields for data export</p><h3 id="request-7" tabindex="-1">Request <a class="header-anchor" href="#request-7" aria-hidden="true">#</a></h3><div class="language-http"><pre><code>GET /api/admin/survey-schemes/:surveySchemeId/data-export
 
@@ -127,4 +126,4 @@ import{_ as s,c as a,o as n,a as e}from"./app.954531fb.js";const v='{"title":"Su
 <span class="token punctuation">[</span>
     <span class="token punctuation">{</span>...<span class="token punctuation">}</span>
 <span class="token punctuation">]</span>
-</code></pre></div>`,55),o=[p];function c(r,u,l,i,d,h){return n(),a("div",null,o)}var y=s(t,[["render",c]]);export{v as __pageData,y as default};
+</code></pre></div>`,55),o=[p];function c(r,u,l,i,d,h){return a(),e("div",null,o)}var y=s(t,[["render",c]]);export{v as __pageData,y as default};
