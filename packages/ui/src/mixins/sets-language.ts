4428d757928e15d3585de52722132c6ad34e75a9
Lukas Hroch <lukas.hroch@mrc-epid.cam.ac.uk>
2022-04-08 10:22:54 +0100

    fix(i18n): improve logic for local locale resolution

--- packages/ui/src/mixins/sets-language.ts
+++ packages/ui/src/mixins/sets-language.ts
@@ -4,12 +4,34 @@ import { useApp } from '../stores';
 
 export default defineComponent({
   methods: {
-    isRrlLocale(languageId: string): boolean {
+    fallbackLanguages(): string[] {
+      const { fallbackLocale } = this.$root.$i18n;
+      if (!fallbackLocale) return [];
+
+      if (typeof fallbackLocale === 'string') return [fallbackLocale];
+
+      return Array.isArray(fallbackLocale) ? fallbackLocale : Object.keys(fallbackLocale);
+    },
+
+    getLanguages(languageId: string): string[] {
+      return [
+        ...new Set(
+          [languageId, languageId.split('-')[0], ...this.fallbackLanguages()].filter(Boolean)
+        ),
+      ];
+    },
+
+    hasLanguage(languageId: string): boolean {
+      return this.$root.$i18n.availableLocales.includes(languageId);
+    },
+
+    isRrlLanguage(languageId: string): boolean {
       return ['ar'].includes(languageId);
     },
 
     async setLanguage(app: 'admin' | 'survey', languageId?: string) {
-      const language = languageId || navigator.language || navigator.userLanguage;
+      let language = languageId || useApp().lang;
+      let isRrlLanguage = this.isRrlLanguage(language);
 
       try {
         const {
@@ -18,19 +40,24 @@ export default defineComponent({
 
         if (Object.keys(messages).length) this.$root.$i18n.setLocaleMessage(id, messages);
 
-        this.updateAppWithLanguage(id, textDirection === 'rtl');
+        language = id;
+        isRrlLanguage = textDirection === 'rtl';
       } catch {
-        if (Object.keys(this.$root.$i18n.messages).includes(language))
-          this.updateAppWithLanguage(language);
+        //
+      }
+
+      for (const lang of this.getLanguages(language)) {
+        if (this.hasLanguage(lang)) {
+          this.updateAppWithLanguage(lang, isRrlLanguage);
+          break;
+        }
       }
     },
 
     updateAppWithLanguage(languageId: string, isRtl?: boolean) {
-      const appStore = useApp();
-
-      appStore.setLanguage(languageId);
+      useApp().setLanguage(languageId);
       this.$root.$i18n.locale = languageId;
-      this.$vuetify.rtl = typeof isRtl !== 'undefined' ? isRtl : this.isRrlLocale(languageId);
+      this.$vuetify.rtl = typeof isRtl !== 'undefined' ? isRtl : this.isRrlLanguage(languageId);
       document.querySelector('html')?.setAttribute('lang', languageId);
       this.$http.axios.defaults.headers.common['Accept-Language'] = languageId;
     },
