d62d569af707159ac91be89bc4883eaf47866040
Lukas Hroch <lukas.hroch@mrc-epid.cam.ac.uk>
2022-06-21 17:27:17 +0100

    ci(deployment): unify nginx sites configs & deployment

--- deployment/nginx-api.sh
+++ deployment/nginx-api.sh
@@ -3,5 +3,5 @@
 if [ "$#" -ne 1 ]; then
   echo "Instance name required"
 else
-  ansible-playbook -i ./instances/$1/hosts -e @./instances/$1/app-api/nginx-site.json --extra-vars="instance=$1" ansible/nginx-site-api.yml
+  ansible-playbook -i ./instances/$1/hosts --extra-vars="instance=$1" ansible/nginx-site-api.yml
 fi
