1984f2418af9e629d216987340f8c3e45ec96275
lukashroch <223967+lukashroch@users.noreply.github.com>
2022-06-22 13:28:04 +0000

    Deploying to ghp-docs from @ MRC-Epid-it24/intake24@9babf6413c8451c19f6e47fc2d4f44c609b6cebc 🚀

---
+++ assets/api_admin_locales.md.83475fab.js
@@ -0,0 +1,92 @@
+import{_ as a,c as n,o as s,a as e}from"./app.e79dd453.js";const g='{"title":"Locales","description":"","frontmatter":{},"headers":[{"level":2,"title":"Browse locales","slug":"browse-locales"},{"level":3,"title":"Request","slug":"request"},{"level":3,"title":"Response","slug":"response"},{"level":2,"title":"Create locale","slug":"create-locale"},{"level":3,"title":"Request","slug":"request-1"},{"level":3,"title":"Response","slug":"response-1"},{"level":2,"title":"Get locale","slug":"get-locale"},{"level":3,"title":"Request","slug":"request-2"},{"level":3,"title":"Response","slug":"response-2"},{"level":2,"title":"Update locale","slug":"update-locale"},{"level":3,"title":"Request","slug":"request-3"},{"level":3,"title":"Response","slug":"response-3"},{"level":2,"title":"Delete locale","slug":"delete-locale"},{"level":3,"title":"Request","slug":"request-4"},{"level":3,"title":"Response","slug":"response-4"},{"level":2,"title":"Locale references","slug":"locale-references"},{"level":3,"title":"Request","slug":"request-5"},{"level":3,"title":"Response","slug":"response-5"}],"relativePath":"api/admin/locales.md"}',t={},p=e(`<h1 id="locales" tabindex="-1">Locales <a class="header-anchor" href="#locales" aria-hidden="true">#</a></h1><h2 id="browse-locales" tabindex="-1">Browse locales <a class="header-anchor" href="#browse-locales" aria-hidden="true">#</a></h2><p>Browse paginated locale list</p><h3 id="request" tabindex="-1">Request <a class="header-anchor" href="#request" aria-hidden="true">#</a></h3><div class="language-http"><pre><code>GET /api/admin/locales
+    ?search={searchText}
+    &amp;page={page}
+    &amp;limit={limit}
+
+<span class="token header"><span class="token header-name keyword">Authorization</span><span class="token punctuation">:</span> <span class="token header-value">Bearer {accessToken}</span></span>
+<span class="token header"><span class="token header-name keyword">Content-Type</span><span class="token punctuation">:</span> <span class="token header-value">application/json</span></span>
+</code></pre></div><h3 id="response" tabindex="-1">Response <a class="header-anchor" href="#response" aria-hidden="true">#</a></h3><div class="language-json"><pre><code><span class="token number">200</span> OK
+
+<span class="token punctuation">{</span>
+    <span class="token property">&quot;data&quot;</span><span class="token operator">:</span> <span class="token punctuation">[</span><span class="token punctuation">{</span>...<span class="token punctuation">}</span><span class="token punctuation">]</span><span class="token punctuation">,</span>
+    <span class="token property">&quot;meta&quot;</span><span class="token operator">:</span> <span class="token punctuation">{</span>...<span class="token punctuation">}</span>
+<span class="token punctuation">}</span>
+</code></pre></div><h2 id="create-locale" tabindex="-1">Create locale <a class="header-anchor" href="#create-locale" aria-hidden="true">#</a></h2><p>Create new locale entry</p><h3 id="request-1" tabindex="-1">Request <a class="header-anchor" href="#request-1" aria-hidden="true">#</a></h3><div class="language-http"><pre><code>POST /api/admin/locales
+
+<span class="token header"><span class="token header-name keyword">Authorization</span><span class="token punctuation">:</span> <span class="token header-value">Bearer {accessToken}</span></span>
+<span class="token header"><span class="token header-name keyword">Content-Type</span><span class="token punctuation">:</span> <span class="token header-value">application/json</span></span>
+<span class="token application-json">
+<span class="token punctuation">{</span>
+    <span class="token property">&quot;id&quot;</span><span class="token operator">:</span> string<span class="token punctuation">,</span>
+    <span class="token property">&quot;englishName&quot;</span><span class="token operator">:</span> string<span class="token punctuation">,</span>
+    <span class="token property">&quot;localName&quot;</span><span class="token operator">:</span> string<span class="token punctuation">,</span>
+    <span class="token property">&quot;respondentLanguageId&quot;</span><span class="token operator">:</span> string<span class="token punctuation">,</span>
+    <span class="token property">&quot;adminLanguageId&quot;</span><span class="token operator">:</span> string<span class="token punctuation">,</span>
+    <span class="token property">&quot;countryFlagCode&quot;</span><span class="token operator">:</span> string<span class="token punctuation">,</span>
+    <span class="token property">&quot;prototypeLocaleId&quot;</span><span class="token operator">:</span> string | <span class="token null keyword">null</span><span class="token punctuation">,</span>
+    <span class="token property">&quot;textDirection&quot;</span><span class="token operator">:</span> string
+<span class="token punctuation">}</span>
+</span></code></pre></div><h3 id="response-1" tabindex="-1">Response <a class="header-anchor" href="#response-1" aria-hidden="true">#</a></h3><div class="language-json"><pre><code><span class="token number">201</span> Created
+
+<span class="token punctuation">{</span>
+    ...
+<span class="token punctuation">}</span>
+</code></pre></div><h2 id="get-locale" tabindex="-1">Get locale <a class="header-anchor" href="#get-locale" aria-hidden="true">#</a></h2><p>Get locale entry</p><h3 id="request-2" tabindex="-1">Request <a class="header-anchor" href="#request-2" aria-hidden="true">#</a></h3><div class="language-http"><pre><code>GET /api/admin/locales/:localeId
+
+<span class="token header"><span class="token header-name keyword">Authorization</span><span class="token punctuation">:</span> <span class="token header-value">Bearer {accessToken}</span></span>
+<span class="token header"><span class="token header-name keyword">Content-Type</span><span class="token punctuation">:</span> <span class="token header-value">application/json</span></span>
+</code></pre></div><h3 id="response-2" tabindex="-1">Response <a class="header-anchor" href="#response-2" aria-hidden="true">#</a></h3><div class="language-json"><pre><code><span class="token number">200</span> OK
+
+<span class="token punctuation">{</span>
+    ...
+<span class="token punctuation">}</span>
+</code></pre></div><h2 id="update-locale" tabindex="-1">Update locale <a class="header-anchor" href="#update-locale" aria-hidden="true">#</a></h2><p>Update locale entry</p><h3 id="request-3" tabindex="-1">Request <a class="header-anchor" href="#request-3" aria-hidden="true">#</a></h3><div class="language-http"><pre><code>PUT /api/admin/locales/:localeId
+
+<span class="token header"><span class="token header-name keyword">Authorization</span><span class="token punctuation">:</span> <span class="token header-value">Bearer {accessToken}</span></span>
+<span class="token header"><span class="token header-name keyword">Content-Type</span><span class="token punctuation">:</span> <span class="token header-value">application/json</span></span>
+<span class="token application-json">
+<span class="token punctuation">{</span>
+    <span class="token property">&quot;englishName&quot;</span><span class="token operator">:</span> string<span class="token punctuation">,</span>
+    <span class="token property">&quot;localName&quot;</span><span class="token operator">:</span> string<span class="token punctuation">,</span>
+    <span class="token property">&quot;respondentLanguageId&quot;</span><span class="token operator">:</span> string<span class="token punctuation">,</span>
+    <span class="token property">&quot;adminLanguageId&quot;</span><span class="token operator">:</span> string<span class="token punctuation">,</span>
+    <span class="token property">&quot;countryFlagCode&quot;</span><span class="token operator">:</span> string<span class="token punctuation">,</span>
+    <span class="token property">&quot;prototypeLocaleId&quot;</span><span class="token operator">:</span> string | <span class="token null keyword">null</span><span class="token punctuation">,</span>
+    <span class="token property">&quot;textDirection&quot;</span><span class="token operator">:</span> string
+<span class="token punctuation">}</span>
+</span></code></pre></div><h3 id="response-3" tabindex="-1">Response <a class="header-anchor" href="#response-3" aria-hidden="true">#</a></h3><div class="language-json"><pre><code><span class="token number">200</span> OK
+
+<span class="token punctuation">{</span>
+    ...
+<span class="token punctuation">}</span>
+</code></pre></div><h2 id="delete-locale" tabindex="-1">Delete locale <a class="header-anchor" href="#delete-locale" aria-hidden="true">#</a></h2><p>Delete locale entry</p><h3 id="request-4" tabindex="-1">Request <a class="header-anchor" href="#request-4" aria-hidden="true">#</a></h3><div class="language-http"><pre><code>DELETE /api/admin/locales/:localeId
+
+<span class="token header"><span class="token header-name keyword">Authorization</span><span class="token punctuation">:</span> <span class="token header-value">Bearer {accessToken}</span></span>
+<span class="token header"><span class="token header-name keyword">Content-Type</span><span class="token punctuation">:</span> <span class="token header-value">application/json</span></span>
+</code></pre></div><h3 id="response-4" tabindex="-1">Response <a class="header-anchor" href="#response-4" aria-hidden="true">#</a></h3><div class="language-json"><pre><code><span class="token number">204</span> No Content
+</code></pre></div><h2 id="locale-references" tabindex="-1">Locale references <a class="header-anchor" href="#locale-references" aria-hidden="true">#</a></h2><p>Get locale references</p><h3 id="request-5" tabindex="-1">Request <a class="header-anchor" href="#request-5" aria-hidden="true">#</a></h3><div class="language-http"><pre><code>GET /api/admin/locales/refs
+
+<span class="token header"><span class="token header-name keyword">Authorization</span><span class="token punctuation">:</span> <span class="token header-value">Bearer {accessToken}</span></span>
+<span class="token header"><span class="token header-name keyword">Content-Type</span><span class="token punctuation">:</span> <span class="token header-value">application/json</span></span>
+</code></pre></div><h3 id="response-5" tabindex="-1">Response <a class="header-anchor" href="#response-5" aria-hidden="true">#</a></h3><div class="language-json"><pre><code><span class="token number">200</span> OK
+
+<span class="token punctuation">{</span>
+    <span class="token property">&quot;languages&quot;</span><span class="token operator">:</span> <span class="token punctuation">[</span>
+        <span class="token punctuation">{</span>
+            <span class="token property">&quot;id&quot;</span><span class="token operator">:</span> string<span class="token punctuation">,</span>
+            <span class="token property">&quot;englishName&quot;</span><span class="token operator">:</span> string<span class="token punctuation">,</span>
+            <span class="token property">&quot;localName&quot;</span><span class="token operator">:</span> string<span class="token punctuation">,</span>
+            <span class="token property">&quot;countryFlagCode&quot;</span><span class="token operator">:</span> string
+        <span class="token punctuation">}</span><span class="token punctuation">,</span>
+        ...
+    <span class="token punctuation">]</span><span class="token punctuation">,</span>
+    <span class="token property">&quot;locales&quot;</span><span class="token operator">:</span> <span class="token punctuation">[</span>
+        <span class="token punctuation">{</span>
+            <span class="token property">&quot;id&quot;</span><span class="token operator">:</span> string<span class="token punctuation">,</span>
+            <span class="token property">&quot;englishName&quot;</span><span class="token operator">:</span> string<span class="token punctuation">,</span>
+            <span class="token property">&quot;localName&quot;</span><span class="token operator">:</span> string
+        <span class="token punctuation">}</span><span class="token punctuation">,</span>
+        ...
+    <span class="token punctuation">]</span>
+<span class="token punctuation">}</span>
+</code></pre></div>`,37),o=[p];function l(r,c,u,i,d,k){return s(),n("div",null,o)}var q=a(t,[["render",l]]);export{g as __pageData,q as default};
