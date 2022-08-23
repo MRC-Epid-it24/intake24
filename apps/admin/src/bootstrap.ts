4331d716dd797006db7eadcabd4999dbaa639ffa
Lukas Hroch <lukas.hroch@mrc-epid.cam.ac.uk>
2022-08-23 09:56:28 +0100

    refactor(admin): mixin barrel imports

--- apps/admin/src/bootstrap.ts
+++ apps/admin/src/bootstrap.ts
@@ -1,19 +1,16 @@
 import Vue from 'vue';
 import { Intersect, Resize } from 'vuetify/lib/directives';
 
-import authMixin from './mixins/auth';
-import isMobile from './mixins/is-mobile';
-import loadingMixin from './mixins/loading';
-import moduleMixin from './mixins/module';
+import { auth, isMobile, loading, module } from './mixins';
 
 //@ts-expect-error Vue2.7 types
-Vue.mixin(authMixin);
+Vue.mixin(auth);
 //@ts-expect-error Vue2.7 types
 Vue.mixin(isMobile);
 //@ts-expect-error Vue2.7 types
-Vue.mixin(loadingMixin);
+Vue.mixin(loading);
 //@ts-expect-error Vue2.7 types
-Vue.mixin(moduleMixin);
+Vue.mixin(module);
 
 Vue.directive('intersect', Intersect);
 Vue.directive('resize', Resize);
--- apps/admin/src/mixins/index.ts
+++ apps/admin/src/mixins/index.ts
@@ -1,6 +1,7 @@
 export { default as auth } from './auth';
 export { default as formatsDateTime } from './formats-date-time';
 export { default as handlesLoading } from './handles-loading';
+export { default as isMobile } from './is-mobile';
 export { default as loading } from './loading';
 export { default as module } from './module';
 export { default as resource } from './resource';
