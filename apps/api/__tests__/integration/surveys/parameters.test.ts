1298f69fe88271bada83a5d6c21ed648cc18f732
Ivan Poliakov <ivpolyakov@gmail.com>
2022-10-05 23:18:19 +0100

    fix(test): survey parameters: new fields

--- apps/api/__tests__/integration/surveys/parameters.test.ts
+++ apps/api/__tests__/integration/surveys/parameters.test.ts
@@ -39,6 +39,8 @@ export default () => {
       'numberOfSubmissionsForFeedback',
       'storeUserSessionOnServer',
       'suspensionReason',
+      'searchSortingAlgorithm',
+      'searchMatchScoreWeight',
     ]);
   });
 };
