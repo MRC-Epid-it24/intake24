b1fddb2b11046a11f0bceb4e137bb641b7069502
Ivan Poliakov <ivpolyakov@gmail.com>
2022-10-12 12:57:29 +0100

    fix(api): fixed food ranking caching

--- apps/api/src/food-index/ranking/fixed-ranking.ts
+++ apps/api/src/food-index/ranking/fixed-ranking.ts
@@ -5,29 +5,43 @@ import { FixedFoodRanking } from '@intake24/db';
 
 const cache = new NodeCache({ stdTTL: 600, checkperiod: 600 });
 
-function key(localeId: string, foodCode: string): string {
+function toKey(localeId: string, foodCode: string): string {
   return `${localeId}.${foodCode}`;
 }
 
-export async function getFixedRanking(localeId: string, foods: string[]): Promise<RankingData> {
-  const keys = foods.map((code) => key(localeId, code));
+function toCode(localeId: string, key: string): string {
+  return key.substring(localeId.length + 1);
+}
 
-  const ranking = cache.mget<number>(keys);
+function mapKeys<T>(obj: { [k: string]: T }, fn: (value: string) => string): { [k: string]: T } {
+  const entries = Object.entries(obj);
+  return Object.fromEntries(entries.map((entry) => [fn(entry[0]), entry[1]]));
+}
+
+export async function getFixedRanking(localeId: string, foods: string[]): Promise<RankingData> {
+  const keys = foods.map((code) => toKey(localeId, code));
+
+  const ranking = mapKeys(cache.mget<number>(keys), (k) => toCode(localeId, k));
 
   const codesToFetch = foods.filter((code) => !ranking[code]);
 
-  const rows = await FixedFoodRanking.findAll({
-    attributes: ['foodCode', 'rank'],
-    where: { localeId, foodCode: codesToFetch },
-  });
+  if (codesToFetch.length > 0) {
+    const rows = await FixedFoodRanking.findAll({
+      attributes: ['foodCode', 'rank'],
+      where: { localeId, foodCode: codesToFetch },
+    });
 
-  const newCacheEntries = rows.map((row) => ({ key: key(localeId, row.foodCode), val: row.rank }));
+    const newCacheEntries = rows.map((row) => ({
+      key: toKey(localeId, row.foodCode),
+      val: row.rank,
+    }));
 
-  cache.mset(newCacheEntries);
+    cache.mset(newCacheEntries);
 
-  rows.forEach((row) => {
-    ranking[row.foodCode] = row.rank;
-  });
+    rows.forEach((row) => {
+      ranking[row.foodCode] = row.rank;
+    });
+  }
 
   return ranking;
 }
