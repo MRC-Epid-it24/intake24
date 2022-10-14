f23f8a15148e11a9955e166d621ff7a6ef6aff0a
Ivan Poliakov <ivpolyakov@gmail.com>
2022-10-14 18:16:33 +0100

    feat(test): workaround for issues with IPv6/IPv4 dual-stacking
    The testing library (supertest) will start the application on a randomly allocated ("ephemeral")
    port on localhost, and Node will bind to the IPv6 localhost ("::") by default if it is
    available.
    
    Supertest then uses a hard-coded hostname in IPv4 format ("127.0.0.1"). Normally operating
    systems support a feature called "dual-stacking" whereby the IPv4 localhost address is
    automatically translated to the IPv6 host, and apps trying to connect using IPv4 should be
    able to connect to the IPv6 socket.
    
    If this feature is unavailable or not able to be used for some reason, the test suite will
    fail to connect to the local app instance and will fail with a timeout.
    
    If the INTAKE24_TEST_SET_HOST environment variable is defined, the test suite will start the
    application using the port and hostname configured in the environment file instead of relying
    on supertest defaults. It is possible to pass a server reference instead of the app reference
    to supertest functions to achieve this.
    
    By setting the hostname to the IPv4 localhost ("127.0.0.1") in the environment file the
    application will bind to an IPv4 socket thus avoiding the potential IPv6/IPv4 mismatch.

--- apps/api/__tests__/integration/helpers/integration-suite.ts
+++ apps/api/__tests__/integration/helpers/integration-suite.ts
@@ -1,4 +1,5 @@
 import type { Express } from 'express';
+import type { Server } from 'http';
 import fs from 'fs-extra';
 
 import app from '@intake24/api/app';
@@ -31,7 +32,7 @@ class IntegrationSuite {
 
   public session;
 
-  public app!: Express;
+  public app!: Express | Server;
 
   public data!: MockData;
 
@@ -64,6 +65,41 @@ class IntegrationSuite {
 
     // Boot up application with all dependencies
     this.app = await app({ config: this.config, logger: this.logger });
+
+    /*
+    Workaround for issues with IPv6/IPv4 "dual-stacking":
+
+    The testing library (supertest) will start the application on a randomly allocated ("ephemeral")
+    port on localhost, and Node will bind to the IPv6 localhost ("::") by default if it is
+    available.
+
+    Supertest then uses a hard-coded hostname in IPv4 format ("127.0.0.1"). Normally operating
+    systems support a feature called "dual-stacking" whereby the IPv4 localhost address is
+    automatically translated to the IPv6 host, and apps trying to connect using IPv4 should be
+    able to connect to the IPv6 socket.
+
+    If this feature is unavailable or not able to be used for some reason, the test suite will
+    fail to connect to the local app instance and will fail with a timeout.
+
+    If the INTAKE24_TEST_SET_HOST environment variable is defined, the test suite will start the
+    application using the port and hostname configured in the environment file instead of relying
+    on supertest defaults. It is possible to pass a server reference instead of the app reference
+    to supertest functions to achieve this.
+
+    By setting the hostname to the IPv4 localhost ("127.0.0.1") in the environment file the
+    application will bind to an IPv4 socket thus avoiding the potential IPv6/IPv4 mismatch.
+    */
+
+    if (process.env['INTAKE24_TEST_SET_HOST']) {
+      let listenResolve: (() => void) | undefined;
+      const listenPromise = new Promise<void>((resolve) => {
+        listenResolve = resolve;
+      });
+
+      this.app = this.app.listen(this.config.app.port, this.config.app.host, listenResolve);
+
+      await listenPromise;
+    }
 
     // Fill in database with initial data
     this.data = await initDatabase();
