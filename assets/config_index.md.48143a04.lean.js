375e80749c7d44206e1ad3251c3ba2c12be40e15
lukashroch <223967+lukashroch@users.noreply.github.com>
2022-10-30 10:31:34 +0000

    Deploying to ghp-docs from @ MRC-Epid-it24/intake24@0fe5d66845e3493f6c10262438b8be8a8fa3cd53 🚀

---
+++ assets/config_index.md.48143a04.lean.js
@@ -0,0 +1,1 @@
+import{_ as e,c as t,o as a,a as o}from"./app.8341d59f.js";const m=JSON.parse('{"title":"Introduction","description":"","frontmatter":{},"headers":[],"relativePath":"config/index.md"}'),n={name:"config/index.md"},c=o("",5),s=[c];function d(i,r,p,l,_,h){return a(),t("div",null,s)}const v=e(n,[["render",d]]);export{m as __pageData,v as default};
