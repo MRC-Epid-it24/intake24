774fb525901facee7aa687bebfba7a27dc5e684a
Lukas Hroch <lukashroch@gmail.com>
2023-03-26 21:12:59 +0100

    fix(admin): missing drink-scale-prompt leftover switch

--- apps/admin/src/components/prompts/portion-size/drink-scale-prompt.vue
+++ apps/admin/src/components/prompts/portion-size/drink-scale-prompt.vue
@@ -3,6 +3,15 @@
     <v-tab-item key="options">
       <v-row class="mb-3">
         <v-col cols="12" md="6">
+          <v-switch
+            hide-details="auto"
+            :input-value="leftovers"
+            :label="$t('survey-schemes.questions.leftovers')"
+            @change="update('leftovers', $event)"
+          >
+          </v-switch>
+        </v-col>
+        <v-col cols="12" md="6">
           <image-map-settings :image-map="imageMap" @update:imageMap="update('imageMap', $event)">
           </image-map-settings>
         </v-col>
@@ -29,6 +38,10 @@ export default defineComponent({
   props: {
     imageMap: {
       type: Object as PropType<Prompts['drink-scale-prompt']['imageMap']>,
+      required: true,
+    },
+    leftovers: {
+      type: Boolean as PropType<Prompts['drink-scale-prompt']['leftovers']>,
       required: true,
     },
   },
