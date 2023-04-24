a660e4268b4545b3e01519f12a448aeb668135e4
Lukas Hroch <lukashroch@gmail.com>
2023-04-24 16:32:59 +0100

    fix(mail): template screen scaling tweaks

--- packages/common-backend/src/util/templates.ts
+++ packages/common-backend/src/util/templates.ts
@@ -2,10 +2,14 @@ import type { DOMWindow } from 'jsdom';
 import { JSDOM } from 'jsdom';
 
 export const replaceCssAsInlineStyle = (html: string) => {
+  const docType = html.split('\n')[0];
   const window = new JSDOM(html).window;
+
   replaceCssAsInlineStyleElement(window.document.body, window);
 
-  return window.document.documentElement.outerHTML;
+  return docType.startsWith('<!DOCTYPE')
+    ? `${docType}\n${window.document.documentElement.outerHTML}`
+    : window.document.documentElement.outerHTML;
 };
 
 export const replaceCssAsInlineStyleElement = (element: HTMLElement, window: DOMWindow) => {
