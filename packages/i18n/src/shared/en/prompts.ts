03718f138788c0b437663340d170f5557de1af56
Lukas Hroch <lukashroch@gmail.com>
2023-08-08 15:04:48 +0100

    fix(i18n): default texts & paragraph spacing

--- packages/i18n/src/shared/en/prompts.ts
+++ packages/i18n/src/shared/en/prompts.ts
@@ -87,8 +87,8 @@ const prompts: LocaleMessageObject = {
     name: 'Edit Meal',
     text: '',
     description: `<p>List everything you had for your <strong>{meal}</strong>, one item per line.</p>
-      For example:<p><ul><li>banana</li><li>crisps</li><li>rice</li><li>tea</li></ul></p>
-      <p>You can press Enter on your keyboard or the "add a food/drink" button to go to the next line as you type.</p>
+      For example:<p><ul><li>Weetabix</li><li>Tea</li></ul></p>
+      <p>You can press Enter on your keyboard or click the "down arrow ADD" button to go to the next line.</p>
       <p><strong>Do not</strong> enter how much you had, just the food names.`,
     add: 'Add',
     drinksOnly: 'Drinks',
