5f99350a2c52f5c86a8de5e18dbf4b8c75d215bc
Ivan Poliakov <ivpolyakov@gmail.com>
2023-08-24 19:13:00 +0100

    fix(survey): V4-883: reversed food order

--- apps/survey/src/components/elements/CategoryContentsView.vue
+++ apps/survey/src/components/elements/CategoryContentsView.vue
@@ -1,42 +1,42 @@
 <template>
-  <v-list
-    v-if="contents.subcategories.length || contents.foods.length"
-    class="list__no-wrap py-0 d-flex"
-    :class="categoriesFirst ? 'flex-column' : 'flex-column-reverse'"
-  >
-    <v-list-item
-      v-for="category in contents.subcategories"
-      :key="category.code"
-      class="list-item-border"
-      @click="categorySelected(category)"
-    >
-      <v-list-item-icon>
-        <v-icon>$category</v-icon>
-      </v-list-item-icon>
-      <v-list-item-content>
-        <v-list-item-title>
-          <span class="font-weight-medium">{{ category.name }}</span>
-        </v-list-item-title>
-      </v-list-item-content>
-    </v-list-item>
-    <v-list-item
-      v-for="food in contents.foods"
-      :key="food.code"
-      class="list-item-border"
-      @click="foodSelected(food)"
-    >
-      <v-list-item-icon>
-        <v-icon>$food</v-icon>
-      </v-list-item-icon>
-      <v-list-item-content>
-        <v-list-item-title>{{ food.name }}</v-list-item-title>
-      </v-list-item-content>
-    </v-list-item>
-  </v-list>
-  <div v-else class="pa-4">
-    <v-alert color="grey lighten-2 mb-0" icon="fas fa-triangle-exclamation">
-      {{ i18n.none }}
-    </v-alert>
+  <div class="d-flex" :class="categoriesFirst ? 'flex-column' : 'flex-column-reverse'">
+    <v-list v-if="contents.subcategories.length" class="list__no-wrap py-0">
+      <v-list-item
+        v-for="category in contents.subcategories"
+        :key="category.code"
+        class="list-item-border"
+        @click="categorySelected(category)"
+      >
+        <v-list-item-icon>
+          <v-icon>$category</v-icon>
+        </v-list-item-icon>
+        <v-list-item-content>
+          <v-list-item-title>
+            <span class="font-weight-medium">{{ category.name }}</span>
+          </v-list-item-title>
+        </v-list-item-content>
+      </v-list-item>
+    </v-list>
+    <v-list v-if="contents.foods.length" class="list__no-wrap py-0">
+      <v-list-item
+        v-for="food in contents.foods"
+        :key="food.code"
+        class="list-item-border"
+        @click="foodSelected(food)"
+      >
+        <v-list-item-icon>
+          <v-icon>$food</v-icon>
+        </v-list-item-icon>
+        <v-list-item-content>
+          <v-list-item-title>{{ food.name }}</v-list-item-title>
+        </v-list-item-content>
+      </v-list-item>
+    </v-list>
+    <div v-if="contents.subcategories.length == 0 && contents.foods.length == 0" class="pa-4">
+      <v-alert color="grey lighten-2 mb-0" icon="fas fa-triangle-exclamation">
+        {{ i18n.none }}
+      </v-alert>
+    </div>
   </div>
 </template>
 
