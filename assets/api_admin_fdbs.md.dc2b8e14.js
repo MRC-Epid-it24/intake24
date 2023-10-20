01076bc679fc88d9244330d98c5177a7661344f4
lukashroch <223967+lukashroch@users.noreply.github.com>
2023-10-20 07:56:40 +0000

    Deploying to ghp-docs from @ MRC-Epid-it24/intake24@a9e168853db794ee0033492ddff8dddab82e9d9c 🚀

---
+++ assets/api_admin_fdbs.md.dc2b8e14.js
@@ -0,0 +1,81 @@
+import{_ as s,o as a,c as n,Q as l}from"./chunks/framework.abd19ccc.js";const u=JSON.parse('{"title":"Food databases","description":"","frontmatter":{},"headers":[],"relativePath":"api/admin/fdbs.md","filePath":"api/admin/fdbs.md"}'),e={name:"api/admin/fdbs.md"},p=l(`<h1 id="food-databases" tabindex="-1">Food databases <a class="header-anchor" href="#food-databases" aria-label="Permalink to &quot;Food databases&quot;">​</a></h1><h2 id="browse-food-databases" tabindex="-1">Browse food databases <a class="header-anchor" href="#browse-food-databases" aria-label="Permalink to &quot;Browse food databases&quot;">​</a></h2><p>Browse paginated food database list (effectively locale list)</p><h3 id="request" tabindex="-1">Request <a class="header-anchor" href="#request" aria-label="Permalink to &quot;Request&quot;">​</a></h3><div class="language-json vp-adaptive-theme"><button title="Copy Code" class="copy"></button><span class="lang">json</span><pre class="shiki github-dark vp-code-dark"><code><span class="line"><span style="color:#E1E4E8;">GET /api/admin/fdbs</span></span>
+<span class="line"><span style="color:#E1E4E8;">    ?search={</span><span style="color:#FDAEB7;font-style:italic;">searchText</span><span style="color:#E1E4E8;">}</span></span>
+<span class="line"><span style="color:#E1E4E8;">    &amp;page={</span><span style="color:#FDAEB7;font-style:italic;">page</span><span style="color:#E1E4E8;">}</span></span>
+<span class="line"><span style="color:#E1E4E8;">    &amp;limit={</span><span style="color:#FDAEB7;font-style:italic;">limit</span><span style="color:#E1E4E8;">}</span></span>
+<span class="line"></span>
+<span class="line"><span style="color:#E1E4E8;">Authorization: Bearer {</span><span style="color:#FDAEB7;font-style:italic;">accessToken</span><span style="color:#E1E4E8;">}</span></span>
+<span class="line"><span style="color:#E1E4E8;">Content-Type: application/json</span></span></code></pre><pre class="shiki github-light vp-code-light"><code><span class="line"><span style="color:#24292E;">GET /api/admin/fdbs</span></span>
+<span class="line"><span style="color:#24292E;">    ?search={</span><span style="color:#B31D28;font-style:italic;">searchText</span><span style="color:#24292E;">}</span></span>
+<span class="line"><span style="color:#24292E;">    &amp;page={</span><span style="color:#B31D28;font-style:italic;">page</span><span style="color:#24292E;">}</span></span>
+<span class="line"><span style="color:#24292E;">    &amp;limit={</span><span style="color:#B31D28;font-style:italic;">limit</span><span style="color:#24292E;">}</span></span>
+<span class="line"></span>
+<span class="line"><span style="color:#24292E;">Authorization: Bearer {</span><span style="color:#B31D28;font-style:italic;">accessToken</span><span style="color:#24292E;">}</span></span>
+<span class="line"><span style="color:#24292E;">Content-Type: application/json</span></span></code></pre></div><h3 id="response" tabindex="-1">Response <a class="header-anchor" href="#response" aria-label="Permalink to &quot;Response&quot;">​</a></h3><div class="language-json vp-adaptive-theme"><button title="Copy Code" class="copy"></button><span class="lang">json</span><pre class="shiki github-dark vp-code-dark"><code><span class="line"><span style="color:#79B8FF;">200</span><span style="color:#E1E4E8;"> OK</span></span>
+<span class="line"></span>
+<span class="line"><span style="color:#E1E4E8;">{</span></span>
+<span class="line"><span style="color:#E1E4E8;">    </span><span style="color:#79B8FF;">&quot;data&quot;</span><span style="color:#E1E4E8;">: [{</span><span style="color:#FDAEB7;font-style:italic;">...</span><span style="color:#E1E4E8;">}],</span></span>
+<span class="line"><span style="color:#E1E4E8;">    </span><span style="color:#79B8FF;">&quot;meta&quot;</span><span style="color:#E1E4E8;">: {</span><span style="color:#FDAEB7;font-style:italic;">...</span><span style="color:#E1E4E8;">}</span></span>
+<span class="line"><span style="color:#E1E4E8;">}</span></span></code></pre><pre class="shiki github-light vp-code-light"><code><span class="line"><span style="color:#005CC5;">200</span><span style="color:#24292E;"> OK</span></span>
+<span class="line"></span>
+<span class="line"><span style="color:#24292E;">{</span></span>
+<span class="line"><span style="color:#24292E;">    </span><span style="color:#005CC5;">&quot;data&quot;</span><span style="color:#24292E;">: [{</span><span style="color:#B31D28;font-style:italic;">...</span><span style="color:#24292E;">}],</span></span>
+<span class="line"><span style="color:#24292E;">    </span><span style="color:#005CC5;">&quot;meta&quot;</span><span style="color:#24292E;">: {</span><span style="color:#B31D28;font-style:italic;">...</span><span style="color:#24292E;">}</span></span>
+<span class="line"><span style="color:#24292E;">}</span></span></code></pre></div><h2 id="get-food-database" tabindex="-1">Get food database <a class="header-anchor" href="#get-food-database" aria-label="Permalink to &quot;Get food database&quot;">​</a></h2><p>Get food database entry</p><h3 id="request-1" tabindex="-1">Request <a class="header-anchor" href="#request-1" aria-label="Permalink to &quot;Request&quot;">​</a></h3><div class="language-json vp-adaptive-theme"><button title="Copy Code" class="copy"></button><span class="lang">json</span><pre class="shiki github-dark vp-code-dark"><code><span class="line"><span style="color:#E1E4E8;">GET /api/admin/fdbs/:localeId</span></span>
+<span class="line"></span>
+<span class="line"><span style="color:#E1E4E8;">Authorization: Bearer {</span><span style="color:#FDAEB7;font-style:italic;">accessToken</span><span style="color:#E1E4E8;">}</span></span>
+<span class="line"><span style="color:#E1E4E8;">Content-Type: application/json</span></span></code></pre><pre class="shiki github-light vp-code-light"><code><span class="line"><span style="color:#24292E;">GET /api/admin/fdbs/:localeId</span></span>
+<span class="line"></span>
+<span class="line"><span style="color:#24292E;">Authorization: Bearer {</span><span style="color:#B31D28;font-style:italic;">accessToken</span><span style="color:#24292E;">}</span></span>
+<span class="line"><span style="color:#24292E;">Content-Type: application/json</span></span></code></pre></div><h3 id="response-1" tabindex="-1">Response <a class="header-anchor" href="#response-1" aria-label="Permalink to &quot;Response&quot;">​</a></h3><div class="language-json vp-adaptive-theme"><button title="Copy Code" class="copy"></button><span class="lang">json</span><pre class="shiki github-dark vp-code-dark"><code><span class="line"><span style="color:#79B8FF;">200</span><span style="color:#E1E4E8;"> OK</span></span>
+<span class="line"></span>
+<span class="line"><span style="color:#E1E4E8;">{</span></span>
+<span class="line"></span>
+<span class="line"><span style="color:#E1E4E8;">}</span></span></code></pre><pre class="shiki github-light vp-code-light"><code><span class="line"><span style="color:#005CC5;">200</span><span style="color:#24292E;"> OK</span></span>
+<span class="line"></span>
+<span class="line"><span style="color:#24292E;">{</span></span>
+<span class="line"></span>
+<span class="line"><span style="color:#24292E;">}</span></span></code></pre></div><h2 id="browse-categories" tabindex="-1">Browse categories <a class="header-anchor" href="#browse-categories" aria-label="Permalink to &quot;Browse categories&quot;">​</a></h2><p>Browse paginated category list</p><h3 id="request-2" tabindex="-1">Request <a class="header-anchor" href="#request-2" aria-label="Permalink to &quot;Request&quot;">​</a></h3><div class="language-json vp-adaptive-theme"><button title="Copy Code" class="copy"></button><span class="lang">json</span><pre class="shiki github-dark vp-code-dark"><code><span class="line"><span style="color:#E1E4E8;">GET /api/admin/fdbs/:localeId/categories</span></span>
+<span class="line"><span style="color:#E1E4E8;">    ?search={</span><span style="color:#FDAEB7;font-style:italic;">searchText</span><span style="color:#E1E4E8;">}</span></span>
+<span class="line"><span style="color:#E1E4E8;">    &amp;page={</span><span style="color:#FDAEB7;font-style:italic;">page</span><span style="color:#E1E4E8;">}</span></span>
+<span class="line"><span style="color:#E1E4E8;">    &amp;limit={</span><span style="color:#FDAEB7;font-style:italic;">limit</span><span style="color:#E1E4E8;">}</span></span>
+<span class="line"></span>
+<span class="line"><span style="color:#E1E4E8;">Authorization: Bearer {</span><span style="color:#FDAEB7;font-style:italic;">accessToken</span><span style="color:#E1E4E8;">}</span></span>
+<span class="line"><span style="color:#E1E4E8;">Content-Type: application/json</span></span></code></pre><pre class="shiki github-light vp-code-light"><code><span class="line"><span style="color:#24292E;">GET /api/admin/fdbs/:localeId/categories</span></span>
+<span class="line"><span style="color:#24292E;">    ?search={</span><span style="color:#B31D28;font-style:italic;">searchText</span><span style="color:#24292E;">}</span></span>
+<span class="line"><span style="color:#24292E;">    &amp;page={</span><span style="color:#B31D28;font-style:italic;">page</span><span style="color:#24292E;">}</span></span>
+<span class="line"><span style="color:#24292E;">    &amp;limit={</span><span style="color:#B31D28;font-style:italic;">limit</span><span style="color:#24292E;">}</span></span>
+<span class="line"></span>
+<span class="line"><span style="color:#24292E;">Authorization: Bearer {</span><span style="color:#B31D28;font-style:italic;">accessToken</span><span style="color:#24292E;">}</span></span>
+<span class="line"><span style="color:#24292E;">Content-Type: application/json</span></span></code></pre></div><h3 id="response-2" tabindex="-1">Response <a class="header-anchor" href="#response-2" aria-label="Permalink to &quot;Response&quot;">​</a></h3><div class="language-json vp-adaptive-theme"><button title="Copy Code" class="copy"></button><span class="lang">json</span><pre class="shiki github-dark vp-code-dark"><code><span class="line"><span style="color:#79B8FF;">200</span><span style="color:#E1E4E8;"> OK</span></span>
+<span class="line"></span>
+<span class="line"><span style="color:#E1E4E8;">{</span></span>
+<span class="line"><span style="color:#E1E4E8;">    </span><span style="color:#79B8FF;">&quot;data&quot;</span><span style="color:#E1E4E8;">: [{</span><span style="color:#FDAEB7;font-style:italic;">...</span><span style="color:#E1E4E8;">}],</span></span>
+<span class="line"><span style="color:#E1E4E8;">    </span><span style="color:#79B8FF;">&quot;meta&quot;</span><span style="color:#E1E4E8;">: {</span><span style="color:#FDAEB7;font-style:italic;">...</span><span style="color:#E1E4E8;">}</span></span>
+<span class="line"><span style="color:#E1E4E8;">}</span></span></code></pre><pre class="shiki github-light vp-code-light"><code><span class="line"><span style="color:#005CC5;">200</span><span style="color:#24292E;"> OK</span></span>
+<span class="line"></span>
+<span class="line"><span style="color:#24292E;">{</span></span>
+<span class="line"><span style="color:#24292E;">    </span><span style="color:#005CC5;">&quot;data&quot;</span><span style="color:#24292E;">: [{</span><span style="color:#B31D28;font-style:italic;">...</span><span style="color:#24292E;">}],</span></span>
+<span class="line"><span style="color:#24292E;">    </span><span style="color:#005CC5;">&quot;meta&quot;</span><span style="color:#24292E;">: {</span><span style="color:#B31D28;font-style:italic;">...</span><span style="color:#24292E;">}</span></span>
+<span class="line"><span style="color:#24292E;">}</span></span></code></pre></div><h2 id="browse-foods" tabindex="-1">Browse foods <a class="header-anchor" href="#browse-foods" aria-label="Permalink to &quot;Browse foods&quot;">​</a></h2><p>Browse paginated food list</p><h3 id="request-3" tabindex="-1">Request <a class="header-anchor" href="#request-3" aria-label="Permalink to &quot;Request&quot;">​</a></h3><div class="language-json vp-adaptive-theme"><button title="Copy Code" class="copy"></button><span class="lang">json</span><pre class="shiki github-dark vp-code-dark"><code><span class="line"><span style="color:#E1E4E8;">GET /api/admin/fdbs/:localeId/foods</span></span>
+<span class="line"><span style="color:#E1E4E8;">    ?search={</span><span style="color:#FDAEB7;font-style:italic;">searchText</span><span style="color:#E1E4E8;">}</span></span>
+<span class="line"><span style="color:#E1E4E8;">    &amp;page={</span><span style="color:#FDAEB7;font-style:italic;">page</span><span style="color:#E1E4E8;">}</span></span>
+<span class="line"><span style="color:#E1E4E8;">    &amp;limit={</span><span style="color:#FDAEB7;font-style:italic;">limit</span><span style="color:#E1E4E8;">}</span></span>
+<span class="line"></span>
+<span class="line"><span style="color:#E1E4E8;">Authorization: Bearer {</span><span style="color:#FDAEB7;font-style:italic;">accessToken</span><span style="color:#E1E4E8;">}</span></span>
+<span class="line"><span style="color:#E1E4E8;">Content-Type: application/json</span></span></code></pre><pre class="shiki github-light vp-code-light"><code><span class="line"><span style="color:#24292E;">GET /api/admin/fdbs/:localeId/foods</span></span>
+<span class="line"><span style="color:#24292E;">    ?search={</span><span style="color:#B31D28;font-style:italic;">searchText</span><span style="color:#24292E;">}</span></span>
+<span class="line"><span style="color:#24292E;">    &amp;page={</span><span style="color:#B31D28;font-style:italic;">page</span><span style="color:#24292E;">}</span></span>
+<span class="line"><span style="color:#24292E;">    &amp;limit={</span><span style="color:#B31D28;font-style:italic;">limit</span><span style="color:#24292E;">}</span></span>
+<span class="line"></span>
+<span class="line"><span style="color:#24292E;">Authorization: Bearer {</span><span style="color:#B31D28;font-style:italic;">accessToken</span><span style="color:#24292E;">}</span></span>
+<span class="line"><span style="color:#24292E;">Content-Type: application/json</span></span></code></pre></div><h3 id="response-3" tabindex="-1">Response <a class="header-anchor" href="#response-3" aria-label="Permalink to &quot;Response&quot;">​</a></h3><div class="language-json vp-adaptive-theme"><button title="Copy Code" class="copy"></button><span class="lang">json</span><pre class="shiki github-dark vp-code-dark"><code><span class="line"><span style="color:#79B8FF;">200</span><span style="color:#E1E4E8;"> OK</span></span>
+<span class="line"></span>
+<span class="line"><span style="color:#E1E4E8;">{</span></span>
+<span class="line"><span style="color:#E1E4E8;">    </span><span style="color:#79B8FF;">&quot;data&quot;</span><span style="color:#E1E4E8;">: [{</span><span style="color:#FDAEB7;font-style:italic;">...</span><span style="color:#E1E4E8;">}],</span></span>
+<span class="line"><span style="color:#E1E4E8;">    </span><span style="color:#79B8FF;">&quot;meta&quot;</span><span style="color:#E1E4E8;">: {</span><span style="color:#FDAEB7;font-style:italic;">...</span><span style="color:#E1E4E8;">}</span></span>
+<span class="line"><span style="color:#E1E4E8;">}</span></span></code></pre><pre class="shiki github-light vp-code-light"><code><span class="line"><span style="color:#005CC5;">200</span><span style="color:#24292E;"> OK</span></span>
+<span class="line"></span>
+<span class="line"><span style="color:#24292E;">{</span></span>
+<span class="line"><span style="color:#24292E;">    </span><span style="color:#005CC5;">&quot;data&quot;</span><span style="color:#24292E;">: [{</span><span style="color:#B31D28;font-style:italic;">...</span><span style="color:#24292E;">}],</span></span>
+<span class="line"><span style="color:#24292E;">    </span><span style="color:#005CC5;">&quot;meta&quot;</span><span style="color:#24292E;">: {</span><span style="color:#B31D28;font-style:italic;">...</span><span style="color:#24292E;">}</span></span>
+<span class="line"><span style="color:#24292E;">}</span></span></code></pre></div>`,25),o=[p];function t(c,i,r,E,y,d){return a(),n("div",null,o)}const f=s(e,[["render",t]]);export{u as __pageData,f as default};
