35e67be2dc5ac0f045ac55f759ba04b872463ffc
Ivan Poliakov <ivpolyakov@gmail.com>
2023-11-07 15:36:09 +0000

    feat(api): French language tools

---
+++ apps/api/src/food-index/fr/french-language-backend.ts
@@ -0,0 +1,24 @@
+import porter from 'talisman/stemmers/french/porter';
+
+import type { LanguageBackend } from '@intake24/api/food-index/phrase-index';
+import FrenchPhoneticEncoder from '@intake24/api/food-index/fr/phonetic';
+
+const sanitiseRegexp = new RegExp(/[.`,/\\\-+)(]|e\.g\.|e\.g|n\.s\.'s/g);
+
+export default {
+  indexIgnore: ['de', 'au', 'aux', 'pour'],
+
+  phoneticEncoder: new FrenchPhoneticEncoder(),
+
+  splitCompound(word: string): Array<string> {
+    return new Array<string>(word);
+  },
+
+  stem(word: string): string {
+    return porter(word);
+  },
+
+  sanitiseDescription(description: string): string {
+    return description.replace(sanitiseRegexp, ' ');
+  },
+} as LanguageBackend;
