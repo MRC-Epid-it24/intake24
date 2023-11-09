d129fb9af919d88263cbeb9a2731a2eb7371aa3d
mucc001 <jiamengsunh@outlook.com>
2023-11-09 11:45:28 +0000

    feat(final review list): add an extra edit meal button

--- apps/survey/src/components/layouts/meal-list/desktop/reviewed-list.vue
+++ apps/survey/src/components/layouts/meal-list/desktop/reviewed-list.vue
@@ -4,6 +4,18 @@
       {{ $t('recall.menu.title') }}
     </v-card-title>
     <v-divider></v-divider>
+    <v-card-actions>
+      <v-hover v-slot="{ hover }">
+        <v-btn
+          :color="hover ? 'primary' : 'inherit'"
+          :title="$t('recall.menu.meal.add')"
+          @click="action('addMeal')"
+        >
+          <v-icon left>$add</v-icon>
+          {{ $t('recall.menu.meal.add') }}
+        </v-btn>
+      </v-hover>
+    </v-card-actions>
     <v-list class="meal-list__list pt-0" dense flat tile>
       <div v-for="meal in meals" :key="meal.id">
         <component
@@ -12,13 +24,26 @@
           :selected-food-in-meal="isSelectedFoodInMeal(meal.id)"
           @action="action"
         ></component>
-        <v-checkbox
-          v-if="review === 'checkbox'"
-          v-model="reviewed"
-          class="review-checkbox__checkbox pl-3"
-          label="Reviewed"
-          :value="meal.id"
-        ></v-checkbox>
+        <v-row class="pl-3">
+          <v-col cols="2">
+            <v-checkbox
+              v-if="review === 'checkbox'"
+              v-model="reviewed"
+              class="review-checkbox__checkbox"
+              label="Reviewed"
+              :value="meal.id"
+            ></v-checkbox>
+            <v-btn
+              class="edit-meal-button"
+              :color="hover ? 'primary' : 'inherit'"
+              :size="x - small"
+              :title="$t('recall.menu.meal.editFoods')"
+              @click="action('editMeal', meal.id)"
+            >
+              {{ $t('recall.menu.meal.editFoods') }}
+            </v-btn>
+          </v-col>
+        </v-row>
       </div>
     </v-list>
     <v-card-actions v-if="!bottomReached" v-intersect="bottomIntersect">
@@ -90,10 +115,15 @@ export default defineComponent({
 });
 </script>
 
-<style lang="scss">
+<style lang="scss" scoped>
 .review-checkbox__checkbox .v-label {
   font-size: 0.8125rem;
   font-weight: bold;
   color: rgba(0, 0, 0, 0.87);
+}
+
+.edit-meal-button {
+  padding: 2px 5px;
+  font-size: 0.8125rem;
 }
 </style>
