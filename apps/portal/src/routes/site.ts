773d06f801a4ba2706898d47179d303fcf866158
Lukas Hroch <lukashroch@gmail.com>
2023-11-27 18:30:53 +0000

    fix(portal): validate page section for redirect

--- apps/portal/src/routes/site.ts
+++ apps/portal/src/routes/site.ts
@@ -6,19 +6,31 @@ const router = Router();
 
 router.get('/', (req, res) => res.render('index'));
 
-router.get('/contacts', (req, res) => res.render('contacts'));
-router.get('/features', (req, res) => res.render('features'));
-router.get('/feedback', (req, res) => res.render('feedback'));
-router.get('/localisation', (req, res) => res.render('localisation'));
-router.get('/open-source', (req, res) => res.render('open-source'));
-router.get('/output', (req, res) => res.render('output'));
-router.get('/publications', (req, res) => res.render('publications'));
-router.get('/recall', (req, res) => res.render('recall'));
-router.get('/validation', (req, res) => res.render('validation'));
-router.get('/privacy', (req, res) => res.render(`${site.content}/privacy`));
-router.get('/terms', (req, res) => res.render(`${site.content}/terms`));
+const global = [
+  'contacts',
+  'features',
+  'feedback',
+  'localisation',
+  'open-source',
+  'output',
+  'publications',
+  'recall',
+  'validation',
+];
+const local = ['privacy', 'terms'];
 
-router.get('/info/:section', (req, res) => res.redirect(301, `/${req.params.section}`));
+for (const page of global) router.get(`/${page}`, (req, res) => res.render(page));
+for (const page of local)
+  router.get(`/${page}`, (req, res) => res.render(`${site.content}/${page}`));
+
+router.get('/info/:section', (req, res) => {
+  if (![...global, ...local].includes(req.params.section)) {
+    res.status(404).render('errors/404');
+    return;
+  }
+
+  res.redirect(301, `/${req.params.section}`);
+});
 
 // Catch Not Found (404)
 router.get('*', (req, res) => res.status(404).render('errors/404'));
