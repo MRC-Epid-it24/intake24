5d38fbc8604b6a948da57ecf87de5f38844870bf
Lukas Hroch <lukashroch@gmail.com>
2023-12-11 12:26:31 +0000

    chore: update dependencies & pwa assets

--- apps/admin/pwa-assets.config.ts
+++ apps/admin/pwa-assets.config.ts
@@ -1,10 +1,13 @@
 import { defineConfig } from '@vite-pwa/assets-generator/config';
 
 export default defineConfig({
+  headLinkOptions: {
+    preset: '2023',
+  },
   preset: {
     transparent: {
       sizes: [64, 192, 512],
-      favicons: [[64, 'icon.ico']],
+      favicons: [[48, 'icon.ico']],
     },
     maskable: {
       sizes: [512],
