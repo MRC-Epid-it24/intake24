ea8e04f3c61edf5a90219cb68506f1748c507d9c
Dinislam <11454021+cheburan@users.noreply.github.com>
2024-01-08 20:44:30 +1100

    feat(db): added migration for the required field in recipe_food_steps table

---
+++ packages/db/sequelize/foods/migrations/20240108141241-recipe-foods-required-parameter.js
@@ -0,0 +1,16 @@
+module.exports = {
+  up: (queryInterface, Sequelize) =>
+    queryInterface.sequelize.transaction(async (transaction) => {
+      await queryInterface.addColumn(
+        'recipe_foods_steps',
+        'required',
+        { type: Sequelize.BOOLEAN, allowNull: false, defaultValue: false },
+        { transaction }
+      );
+    }),
+
+  down: (queryInterface, Sequelize) =>
+    queryInterface.sequelize.transaction(async (transaction) => {
+      await queryInterface.removeColumn('recipe_foods_steps', 'required', { transaction });
+    }),
+};
