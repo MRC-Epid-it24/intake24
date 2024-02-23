MRC-Epid-it24/intake24

commit 997793f99fa1b67a939f7420b5536c0a5acc44e4

feat(survey): multi prompt support (V4-1065)
- group custom prompts together to display in one prompt screen

apps/survey/src/components/handlers/mixins/use-custom-prompt-handler.ts
@@ -1,0 +1,78 @@
+import type { Prompt, Prompts } from '@intake24/common/prompts';
+import type { CustomPromptAnswer } from '@intake24/common/types';
+import { useSurvey } from '@intake24/survey/stores';
+
+import { useFoodPromptUtils } from './use-food-prompt-utils';
+import { useMealPromptUtils } from './use-meal-prompt-utils';
+
+const infoPrompts = ['info-prompt'];
+
+export type UseCustomPromptHandlerProps<P extends keyof Prompts> = {
+  prompt: Prompts[P];
+};
+
+export const useCustomPromptHandler = <P extends keyof Prompts>(
+  props: UseCustomPromptHandlerProps<P>
+) => {
+  const { foodOptional } = useFoodPromptUtils();
+  const { mealOptional } = useMealPromptUtils();
+  const survey = useSurvey();
+
+  const isInfoPrompt = (prompt: Prompt) => infoPrompts.includes(prompt.component);
+
+  const commitPromptAnswer = (prompt: Prompt, answer?: CustomPromptAnswer) => {
+    const promptId = prompt.id;
+    const isInfo = isInfoPrompt(prompt);
+    const isValidAnswer = answer && (!isInfo || answer !== 'next');
+
+    if (survey.selection.element) {
+      switch (survey.selection.element.type) {
+        case 'food': {
+          const food = foodOptional.value;
+          if (!food) {
+            console.warn('Expected meal to be defined');
+            return;
+          }
+
+          if (isValidAnswer) {
+            survey.setFoodCustomPromptAnswer({ foodId: food.id, promptId, answer });
+            return;
+          }
+
+          if (isInfo) survey.addFoodFlag(food.id, `${promptId}-acknowledged`);
+
+          break;
+        }
+        case 'meal': {
+          const meal = mealOptional.value;
+          if (!meal) {
+            console.warn('Expected meal to be defined');
+            return;
+          }
+
+          if (isValidAnswer) {
+            survey.setMealCustomPromptAnswer({ mealId: meal.id, promptId, answer });
+            return;
+          }
+
+          if (isInfo) survey.addMealFlag(meal.id, `${promptId}-acknowledged`);
+
+          break;
+        }
+      }
+    } else {
+      if (isValidAnswer) {
+        survey.setCustomPromptAnswer({ promptId, answer });
+        return;
+      }
+
+      if (isInfo) survey.addFlag(`${promptId}-acknowledged`);
+    }
+  };
+
+  return {
+    commitPromptAnswer,
+    foodOptional,
+    mealOptional,
+  };
+};

apps/admin/src/components/prompts/prompt-selector.vue
@@ -70,6 +70,16 @@
                             outlined
                           ></v-text-field>
                         </v-col>
+                        <v-col v-if="dialog.prompt.type === 'custom'" cols="12" md="6">
+                          <v-text-field
+                            v-model="dialog.prompt.group"
+                            :disabled="isOverrideMode"
+                            hide-details="auto"
+                            :label="$t('survey-schemes.prompts.internal.group._')"
+                            :messages="$t('survey-schemes.prompts.internal.group.hint')"
+                            outlined
+                          ></v-text-field>
+                        </v-col>
                       </v-row>
                     </v-container>
                   </v-card>
@@ -117,7 +127,10 @@
             <prompt-actions :actions.sync="dialog.prompt.actions"></prompt-actions>
             <prompt-conditions :conditions.sync="dialog.prompt.conditions"></prompt-conditions>
             <prompt-validation
-              v-if="promptSettings[dialog.prompt.component].tabs.includes('validation')"
+              v-if="
+                'validation' in dialog.prompt &&
+                promptSettings[dialog.prompt.component].tabs.includes('validation')
+              "
               v-bind.sync="dialog.prompt.validation"
             ></prompt-validation>
             <component

apps/api/src/http/controllers/survey-respondent.controller.ts
@@ -13,7 +13,11 @@ import type {
   SurveyUserSessionResponse,
 } from '@intake24/common/types/http';
 import { NotFoundError } from '@intake24/api/http/errors';
-import { flattenSchemeWithSection, isMealSection } from '@intake24/common/surveys';
+import {
+  flattenSchemeWithSection,
+  groupSchemeMultiPrompts,
+  isMealSection,
+} from '@intake24/common/surveys';
 import { merge } from '@intake24/common/util';
 import { Survey } from '@intake24/db';
 
@@ -67,8 +71,7 @@ const surveyRespondentController = ({
       searchMatchScoreWeight,
     } = survey;
 
-    let { meals } = surveyScheme;
-    const { prompts } = surveyScheme;
+    let { meals, prompts } = surveyScheme;
 
     let state: SurveyStatus;
     const today = new Date();
@@ -82,7 +85,7 @@ const surveyRespondentController = ({
 
     // 2) Prompts - merge by Prompt ID & Prompt Name
     if (surveySchemeOverrides.prompts.length) {
-      const flattenScheme = flattenSchemeWithSection(surveyScheme.prompts);
+      const flattenScheme = flattenSchemeWithSection(prompts);
       for (const prompt of surveySchemeOverrides.prompts) {
         const match = flattenScheme.find(
           (item) => item.id === prompt.id && item.name === prompt.name
@@ -104,6 +107,9 @@ const surveyRespondentController = ({
         }
       }
     }
+
+    // 3) Crate multi-prompt groups
+    prompts = groupSchemeMultiPrompts(prompts);
 
     res.json({
       id,

apps/api/src/services/core/auth/strategies.ts
@@ -1,5 +1,5 @@
 import type { PassportStatic } from 'passport';
-import type { StrategyOptions, StrategyOptionsWithoutRequest } from 'passport-jwt';
+import type { StrategyOptionsWithoutRequest } from 'passport-jwt';
 import { ExtractJwt, Strategy } from 'passport-jwt';
 import { Op } from 'sequelize';
 

apps/survey/src/components/handlers/custom/CustomPromptHandler.vue
@@ -23,7 +23,7 @@ import type { CustomPromptAnswer } from '@intake24/common/types';
 import { customPrompts } from '@intake24/survey/components/prompts';
 import { useSurvey } from '@intake24/survey/stores';
 
-import { useFoodPromptUtils, useMealPromptUtils } from './mixins';
+import { useCustomPromptHandler } from '../mixins';
 
 const infoPrompts = ['info-prompt'];
 
@@ -44,12 +44,10 @@ export default defineComponent({
   },
 
   setup(props, { emit }) {
-    const { foodOptional } = useFoodPromptUtils();
-    const { mealOptional } = useMealPromptUtils();
+    const { commitPromptAnswer, foodOptional, mealOptional } = useCustomPromptHandler(props);
     const survey = useSurvey();
 
     const isInfoPrompt = computed(() => infoPrompts.includes(props.prompt.component));
-
     const state = ref<CustomPromptAnswer | undefined>(isInfoPrompt.value ? 'next' : undefined);
 
     const action = (type: string, ...args: [id?: string, params?: object]) => {
@@ -70,45 +68,7 @@ export default defineComponent({
         survey.setSelection(newSelection);
       }
 
-      const promptId = props.prompt.id;
-
-      if (survey.selection.element) {
-        switch (survey.selection.element.type) {
-          case 'food': {
-            const food = foodOptional.value;
-            if (!food) {
-              console.warn('Expected meal to be defined');
-              return;
-            }
-
-            if (!isInfoPrompt.value || state.value !== 'next')
-              survey.setFoodCustomPromptAnswer({ foodId: food.id, promptId, answer: state.value });
-
-            if (isInfoPrompt.value) survey.addFoodFlag(food.id, `${promptId}-acknowledged`);
-
-            break;
-          }
-          case 'meal': {
-            const meal = mealOptional.value;
-            if (!meal) {
-              console.warn('Expected meal to be defined');
-              return;
-            }
-
-            if (!isInfoPrompt.value || state.value !== 'next')
-              survey.setMealCustomPromptAnswer({ mealId: meal.id, promptId, answer: state.value });
-
-            if (isInfoPrompt.value) survey.addMealFlag(meal.id, `${promptId}-acknowledged`);
-
-            break;
-          }
-        }
-      } else {
-        if (!isInfoPrompt.value || state.value !== 'next')
-          survey.setCustomPromptAnswer({ promptId, answer: state.value });
-
-        if (isInfoPrompt.value) survey.addFlag(`${promptId}-acknowledged`);
-      }
+      commitPromptAnswer(props.prompt, state.value);
     };
 
     return {

apps/survey/src/components/handlers/custom/MultiCustomPromptHandler.vue
@@ -1,0 +1,81 @@
+<template>
+  <multi-prompt
+    v-model="state"
+    v-bind="{
+      meal: mealOptional,
+      food: foodOptional,
+      prompt,
+      section,
+    }"
+    @action="action"
+  ></multi-prompt>
+</template>
+
+<script lang="ts">
+import type { PropType } from 'vue';
+import { defineComponent, ref } from 'vue';
+
+import type { Prompt, Prompts } from '@intake24/common/prompts';
+import type { PromptSection } from '@intake24/common/surveys';
+import type { CustomPromptAnswer } from '@intake24/common/types';
+import { MultiPrompt } from '@intake24/survey/components/prompts';
+import { useSurvey } from '@intake24/survey/stores';
+
+import { useCustomPromptHandler } from '../mixins';
+
+const infoPrompts = ['info-prompt'];
+
+export default defineComponent({
+  name: 'MultiCustomPromptHandler',
+
+  components: { MultiPrompt },
+
+  props: {
+    prompt: {
+      type: Object as PropType<Prompts['multi-prompt']>,
+      required: true,
+    },
+    section: {
+      type: String as PropType<PromptSection>,
+      required: true,
+    },
+  },
+
+  setup(props, { emit }) {
+    const { commitPromptAnswer, foodOptional, mealOptional } = useCustomPromptHandler(props);
+    const survey = useSurvey();
+
+    const isInfoPrompt = (prompt: Prompt) => infoPrompts.includes(prompt.component);
+    const state = ref<(CustomPromptAnswer | undefined)[]>(
+      props.prompt.prompts.map((prompt) => (isInfoPrompt(prompt) ? 'next' : undefined))
+    );
+
+    const action = (type: string, ...args: [id?: string, params?: object]) => {
+      if (type === 'next') commitAnswer();
+
+      emit('action', type, ...args);
+    };
+
+    const commitAnswer = () => {
+      if (state.value.some((answer) => answer === undefined)) {
+        console.warn('Did not expect answer to be undefined');
+        return;
+      }
+
+      props.prompt.prompts.forEach((prompt, idx) => {
+        commitPromptAnswer(prompt, state.value[idx]);
+      });
+    };
+
+    return {
+      action,
+      foodOptional,
+      mealOptional,
+      state,
+      survey,
+    };
+  },
+});
+</script>
+
+<style scoped></style>

apps/survey/src/components/handlers/custom/index.ts
@@ -1,0 +1,7 @@
+import CustomPromptHandler from './CustomPromptHandler.vue';
+import MultiCustomPromptHandler from './MultiCustomPromptHandler.vue';
+
+export default {
+  CustomPromptHandler,
+  MultiCustomPromptHandler,
+};

apps/survey/src/components/handlers/index.ts
@@ -1,4 +1,3 @@
-export { default as CustomPromptHandler } from './CustomPromptHandler.vue';
-export * from './mixins';
+export { default as customHandlers } from './custom';
 export { default as portionSizeHandlers } from './portion-size';
 export { default as standardHandlers } from './standard';

commit cfff9012d0d33136fe14d3419792af3178b1bc71

fix(CustomPromptHandler): store flags for custom answers
- regression from 997793f99fa1b67a939f7420b5536c0a5acc44e4

apps/survey/src/components/handlers/mixins/use-custom-prompt-handler.ts
@@ -34,10 +34,8 @@ export const useCustomPromptHandler = <P extends keyof Prompts>(
             return;
           }
 
-          if (isValidAnswer) {
+          if (isValidAnswer)
             survey.setFoodCustomPromptAnswer({ foodId: food.id, promptId, answer });
-            return;
-          }
 
           if (isInfo) survey.addFoodFlag(food.id, `${promptId}-acknowledged`);
 
@@ -50,10 +48,8 @@ export const useCustomPromptHandler = <P extends keyof Prompts>(
             return;
           }
 
-          if (isValidAnswer) {
+          if (isValidAnswer)
             survey.setMealCustomPromptAnswer({ mealId: meal.id, promptId, answer });
-            return;
-          }
 
           if (isInfo) survey.addMealFlag(meal.id, `${promptId}-acknowledged`);
 
@@ -61,10 +57,7 @@ export const useCustomPromptHandler = <P extends keyof Prompts>(
         }
       }
     } else {
-      if (isValidAnswer) {
-        survey.setCustomPromptAnswer({ promptId, answer });
-        return;
-      }
+      if (isValidAnswer) survey.setCustomPromptAnswer({ promptId, answer });
 
       if (isInfo) survey.addFlag(`${promptId}-acknowledged`);
     }