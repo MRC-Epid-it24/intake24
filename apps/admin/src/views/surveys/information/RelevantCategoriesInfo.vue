f5f6a56d77991d5172087c0316bbb3a26ef201f6
Ivan Poliakov <ivpolyakov@gmail.com>
2024-05-22 17:36:01 +0100

    feat(api): V4-1137: relevant categories search option

---
+++ apps/admin/src/views/surveys/information/RelevantCategoriesInfo.vue
@@ -0,0 +1,21 @@
+<script setup lang='ts'>
+
+</script>
+
+<template>
+  <v-card flat>
+    <v-card-title>
+      {{ $t('surveys.search.information.relevantCategoriesInfo.title') }}
+    </v-card-title>
+    <v-card-text>
+      <p v-dompurify-html:i18n="$t('surveys.search.information.relevantCategoriesInfo.p1')" />
+      <p v-dompurify-html:i18n="$t('surveys.search.information.relevantCategoriesInfo.p2')" />
+      <p v-dompurify-html:i18n="$t('surveys.search.information.relevantCategoriesInfo.p3')" />
+      <p v-dompurify-html:i18n="$t('surveys.search.information.relevantCategoriesInfo.relevantCategoryDepth')" />
+    </v-card-text>
+  </v-card>
+</template>
+
+<style scoped lang='scss'>
+
+</style>
