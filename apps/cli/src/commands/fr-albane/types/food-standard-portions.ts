3852e5a297609f0ea75a46f9c744d7e4aefc7770
Ivan Poliakov <ivpolyakov@gmail.com>
2024-05-31 17:33:41 +0100

    feat(cli): WIP Fr Albane locale build

---
+++ apps/cli/src/commands/fr-albane/types/food-standard-portions.ts
@@ -0,0 +1,11 @@
+export interface INCA3FoodStandardUnitRow {
+  A_CODE: string;
+  A_LIBELLE: string;
+  A_US_TYPE: string;
+  A_US_NUM: string;
+  A_US_RAWCOOK: string;
+  A_US_EDIB: string;
+  A_US_POIDS: number;
+  A_US_UNITE: string;
+  A_US_LIBELLE: string;
+}
---
+++ apps/cli/src/commands/fr-albane/types/recipe-standard-portions.ts
@@ -0,0 +1,7 @@
+export interface INCA3RecipeStandardUnitRow {
+  R_CODE: string;
+  R_LIBELLE: string;
+  R_US_NUM: string;
+  R_US_POIDS: number;
+  R_US_LIBELLE: string;
+}
