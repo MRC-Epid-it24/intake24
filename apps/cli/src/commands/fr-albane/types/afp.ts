6b4645a2c205a14114f105a53a6b37fd2fcdaa82
Ivan Poliakov <ivpolyakov@gmail.com>
2024-07-05 11:28:59 +0100

    feat(cli): French/Albane assoc foods import

---
+++ apps/cli/src/commands/fr-albane/types/afp.ts
@@ -0,0 +1,11 @@
+export interface AlbaneAfpRow {
+  code: string;
+  label: string;
+  categoryCode1: string;
+  promptText1: string;
+  categoryCode2: string;
+  promptText2: string;
+  categoryCode3: string;
+  promptText3: string;
+  genericName: string;
+}
