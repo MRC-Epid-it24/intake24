fb7ab55257ffeaa84132566f317b99feaaeffad1
Ivan Poliakov <ivpolyakov@gmail.com>
2024-09-02 13:24:08 +0100

    feat(deployment): apply config changes without rebuild

---
+++ deployment/app-config-api.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+if [ "$#" -lt 1 ]; then
+  echo "Instance name required"
+else
+  ansible-playbook -i ./instances/$1/hosts --extra-vars="instance=$1 version=$2" ansible/app-config-api.yml
+fi
