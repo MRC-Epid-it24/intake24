71247c45a4273ed21832ca4da024e776002690e4
Lukas Hroch <lukashroch@gmail.com>
2024-09-03 13:05:16 +0100

    fix(feedback): add mission cards section title (V4-1240)

--- packages/ui/src/feedback/components/feedback-cards.vue
+++ packages/ui/src/feedback/components/feedback-cards.vue
@@ -1,14 +1,19 @@
 <template>
-  <v-container class="container-max">
-    <v-row class="d-print-none" justify="center">
-      <v-col v-for="card in cards" :key="`screen-${card.id}`" cols="12" md="4" sm="6">
-        <screen-card :parameters="card" />
-      </v-col>
-    </v-row>
-    <div v-for="card in cards" :key="`print-${card.id}`" class="d-none d-print-block">
-      <print-card :parameters="card" />
-    </div>
-  </v-container>
+  <div>
+    <h2 class="text-h2 font-weight-medium text-center mb-6 chart-print-title">
+      {{ $t('feedback.cards.title') }}
+    </h2>
+    <v-container class="container-max">
+      <v-row class="d-print-none" justify="center">
+        <v-col v-for="card in cards" :key="`screen-${card.id}`" cols="12" md="4" sm="6">
+          <screen-card :parameters="card" />
+        </v-col>
+      </v-row>
+      <div v-for="card in cards" :key="`print-${card.id}`" class="d-none d-print-block">
+        <print-card :parameters="card" />
+      </div>
+    </v-container>
+  </div>
 </template>
 
 <script lang="ts">
