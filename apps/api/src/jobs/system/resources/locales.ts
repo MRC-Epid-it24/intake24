6e68eddf4213b434e836e7d7615b19d3f0ecc9e9
Lukas Hroch <lukashroch@gmail.com>
2025-02-05 11:30:28 +0000

    feat: food database changes (V4-1282)

--- apps/api/src/jobs/system/resources/locales.ts
+++ apps/api/src/jobs/system/resources/locales.ts
@@ -19,7 +19,6 @@ export async function locales({ kyselyDb }: ResourceOps) {
         'respondentLanguageId',
         'adminLanguageId',
         'countryFlagCode',
-        'prototypeLocaleId',
         'textDirection',
         'foodIndexEnabled',
         'foodIndexLanguageBackendId',
