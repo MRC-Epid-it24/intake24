38682765f91a27b7bd675a7cd1aa53c2fdcab185
Ivan Poliakov <ivpolyakov@gmail.com>
2025-02-05 20:11:29 +0000

    feat(api): food thumbnail images

--- apps/api/src/services/admin/images/index.ts
+++ apps/api/src/services/admin/images/index.ts
@@ -2,6 +2,8 @@ export * from './as-served.service';
 export { default as asServedService } from './as-served.service';
 export * from './drinkware-set.service';
 export { default as drinkwareSetService } from './drinkware-set.service';
+export * from './food-thumbnail-image.service';
+export { default as foodThumbnailImageService } from './food-thumbnail-image.service';
 export * from './guide-image.service';
 export { default as guideImageService } from './guide-image.service';
 export * from './image-map.service';
