07dcf36a83f29faeb8f7122fefd8e59e1e21a1a2
Ivan Poliakov <ivpolyakov@gmail.com>
2025-02-07 17:45:35 +0000

    feat(survey): food thumbnail images

--- apps/api/src/services/admin/images/food-thumbnail-image.service.ts
+++ apps/api/src/services/admin/images/food-thumbnail-image.service.ts
@@ -1,11 +1,13 @@
 import { IoC } from '@intake24/api/ioc';
+import { Dictionary } from '@intake24/common/types';
 import type { SourceFileInput } from '@intake24/common/types/http/admin';
 import { FoodThumbnailImage } from '@intake24/db';
 
 function foodThumbnailImageService({
   processedImageService,
   sourceImageService,
-}: Pick<IoC, 'processedImageService' | 'sourceImageService'>) {
+  kyselyDb,
+}: Pick<IoC, 'processedImageService' | 'sourceImageService' | 'kyselyDb'>) {
   const createImage = async (uploaderId: string, foodLocalId: string, sourceImageInput: SourceFileInput): Promise<FoodThumbnailImage> => {
     const sourceImage = await sourceImageService.uploadSourceImage({ file: sourceImageInput, uploader: uploaderId, id: foodLocalId }, 'food_thumbnail');
     const thumbnailImage = await processedImageService.createFoodThumbnailImage(foodLocalId, sourceImage);
@@ -20,8 +22,25 @@ function foodThumbnailImageService({
     return instance;
   };
 
+  const resolveImages = async (localeId: string, foodCodes: string[]): Promise<Dictionary<string>> => {
+    if (foodCodes.length === 0)
+      return {};
+
+    const rows = await kyselyDb.foods
+      .selectFrom('foodLocals')
+      .innerJoin('foodThumbnailImages', 'foodLocals.id', 'foodThumbnailImages.foodLocalId')
+      .innerJoin('processedImages', 'foodThumbnailImages.imageId', 'processedImages.id')
+      .select(['foodLocals.foodCode as foodCode', 'processedImages.path as path'])
+      .where('foodLocals.localeId', '=', localeId)
+      .where('foodLocals.foodCode', 'in', foodCodes)
+      .execute();
+
+    return Object.fromEntries(rows.map(row => [row.foodCode, row.path]));
+  };
+
   return {
     createImage,
+    resolveImages,
   };
 }
 
