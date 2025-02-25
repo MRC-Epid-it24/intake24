db282c0a8f70a51845a1db4930c122234e03c6e8
Ivan Poliakov <ivpolyakov@gmail.com>
2025-02-25 18:39:09 +0000

    feat: food selection prompt

---
+++ apps/survey/src/components/handlers/custom/FoodSelectionPromptHandler.vue
@@ -0,0 +1,105 @@
+<template>
+  <food-selection-prompt
+    v-model="state"
+    v-bind="{ filteredFoods, meal, prompt, section }"
+    @action="action"
+  />
+</template>
+
+<script lang="ts">
+import type { PropType } from 'vue';
+import { computed, defineComponent } from 'vue';
+
+import type { Prompts } from '@intake24/common/prompts';
+import { dynamicFoodFlag } from '@intake24/common/surveys';
+import type { FoodState, PromptSection } from '@intake24/common/surveys';
+
+import FoodSelectionPrompt from '@intake24/survey/components/prompts/custom/food-selection/food-selection-prompt.vue';
+
+import { useSurvey } from '@intake24/survey/stores';
+import { filterFoodsForFoodSelectionPrompt } from '../../prompts/custom/food-selection/food-selection';
+import { useMealPromptUtils, usePromptHandlerNoStore } from '../mixins';
+
+export default defineComponent({
+  name: 'FoodSelectionPromptHandler',
+
+  components: { FoodSelectionPrompt },
+
+  props: {
+    prompt: {
+      type: Object as PropType<Prompts['food-selection-prompt']>,
+      required: true,
+    },
+    section: {
+      type: String as PropType<PromptSection>,
+      required: true,
+    },
+  },
+
+  emits: ['action'],
+
+  setup(props, ctx) {
+    const surveyStore = useSurvey();
+    const { meal } = useMealPromptUtils();
+
+    const parsedFlag = computed(() => {
+      try {
+        return dynamicFoodFlag.parse(props.prompt.flag);
+      }
+      catch (e: any) {
+        console.warn(`Invalid food flag in ${props.prompt.component} (${props.prompt.id}): "${props.prompt.flag}" (${e.message})`);
+        return undefined;
+      }
+    });
+
+    const filteredFoods = computed(() => filterFoodsForFoodSelectionPrompt(surveyStore, meal.value, props.prompt));
+
+    const isFoodSelected = (food: FoodState): boolean => {
+      if (props.prompt.useFlag) {
+        return parsedFlag.value ? food.flags.includes(parsedFlag.value) : false;
+      }
+      else {
+        return food.customPromptAnswers[props.prompt.id] !== undefined;
+      }
+    };
+
+    const getInitialState = computed(() => meal.value.foods.filter(food => isFoodSelected(food)).map(food => food.id));
+
+    // TODO: use store for intermediate state
+    const { state } = usePromptHandlerNoStore(ctx, getInitialState);
+
+    const commitAnswer = () => {
+      function commitAnswers(food: FoodState) {
+        const selected = state.value.includes(food.id);
+
+        if (props.prompt.useFlag) {
+          if (parsedFlag.value !== undefined)
+            surveyStore.setFoodFlag(food.id, parsedFlag.value, selected);
+        }
+        else {
+          surveyStore.setFoodCustomPromptAnswer({ foodId: food.id, promptId: props.prompt.id, answer: selected });
+        }
+      }
+
+      for (const food of meal.value.foods) {
+        commitAnswers(food);
+      }
+
+      surveyStore.addMealFlag(meal.value.id, `${props.prompt.id}-complete`);
+    };
+
+    const action = (type: string, ...args: [id?: string, params?: object]) => {
+      if (type === 'next') {
+        commitAnswer();
+        ctx.emit('action', type);
+        return;
+      }
+      ctx.emit('action', type, ...args);
+    };
+
+    return { state, action, filteredFoods, meal };
+  },
+});
+</script>
+
+<style scoped></style>
--- apps/survey/src/components/handlers/custom/index.ts
+++ apps/survey/src/components/handlers/custom/index.ts
@@ -1,9 +1,11 @@
 import AggregateChoicePromptHandler from './AggregateChoicePromptHandler.vue';
 import CustomPromptHandler from './CustomPromptHandler.vue';
+import FoodSelectionPromptHandler from './FoodSelectionPromptHandler.vue';
 import MultiPromptHandler from './MultiPromptHandler.vue';
 
 export default {
   AggregateChoicePromptHandler,
   CustomPromptHandler,
+  FoodSelectionPromptHandler,
   MultiPromptHandler,
 };
