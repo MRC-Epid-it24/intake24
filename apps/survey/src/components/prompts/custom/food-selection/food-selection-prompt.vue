7b75e3f59adc33f3a4e6957937c68cf1351068ad
Ivan Poliakov <ivpolyakov@gmail.com>
2025-03-05 12:09:41 +0000

    fix(survey): refactor food selection prompt

--- apps/survey/src/components/prompts/custom/food-selection/food-selection-prompt.vue
+++ apps/survey/src/components/prompts/custom/food-selection/food-selection-prompt.vue
@@ -22,70 +22,51 @@
   </component>
 </template>
 
-<script lang="ts">
+<script lang="ts" setup>
 import type { PropType } from 'vue';
-import { computed, defineComponent, ref } from 'vue';
-
+import { computed, ref } from 'vue';
 import type { FoodState, MealState } from '@intake24/common/surveys';
-import { copy } from '@intake24/common/util';
-import { useI18n } from '@intake24/i18n';
-
 import { usePromptUtils } from '@intake24/survey/composables';
-import createBasePrompt from '../../createBasePrompt';
+import { Next, NextMobile } from '../../actions';
+import { BaseLayout, CardLayout, PanelLayout } from '../../layouts';
+import { createBasePromptProps } from '../../prompt-props';
 
-export default defineComponent({
+defineOptions({
   name: 'FoodSelectionPrompt',
+  components: { BaseLayout, CardLayout, PanelLayout },
+});
 
-  mixins: [createBasePrompt<'food-selection-prompt'>()],
-
-  props: {
-    modelValue: {
-      type: Array as PropType<string[]>,
-      default: () => [] as string[],
-    },
-    meal: {
-      type: Object as PropType<MealState>,
-      required: true,
-    },
-    filteredFoods: {
-      type: Array as PropType<FoodState[]>,
-      required: true,
-    },
+const props = defineProps({
+  ...createBasePromptProps<'food-selection-prompt'>(),
+  modelValue: {
+    type: Array as PropType<string[]>,
+    default: () => [] as string[],
   },
-
-  emits: ['action', 'update:modelValue'],
-
-  setup(props, ctx) {
-    const { i18n: translate } = useI18n();
-
-    const promptAnswers = ref(copy(props.modelValue));
-
-    const { action, getFoodName, getMealTime, customPromptLayout, type } = usePromptUtils(
-      props,
-      ctx,
-    );
-
-    const selected = ref(Array.isArray(props.modelValue) ? props.modelValue : []);
-    const isValid = computed(() => true);
-
-    const update = () => {
-      ctx.emit('update:modelValue', [...selected.value]);
-    };
-
-    return {
-      action,
-      customPromptLayout,
-      type,
-      selected,
-      getFoodName,
-      getMealTime,
-      isValid,
-      promptAnswers,
-      translate,
-      update,
-    };
+  meal: {
+    type: Object as PropType<MealState>,
+    required: true,
+  },
+  filteredFoods: {
+    type: Array as PropType<FoodState[]>,
+    required: true,
   },
 });
+
+const emit = defineEmits(['action', 'update:modelValue']);
+
+const { action, getFoodName, customPromptLayout } = usePromptUtils(
+  props,
+  { emit },
+);
+
+const selected = ref(Array.isArray(props.modelValue) ? props.modelValue : []);
+const isValid = computed(() => true);
+
+function update() {
+  emit('update:modelValue', [...selected.value]);
+}
+
+defineExpose({ isValid });
 </script>
 
 <style lang="scss" scoped>
